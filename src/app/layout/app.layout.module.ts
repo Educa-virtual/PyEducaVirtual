52e69f5ee4e90ae1b63b9ecc114825e29796db84
DREW <jhoand60@gmail.com>
2024-09-09 13:58:34 -0500

    fix(AppLayout): add DropdownModule and PrimengModule

--- src/app/layout/app.layout.module.ts
+++ src/app/layout/app.layout.module.ts
@@ -1,23 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { InputTextModule } from 'primeng/inputtext';
-import { SidebarModule } from 'primeng/sidebar';
-import { BadgeModule } from 'primeng/badge';
-import { RadioButtonModule } from 'primeng/radiobutton';
-import { InputSwitchModule } from 'primeng/inputswitch';
-import { RippleModule } from 'primeng/ripple';
-import { AppMenuComponent } from './app.menu.component';
-import { AppMenuitemComponent } from './app.menuitem.component';
-import { RouterModule } from '@angular/router';
-import { AppTopBarComponent } from './app.topbar.component';
-import { AppFooterComponent } from './app.footer.component';
-import { AppConfigModule } from './config/config.module';
-import { AppSidebarComponent } from "./app.sidebar.component";
-import { AppLayoutComponent } from "./app.layout.component";
-import { DropdownModule } from 'primeng/dropdown';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { FormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { InputTextModule } from 'primeng/inputtext'
+import { SidebarModule } from 'primeng/sidebar'
+import { BadgeModule } from 'primeng/badge'
+import { RadioButtonModule } from 'primeng/radiobutton'
+import { InputSwitchModule } from 'primeng/inputswitch'
+import { RippleModule } from 'primeng/ripple'
+import { AppMenuComponent } from './app.menu.component'
+import { AppMenuitemComponent } from './app.menuitem.component'
+import { RouterModule } from '@angular/router'
+import { AppTopBarComponent } from './app.topbar.component'
+import { AppFooterComponent } from './app.footer.component'
+import { AppConfigModule } from './config/config.module'
+import { AppSidebarComponent } from './app.sidebar.component'
+import { AppLayoutComponent } from './app.layout.component'
+import { DropdownModule } from 'primeng/dropdown'
+import { PrimengModule } from '../primeng.module'
 
 @NgModule({
     declarations: [
@@ -41,8 +42,9 @@ import { DropdownModule } from 'primeng/dropdown';
         RippleModule,
         RouterModule,
         AppConfigModule,
-        DropdownModule
+        DropdownModule,
+        PrimengModule,
     ],
-    exports: [AppLayoutComponent]
+    exports: [AppLayoutComponent],
 })
-export class AppLayoutModule { }
+export class AppLayoutModule {}

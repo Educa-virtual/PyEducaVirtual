a465d692893e1efc601295090ed95045fb442a14
Aron <blazte.aster@gmail.com>
2024-09-09 16:28:23 -0500

    fix logit height (#8)

---
+++ src/app/sistema/docente/areas-estudios/areas-estudios.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-areas-estudios',
+  standalone: true,
+  imports: [],
+  templateUrl: './areas-estudios.component.html',
+  styleUrl: './areas-estudios.component.scss'
+})
+export class AreasEstudiosComponent {
+
+}

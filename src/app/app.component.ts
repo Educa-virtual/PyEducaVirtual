b009c11428117a2c0f1566e4da91e5fb71384bb8
DREW <jhoand60@gmail.com>
2024-09-09 13:57:58 -0500

    fix(AppComponent): add isLoggedIn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
 import { Component, OnInit } from '@angular/core'
 import { PrimeNGConfig } from 'primeng/api'
+import { TokenStorageService } from './servicios/token.service'
+import { Router } from '@angular/router'
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
-    constructor(private primengConfig: PrimeNGConfig) {}
+    isLoggedIn = false
+    username: string
+    constructor(
+        private primengConfig: PrimeNGConfig,
+        private tokenStorageService: TokenStorageService,
+        private router: Router
+    ) {}
 
     ngOnInit() {
         this.primengConfig.ripple = true
+        this.isLoggedIn = !!this.tokenStorageService.getToken()
+
+        if (this.isLoggedIn) {
+            const user = this.tokenStorageService.getUser()
+            this.username = user.user.cCredUsuario
+        } else {
+            this.router.navigate(['login'])
+        }
     }
 }

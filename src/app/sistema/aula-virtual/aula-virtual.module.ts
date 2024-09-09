163510aa4ac13f5a068abd32021dd1cec8b8ac97
Aron <blazte.aster@gmail.com>
2024-09-09 14:40:11 -0500

    Revert "Aula folder structure (#5)"
    This reverts commit f96fd7d961d944f7d3f4c75dc4ad21b7afb42e77.

--- src/app/sistema/aula-virtual/aula-virtual.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core'
-import { CommonModule } from '@angular/common'
-import { AulaVirtualRoutingModule } from './aula-virtual-routing.module'
-
-@NgModule({
-    declarations: [],
-    imports: [CommonModule, AulaVirtualRoutingModule],
-})
-export class AulaVirtualModule {}

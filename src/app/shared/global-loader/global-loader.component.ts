2efe9c066eaceeef18a035408b84de46eaa5f436
Aron <blazte.aster@gmail.com>
2024-09-10 11:06:15 -0500

    feat: add animation to the global loader

--- src/app/shared/global-loader/global-loader.component.ts
+++ src/app/shared/global-loader/global-loader.component.ts
@@ -4,13 +4,31 @@ import { ProgressSpinnerModule } from 'primeng/progressspinner'
 import { GlobalLoaderServiceService } from './global-loader-service/global-loader-service.service'
 import { Observable } from 'rxjs'
 import { AsyncPipe } from '@angular/common'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { trigger, state, style, transition, animate } from '@angular/animations'
 
 @Component({
     selector: 'app-global-loader',
     standalone: true,
-    imports: [ProgressSpinnerModule, DialogModule, AsyncPipe],
+    imports: [
+        ProgressSpinnerModule,
+        DialogModule,
+        AsyncPipe,
+        BrowserAnimationsModule,
+    ],
     templateUrl: './global-loader.component.html',
     styleUrl: './global-loader.component.scss',
+    animations: [
+        trigger('fadeInOut', [
+            state(
+                'void',
+                style({
+                    opacity: 0,
+                })
+            ),
+            transition('void <=> *', animate(300)),
+        ]),
+    ],
 })
 export class GlobalLoaderComponent {
     private _loaderService = inject(GlobalLoaderServiceService)

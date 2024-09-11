ea0df874df8464abf1d743292f87af56dd36ed38
Aron <blazte.aster@gmail.com>
2024-09-11 08:41:59 -0500

    fix: change import global loader component

--- src/app/layout/app.layout.module.ts
+++ src/app/layout/app.layout.module.ts
@@ -19,7 +19,7 @@ import { AppSidebarComponent } from './app.sidebar.component'
 import { AppLayoutComponent } from './app.layout.component'
 import { DropdownModule } from 'primeng/dropdown'
 import { PrimengModule } from '../primeng.module'
-import { GlobalLoaderComponent } from '../shared/global-loader/global-loader.component'
+import { GlobalLoaderComponent } from '../shared/interceptors/global-loader/global-loader.component'
 
 @NgModule({
     declarations: [

f3705108604826a84251cf378d90744c015cdf68
Aron <blazte.aster@gmail.com>
2024-09-11 09:43:00 -0500

    feat: add error interceptor using messageService from primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,12 +15,21 @@ import { PrimengModule } from './primeng.module'
 import { SharedModule } from './shared/shared.module'
 import { HTTP_INTERCEPTORS } from '@angular/common/http'
 import { GlobalLoaderInterceptor } from './shared/interceptors/global-loader/global-loader-interceptor/global-loader-interceptor.interceptor'
+import { ErrorInterceptor } from './shared/interceptors/error-interceptor/error-interceptor.interceptor'
+import { MessageService } from 'primeng/api'
+import { ToastModule } from 'primeng/toast'
 
 //////////////////
 
 @NgModule({
     declarations: [AppComponent, NotfoundComponent],
-    imports: [AppRoutingModule, AppLayoutModule, PrimengModule, SharedModule],
+    imports: [
+        AppRoutingModule,
+        AppLayoutModule,
+        PrimengModule,
+        SharedModule,
+        ToastModule,
+    ],
     providers: [
         { provide: LocationStrategy, useClass: PathLocationStrategy },
         CountryService,
@@ -29,10 +38,17 @@ import { GlobalLoaderInterceptor } from './shared/interceptors/global-loader/glo
         IconService,
         NodeService,
         PhotoService,
+        MessageService,
+
         ProductService,
         {
             provide: HTTP_INTERCEPTORS,
             useClass: GlobalLoaderInterceptor,
+            multi: true,
+        },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: ErrorInterceptor,
             multi: true,
         },
     ],

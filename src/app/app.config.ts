79964ebef0b77e24bfec4b46ab218cb04639708a
Aron <blazte.aster@gmail.com>
2024-09-11 11:13:38 -0500

    refactor: replace app-routing.module  for app.route.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,6 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http'
 import { ApplicationConfig, importProvidersFrom } from '@angular/core'
 import { MessageService } from 'primeng/api'
 import { ToastModule } from 'primeng/toast'
-import { AppRoutingModule } from './app-routing.module'
 import { CountryService } from './demo/service/country.service'
 import { CustomerService } from './demo/service/customer.service'
 import { EventService } from './demo/service/event.service'
@@ -15,15 +14,32 @@ import { AppLayoutModule } from './layout/app.layout.module'
 import { PrimengModule } from './primeng.module'
 import { ErrorInterceptor } from './shared/interceptors/error-interceptor/error-interceptor.interceptor'
 import { GlobalLoaderInterceptor } from './shared/interceptors/global-loader/global-loader-interceptor/global-loader-interceptor.interceptor'
+import {
+    InMemoryScrollingFeature,
+    InMemoryScrollingOptions,
+    provideRouter,
+    RouterConfigurationFeature,
+    withInMemoryScrolling,
+    withRouterConfig,
+} from '@angular/router'
+import { routes } from './app.routes'
+
+const scrollConfig: InMemoryScrollingOptions = {
+    scrollPositionRestoration: 'enabled',
+    anchorScrolling: 'enabled',
+}
+
+const routerConfig: RouterConfigurationFeature = withRouterConfig({
+    onSameUrlNavigation: 'reload',
+})
+
+const inMemoryScrollingFeature: InMemoryScrollingFeature =
+    withInMemoryScrolling(scrollConfig)
 
 export const appConfig: ApplicationConfig = {
     providers: [
-        importProvidersFrom(
-            AppRoutingModule,
-            AppLayoutModule,
-            PrimengModule,
-            ToastModule
-        ),
+        provideRouter(routes, inMemoryScrollingFeature, routerConfig),
+        importProvidersFrom(AppLayoutModule, PrimengModule, ToastModule),
         { provide: LocationStrategy, useClass: PathLocationStrategy },
         CountryService,
         CustomerService,

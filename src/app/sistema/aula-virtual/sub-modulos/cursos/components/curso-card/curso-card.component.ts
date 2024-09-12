bdf895430a62a3fd34c23fbbde7e75cc7db97b31
Aron <blazte.aster@gmail.com>
2024-09-12 15:52:56 -0500

    feact: add routerlink to curso-card

--- src/app/sistema/aula-virtual/sub-modulos/cursos/components/curso-card/curso-card.component.ts
+++ src/app/sistema/aula-virtual/sub-modulos/cursos/components/curso-card/curso-card.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common'
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
 import { ICurso } from '../../interfaces/curso.interface'
 import { ButtonModule } from 'primeng/button'
+import { RouterModule } from '@angular/router'
 
 @Component({
     selector: 'app-curso-card',
     standalone: true,
-    imports: [CommonModule, ButtonModule],
+    imports: [CommonModule, ButtonModule, RouterModule],
     templateUrl: './curso-card.component.html',
     styleUrl: './curso-card.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,

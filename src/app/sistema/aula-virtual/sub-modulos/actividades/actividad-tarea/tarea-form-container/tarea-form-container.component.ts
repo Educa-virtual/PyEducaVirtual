5a299b9a1757dc57bd91c48a7eaef5a3a985e0cc
Aron <blazte.aster@gmail.com>
2024-09-16 10:39:46 -0500

    feat: add tarea-form container

--- src/app/sistema/aula-virtual/sub-modulos/actividades/actividad-tarea/tarea-form-container/tarea-form-container.component.ts
+++ src/app/sistema/aula-virtual/sub-modulos/actividades/actividad-tarea/tarea-form-container/tarea-form-container.component.ts
@@ -1,9 +1,16 @@
 import { CommonModule } from '@angular/common'
-import { Component, Input } from '@angular/core'
+import {
+    Component,
+    EventEmitter,
+    Input,
+    Output,
+    ViewChild,
+} from '@angular/core'
 import { DialogModule } from 'primeng/dialog'
 import { TareaFormComponent } from '../tarea-form/tarea-form.component'
 import { IActividad } from '@/app/sistema/aula-virtual/interfaces/actividad.interface'
 import { ButtonModule } from 'primeng/button'
+import { FormGroup } from '@angular/forms'
 
 @Component({
     selector: 'app-tarea-form-container',
@@ -13,6 +20,22 @@ import { ButtonModule } from 'primeng/button'
     styleUrl: './tarea-form-container.component.scss',
 })
 export class TareaFormContainerComponent {
+    @ViewChild(TareaFormComponent) tareaFormComponent: TareaFormComponent
     @Input({ required: true }) visible: boolean = false
     @Input() actividad: IActividad | undefined
+
+    @Output() visibleChange = new EventEmitter<boolean>()
+
+    closeModal(event: boolean) {
+        this.visibleChange.emit(event)
+    }
+
+    submitFormulario(form: FormGroup) {
+        console.log('Formulario enviado:', form.value)
+    }
+
+    cancelar() {
+        this.visibleChange.emit(false)
+        console.log('Formulario cancelado')
+    }
 }
--- src/app/sistema/aula-virtual/sub-modulos/actividades/actividad-tarea/tarea-form/tarea-form.component.ts
+++ src/app/sistema/aula-virtual/sub-modulos/actividades/actividad-tarea/tarea-form/tarea-form.component.ts
@@ -1,23 +1,54 @@
+import { CommonInputComponent } from '@/app/shared/components/common-input/common-input.component'
 import { CommonModule } from '@angular/common'
-import { Component, inject } from '@angular/core'
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms'
+import { Component, EventEmitter, inject, Output } from '@angular/core'
+import {
+    FormBuilder,
+    FormGroup,
+    FormsModule,
+    ReactiveFormsModule,
+    Validators,
+} from '@angular/forms'
+import { EditorModule } from 'primeng/editor'
+import { DisponibilidadFormComponent } from '../../components/disponibilidad-form/disponibilidad-form.component'
 
 @Component({
     selector: 'app-tarea-form',
     standalone: true,
-    imports: [CommonModule, ReactiveFormsModule],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        CommonInputComponent,
+        EditorModule,
+        FormsModule,
+        DisponibilidadFormComponent,
+    ],
     templateUrl: './tarea-form.component.html',
     styleUrl: './tarea-form.component.scss',
 })
 export class TareaFormComponent {
+    @Output() submitEvent = new EventEmitter<FormGroup>()
+    @Output() cancelEvent = new EventEmitter<void>()
+
     private _formBuilder = inject(FormBuilder)
+
+    public text: string = ''
 
     public tareaForm = this._formBuilder.group({
         titulo: ['', [Validators.required]],
-        descripcion: [''],
+        descripcion: ['', [Validators.required]],
+        indicaciones: [''],
     })
 
-    onSubmit() {
-        console.log(this.tareaForm.value)
+    submit() {
+        if (this.tareaForm.valid) {
+            this.submitEvent.emit(this.tareaForm)
+        } else {
+            this.tareaForm.markAllAsTouched()
+        }
+    }
+
+    cancel() {
+        this.tareaForm.reset()
+        this.cancelEvent.emit()
     }
 }

ac286f627d689c0e6db658db3a903c40e64c0ec4
Aron <blazte.aster@gmail.com>
2024-09-16 08:20:30 -0500

    feat: create commo-base-input

--- src/app/shared/components/common-input/common-input.component.ts
+++ src/app/shared/components/common-input/common-input.component.ts
@@ -17,13 +17,31 @@ import { InputTextModule } from 'primeng/inputtext'
 export class CommonInputComponent implements OnInit {
     @Input({ required: true }) controlKey: string
     @Input() id: string
+    @Input() label: string
+    @Input() type: string = 'text'
     private _parentContainer = inject(ControlContainer)
 
     ngOnInit(): void {
+        this.parentFormGroup.get(this.controlKey).hasError('required')
         console.log(this._parentContainer.control.get(this.controlKey))
     }
 
     get parentFormGroup() {
         return this._parentContainer.control as FormGroup
+    }
+
+    get errorMessage(): string | null {
+        const control = this._parentContainer.control.get(this.controlKey)
+        if (control.hasError('required')) {
+            return 'Este campo es requerido'
+        }
+        return null
+    }
+
+    get campoInvalido(): boolean {
+        return (
+            this._parentContainer.control.get(this.controlKey).invalid &&
+            this._parentContainer.control.get(this.controlKey).touched
+        )
     }
 }

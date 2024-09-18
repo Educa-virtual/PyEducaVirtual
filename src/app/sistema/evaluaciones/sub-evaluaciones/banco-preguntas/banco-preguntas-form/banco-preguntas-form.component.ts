f06fb58a1783f7f305c41c924be5e8a27c2e174d
Richard <techsolutionscarlnet@gmail.com>
2024-09-18 15:19:20 -0500

    agregando banco-preguntas-form

---
+++ src/app/sistema/evaluaciones/sub-evaluaciones/banco-preguntas/banco-preguntas-form/banco-preguntas-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-banco-preguntas-form',
+    standalone: true,
+    imports: [],
+    templateUrl: './banco-preguntas-form.component.html',
+    styleUrl: './banco-preguntas-form.component.scss',
+})
+export class BancoPreguntasFormComponent {}

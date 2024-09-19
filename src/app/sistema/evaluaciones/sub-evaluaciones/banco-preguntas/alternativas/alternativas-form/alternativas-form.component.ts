03b3f4539bb0e4e3a115a8f15f017bb2af6c87ae
Richard <techsolutionscarlnet@gmail.com>
2024-09-19 10:04:02 -0500

    se actulizo formulario banco de preguntas

---
+++ src/app/sistema/evaluaciones/sub-evaluaciones/banco-preguntas/alternativas/alternativas-form/alternativas-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-alternativas-form',
+    standalone: true,
+    imports: [],
+    templateUrl: './alternativas-form.component.html',
+    styleUrl: './alternativas-form.component.scss',
+})
+export class AlternativasFormComponent {}

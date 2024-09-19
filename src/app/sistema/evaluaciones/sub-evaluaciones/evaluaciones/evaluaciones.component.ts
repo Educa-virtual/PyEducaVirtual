22d9cd00eaf7a087f51a2e27c85c5c820ea87437
Aron <blazte.aster@gmail.com>
2024-09-19 08:27:56 -0500

    feat(banco-preguntas): add banco de preguntas new layout (#51)

--- src/app/sistema/evaluaciones/sub-evaluaciones/evaluaciones/evaluaciones.component.ts
+++ src/app/sistema/evaluaciones/sub-evaluaciones/evaluaciones/evaluaciones.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+
+/*GRILLA */
+import { Customer } from 'src/app/demo/api/customer'
+import { CustomerService } from 'src/app/demo/service/customer.service'
+
+import { TableModule } from 'primeng/table'
+import { CommonModule } from '@angular/common'
+
+/*BOTONES */
+import { ButtonModule } from 'primeng/button'
+
+/*MODAL */
+import { DialogModule } from 'primeng/dialog'
+
+/*INPUT TEXT */
+import { InputTextModule } from 'primeng/inputtext'
 
 @Component({
     selector: 'app-evaluaciones',
     standalone: true,
-    imports: [],
+    imports: [
+        TableModule,
+        CommonModule,
+        ButtonModule,
+        DialogModule,
+        InputTextModule,
+    ],
     templateUrl: './evaluaciones.component.html',
     styleUrl: './evaluaciones.component.scss',
 })
-export class EvaluacionesComponent {}
+export class EvaluacionesComponent implements OnInit {
+    customers!: Customer[]
+    visible: boolean = false
+
+    constructor(private customerService: CustomerService) {}
+
+    ngOnInit() {
+        this.customerService
+            .getCustomersLarge()
+            .then((customers) => (this.customers = customers))
+    }
+    showDialog() {
+        this.visible = true
+    }
+    click() {}
+}

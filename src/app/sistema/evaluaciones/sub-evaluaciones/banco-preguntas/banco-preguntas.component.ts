8590583e02abc692cafef64d9243234da65ab259
Aron <blazte.aster@gmail.com>
2024-09-19 11:55:12 -0500

    feat(selectors): add selector for competencias, capacidades

--- src/app/sistema/evaluaciones/sub-evaluaciones/banco-preguntas/banco-preguntas.component.ts
+++ src/app/sistema/evaluaciones/sub-evaluaciones/banco-preguntas/banco-preguntas.component.ts
@@ -1,10 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, inject } from '@angular/core'
-import { SelectItem } from 'primeng/api'
-import { Customer, Representative } from 'src/app/demo/api/customer'
-import { CustomerService } from 'src/app/demo/service/customer.service'
-import { Product } from 'src/app/demo/api/product'
-import { ProductService } from 'src/app/demo/service/product.service'
-import { Table } from 'primeng/table'
+import { Component, OnInit, inject } from '@angular/core'
 import { Button } from 'primeng/button'
 import { InputTextModule } from 'primeng/inputtext'
 import { MultiSelectModule } from 'primeng/multiselect'
@@ -14,7 +8,6 @@ import { DropdownModule } from 'primeng/dropdown'
 import { AlternativasComponent } from './alternativas/alternativas.component'
 import { CompetenciasComponent } from '../competencias/competencias.component'
 
-//EDITOR
 import {
     IColumn,
     TablePrimengComponent,
@@ -26,11 +19,6 @@ import {
 import { provideIcons } from '@ng-icons/core'
 import { matGroupWork } from '@ng-icons/material-icons/baseline'
 
-interface expandedRows {
-    [key: string]: boolean
-}
-
-/* modal  */
 import { DialogService } from 'primeng/dynamicdialog'
 import { BancoPreguntasFormComponent } from '../banco-preguntas/banco-preguntas-form/banco-preguntas-form.component'
 import { MODAL_CONFIG } from '@/app/shared/constants/modal.config'
@@ -50,45 +38,14 @@ import { MODAL_CONFIG } from '@/app/shared/constants/modal.config'
         Button,
         TablePrimengComponent,
     ],
+    styleUrls: ['./banco-preguntas.component.scss'],
 })
 export class BancoPreguntasComponent implements OnInit {
     private _dialogService = inject(DialogService)
 
-    display: boolean = false
-
-    customers1: Customer[] = []
-
-    selectedCustomers1: Customer[] = []
-
-    selectedCustomer: Customer = {}
-
-    representatives: Representative[] = []
-
-    statuses: Product[] = []
-
-    products: Product[] = []
-
-    rowGroupMetadata: Product
-
-    expandedRows: expandedRows = {}
-
-    activityValues: number[] = [0, 100]
-
-    loading: boolean = true
-    valSwitch: boolean = false
-    selectedDrop: SelectItem = { value: '' }
-    nivel: SelectItem[] = []
-    grado: SelectItem[] = []
-    area: SelectItem[] = []
-
-    competencia: SelectItem[] = []
-    capacidad: SelectItem[] = []
-    desempenio: SelectItem[] = []
-    tipo_pregunta: SelectItem[] = []
-    clave: SelectItem[] = []
-
-    text
-    textAyuda
+    public competencias = []
+    public capacidades = []
+    public desempenios = []
 
     selectedItems = []
     accionesPrincipal: IActionContainer[] = [
@@ -162,76 +119,30 @@ export class BancoPreguntasComponent implements OnInit {
             text_header: 'Puntaje',
         },
     ]
-
-    @ViewChild('filter') filter!: ElementRef
-
-    constructor(
-        private customerService: CustomerService,
-        private productService: ProductService
-    ) {}
+    constructor() {}
 
     ngOnInit() {
-        this.customerService.getCustomersLarge().then((customers) => {
-            this.customers1 = customers
-            this.loading = false
-
-            /*  this.customers1.forEach(
-                (customer) => (customer.date = new Date(customer.date))
-            )*/
-        })
-        this.nivel = [
+        this.competencias = [
             {
-                label: 'Nivel Primaria',
-                value: { id: 1, name: 'New York', code: 'NY' },
+                iCompentenciaId: 0,
+                cCompetenciaDescripcion: 'Sin Asignar',
             },
             {
-                label: 'Nivel Secundaria',
-                value: { id: 2, name: 'Rome', code: 'RM' },
+                iCompentenciaId: 1,
+                cCompetenciaDescripcion: 'Capacidad 1',
             },
         ]
-        this.grado = [
-            { label: '2do.', value: { id: 1, name: 'New York', code: 'NY' } },
-            { label: '4to.', value: { id: 2, name: 'Rome', code: 'RM' } },
-        ]
-        this.area = [
+        this.capacidades = [
             {
-                label: 'Matemáticas',
-                value: { id: 1, name: 'New York', code: 'NY' },
+                iCapacidadId: 0,
+                cCapacidadDescripcion: 'Sin Asignar',
             },
             {
-                label: 'Comunicación',
-                value: { id: 2, name: 'Rome', code: 'RM' },
+                iCapacidadId: 1,
+                cCapacidadDescripcion:
+                    'Comunica su comprensión sobrelos números y las operacione',
             },
         ]
-    }
-
-    onSort() {
-        this.updateRowGroupMetaData()
-    }
-
-    updateRowGroupMetaData() {
-        this.rowGroupMetadata = {}
-    }
-
-    formatCurrency(value: number) {
-        return value.toLocaleString('en-US', {
-            style: 'currency',
-            currency: 'USD',
-        })
-    }
-
-    onGlobalFilter(table: Table, event: Event) {
-        table.filterGlobal((event.target as HTMLInputElement).value, 'contains')
-    }
-
-    clear(table: Table) {
-        table.clear()
-        this.filter.nativeElement.value = ''
-    }
-
-    ok() {
-        console.log(this.textAyuda)
-        alert(this.textAyuda)
     }
 
     setSelectedItems(event) {

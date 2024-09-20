b944d9194dd174cdaa41e98662e22548307d0af5
Richard <techsolutionscarlnet@gmail.com>
2024-09-20 14:45:53 -0500

    Se actualizo formulario Evaluaciones

--- src/app/sistema/evaluaciones/sub-evaluaciones/evaluaciones/evaluaciones-form/evaluaciones-form.component.ts
+++ src/app/sistema/evaluaciones/sub-evaluaciones/evaluaciones/evaluaciones-form/evaluaciones-form.component.ts
@@ -8,14 +8,22 @@ import { DialogModule } from 'primeng/dialog'
 
 /*INPUT TEXT */
 import { InputTextModule } from 'primeng/inputtext'
-
+import { InputTextareaModule } from 'primeng/inputtextarea'
+import { FormsModule } from '@angular/forms'
 @Component({
     selector: 'app-evaluaciones-form',
     standalone: true,
-    imports: [ButtonModule, DialogModule, InputTextModule],
+    imports: [
+        ButtonModule,
+        DialogModule,
+        InputTextModule,
+        InputTextareaModule,
+        FormsModule,
+    ],
     templateUrl: './evaluaciones-form.component.html',
     styleUrl: './evaluaciones-form.component.scss',
 })
 export class EvaluacionesFormComponent {
     visible: boolean = false
+    value!: string
 }

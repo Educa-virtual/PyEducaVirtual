efb3c883bc9636967672cdbbf1cfa832816be032
Aron <blazte.aster@gmail.com>
2024-09-24 17:00:34 -0500

    use endpoints and validate default forms

--- src/app/sistema/evaluaciones/sub-evaluaciones/banco-preguntas/alternativas/alternativas.component.ts
+++ src/app/sistema/evaluaciones/sub-evaluaciones/banco-preguntas/alternativas/alternativas.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core'
+import { Component, inject, Input } from '@angular/core'
 import { ButtonModule } from 'primeng/button'
 
 /*import { Product } from '@domain/product';
@@ -11,25 +11,163 @@ import { DialogService } from 'primeng/dynamicdialog'
 import { AlternativasFormComponent } from '../alternativas/alternativas-form/alternativas-form.component'
 import { MODAL_CONFIG } from '@/app/shared/constants/modal.config'
 
-import { BotonesModalFormComponent } from '../alternativas/alternativas-form/botones-modal-form/botones-modal-form.component'
+import {
+    IActionTable,
+    IColumn,
+    TablePrimengComponent,
+} from '../../../../../shared/table-primeng/table-primeng.component'
+import { ConfirmationModalService } from '@/app/shared/confirm-modal/confirmation-modal.service'
+import { ApiEvaluacionesService } from '../../../services/api-evaluaciones.service'
+import { ConfirmationService } from 'primeng/api'
+import { alertToConfirm } from '@/app/shared/utils/default-confirm-message'
 @Component({
     selector: 'app-alternativas',
     standalone: true,
-    imports: [ButtonModule, TableModule, CommonModule],
+    imports: [ButtonModule, TableModule, CommonModule, TablePrimengComponent],
     templateUrl: './alternativas.component.html',
     styleUrl: './alternativas.component.scss',
     providers: [DialogService],
 })
 export class AlternativasComponent {
+    @Input() alternativas = []
+    @Input() pregunta
     private _dialogService = inject(DialogService)
+    private _confirmationModalService = inject(ConfirmationModalService)
+    private _evaluacionesService = inject(ApiEvaluacionesService)
+    private _confirmationService = inject(ConfirmationService)
+    public columnas: IColumn[] = [
+        {
+            type: 'text',
+            width: '5rem',
+            field: 'cAlternativaDescripcion',
+            header: 'Descripción',
+            text_header: 'left',
+            text: 'left',
+        },
 
-    agregarAlternativa() {
-        this._dialogService.open(AlternativasFormComponent, {
-            ...MODAL_CONFIG,
-            header: 'Alternativa',
-            templates: {
-                footer: BotonesModalFormComponent,
+        {
+            type: 'text',
+            width: '5rem',
+            field: 'cAlternativaLetra',
+            header: 'Letra Alternativa',
+            text_header: 'left',
+            text: 'left',
+        },
+        {
+            type: 'estado',
+            width: '5rem',
+            field: 'bAlternativaCorrecta',
+            header: 'Alternativa Correcta',
+            text_header: 'left',
+            text: 'left',
+            customFalsy: {
+                trueText: 'Correcta',
+                falseText: 'Incorrecta',
             },
+        },
+        {
+            type: 'text',
+            width: '5rem',
+            field: 'cAlternativaExplicacion',
+            header: 'Explicación',
+            text_header: 'left',
+            text: 'left',
+        },
+        {
+            type: 'actions',
+            width: '5rem',
+            field: 'Acciones',
+            header: 'Acciones',
+            text_header: 'left',
+            text: 'left',
+        },
+    ]
+
+    public accionesTabla: IActionTable[] = [
+        {
+            labelTooltip: 'Editar',
+            icon: 'pi pi-pencil',
+            accion: 'editar',
+            type: 'item',
+            class: 'p-button-rounded p-button-warning p-button-text',
+        },
+        {
+            labelTooltip: 'Eliminar',
+            icon: 'pi pi-trash',
+            accion: 'eliminar',
+            type: 'item',
+            class: 'p-button-rounded p-button-danger p-button-text',
+        },
+    ]
+
+    agregarActualizarAlternativa(alternativa) {
+        const refModal = this._dialogService.open(AlternativasFormComponent, {
+            ...MODAL_CONFIG,
+            data: {
+                alternativa: alternativa,
+                pregunta: this.pregunta,
+            },
+            header:
+                alternativa == null
+                    ? 'Agregar Alternativa'
+                    : 'Actualizar Alternativa',
         })
+
+        refModal.onClose.subscribe((result) => {
+            if (result) {
+                result.bAlternativaCorrecta = result.bAlternativaCorrecta
+                    ? 1
+                    : 0
+                const existeAlternativa = this.alternativas.some(
+                    (x) => x.iAlternativaId == result.iAlternativaId
+                )
+                if (existeAlternativa) {
+                    this.alternativas[
+                        this.alternativas.findIndex(
+                            (x) => x.iAlternativaId == result.iAlternativaId
+                        )
+                    ] = result
+                } else {
+                    this.alternativas.push(result)
+                }
+            }
+        })
+    }
+
+    eliminarAlternativa(alternativa) {
+        if (alternativa.isLocal) {
+            this.eliminarAlternativaLocal(alternativa)
+            return
+        }
+        this._evaluacionesService
+            .eliminarAlternativaById(alternativa.iAlternativaId)
+            .subscribe({
+                next: () => {
+                    this.eliminarAlternativaLocal(alternativa)
+                },
+            })
+        return
+        this._confirmationService.confirm(
+            alertToConfirm({
+                header: 'Esta seguro de eliminar la alternativa?',
+                accept: () => {},
+                reject: () => {},
+            })
+        )
+    }
+
+    eliminarAlternativaLocal(alternativa) {
+        this.alternativas = this.alternativas.filter(
+            (item) => item.iAlternativaId != alternativa.iAlternativaId
+        )
+    }
+
+    accionBtnItemTable({ accion, item }) {
+        if (accion === 'eliminar') {
+            this.eliminarAlternativa(item)
+        }
+        if (accion === 'editar') {
+            this.agregarActualizarAlternativa(item)
+        }
     }
 }

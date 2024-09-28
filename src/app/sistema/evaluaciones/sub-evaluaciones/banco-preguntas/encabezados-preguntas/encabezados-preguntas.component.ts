9ecc99bec6483ec9dffa5808bc36ec418e89a876
Aron <blazte.aster@gmail.com>
2024-09-28 15:38:32 -0500

    feat: add encabezado pregunta

--- src/app/sistema/evaluaciones/sub-evaluaciones/banco-preguntas/encabezados-preguntas/encabezados-preguntas.component.ts
+++ src/app/sistema/evaluaciones/sub-evaluaciones/banco-preguntas/encabezados-preguntas/encabezados-preguntas.component.ts
@@ -1,15 +1,21 @@
+import { ConfirmationModalService } from '@/app/shared/confirm-modal/confirmation-modal.service'
 import {
     IActionTable,
     IColumn,
     TablePrimengComponent,
 } from '@/app/shared/table-primeng/table-primeng.component'
 import { CommonModule } from '@angular/common'
-import { Component, EventEmitter, Input, Output } from '@angular/core'
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core'
+import { ApiEvaluacionesService } from '../../../services/api-evaluaciones.service'
+import { DialogService } from 'primeng/dynamicdialog'
+import { EncabezadoFormComponent } from './encabezado-form/encabezado-form.component'
+import { MODAL_CONFIG } from '@/app/shared/constants/modal.config'
+import { ButtonModule } from 'primeng/button'
 
 @Component({
     selector: 'app-encabezados-preguntas',
     standalone: true,
-    imports: [CommonModule, TablePrimengComponent],
+    imports: [CommonModule, TablePrimengComponent, ButtonModule],
     templateUrl: './encabezados-preguntas.component.html',
     styleUrl: './encabezados-preguntas.component.scss',
 })
@@ -17,6 +23,11 @@ export class EncabezadosPreguntasComponent {
     @Input() encabezados
     @Input() encabezado
     @Output() encabezadoChange = new EventEmitter()
+    @Output() encabezadosChange = new EventEmitter()
+
+    private _confirmService = inject(ConfirmationModalService)
+    private _evaluacionesService = inject(ApiEvaluacionesService)
+    private _dialogService = inject(DialogService)
 
     public columnas: IColumn[] = [
         {
@@ -70,7 +81,62 @@ export class EncabezadosPreguntasComponent {
     ]
 
     public accionBtnItemTable({ accion, item }) {
-        console.log(accion, item)
+        if (accion === 'eliminar') {
+            this.eliminarEncabezado(item)
+        }
+        if (accion === 'editar') {
+            this.agregarActualizarEncabezadoForm(item)
+        }
+    }
+
+    private async eliminarEncabezado(item) {
+        this._confirmService.openConfirm({
+            header: 'Esta seguro de eliminar el encabezado?',
+            accept: () => {
+                this._evaluacionesService
+                    .eliminarEncabezadoPreguntaById(item.iEncabPregId)
+                    .subscribe({
+                        next: () => {
+                            this.encabezados = this.encabezados.filter(
+                                (enc) => enc.iEncabPregId != item.iEncabPregId
+                            )
+                        },
+                    })
+            },
+        })
+    }
+
+    agregarActualizarEncabezadoForm(encabezado) {
+        const refModal = this._dialogService.open(EncabezadoFormComponent, {
+            ...MODAL_CONFIG,
+            data: {
+                encabezado,
+            },
+            header:
+                encabezado == null
+                    ? 'Agregar Encabezado'
+                    : 'Actualizar Encabezado',
+        })
+        refModal.onClose.subscribe((result) => {
+            if (result) {
+                const existeEncabezado = this.encabezados.some((x) => {
+                    return x.iEncabPregId == result.iEncabPregId
+                })
+
+                // si existe actualizar el encabezado
+                if (existeEncabezado) {
+                    const index = this.encabezados.findIndex(
+                        (x) => x.iEncabPregId == result.iEncabPregId
+                    )
+
+                    this.encabezados[index] = result
+                    this.encabezadosChange.emit(this.encabezados)
+                } else {
+                    this.encabezados.push(result)
+                    this.encabezadosChange.emit(this.encabezados)
+                }
+            }
+        })
     }
 
     public onSelectionChange(event) {

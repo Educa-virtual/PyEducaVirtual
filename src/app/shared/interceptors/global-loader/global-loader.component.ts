fcfed54101886b9cf20479edcad1cb74f785ec01
Aron <blazte.aster@gmail.com>
2024-09-30 15:21:59 -0500

    refactor: move imports into module

--- src/app/shared/interceptors/global-loader/global-loader.component.ts
+++ src/app/shared/interceptors/global-loader/global-loader.component.ts
@@ -1,17 +1,24 @@
-import { Component, inject } from '@angular/core'
+import {
+    ChangeDetectionStrategy,
+    Component,
+    inject,
+    OnInit,
+} from '@angular/core'
 import { DialogModule } from 'primeng/dialog'
 import { ProgressSpinnerModule } from 'primeng/progressspinner'
 import { GlobalLoaderServiceService } from './global-loader-service/global-loader-service.service'
 import { Observable } from 'rxjs'
-import { AsyncPipe } from '@angular/common'
+import { AsyncPipe, NgIf } from '@angular/common'
 import { trigger, state, style, transition, animate } from '@angular/animations'
 
 @Component({
     selector: 'app-global-loader',
     standalone: true,
-    imports: [ProgressSpinnerModule, DialogModule, AsyncPipe],
+    imports: [ProgressSpinnerModule, DialogModule, AsyncPipe, NgIf],
     templateUrl: './global-loader.component.html',
     styleUrl: './global-loader.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+
     animations: [
         trigger('fadeInOut', [
             state(
@@ -24,7 +31,11 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
         ]),
     ],
 })
-export class GlobalLoaderComponent {
+export class GlobalLoaderComponent implements OnInit {
     private _loaderService = inject(GlobalLoaderServiceService)
-    public isVisible$: Observable<boolean> = this._loaderService.loading$
+    public isVisible$: Observable<boolean>
+
+    ngOnInit() {
+        this.isVisible$ = this._loaderService.loading$
+    }
 }

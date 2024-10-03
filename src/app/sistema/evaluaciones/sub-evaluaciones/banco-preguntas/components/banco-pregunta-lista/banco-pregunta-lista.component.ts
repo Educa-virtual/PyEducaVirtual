694a74ae65cec198c3bd39d7f4b68c46418ee76e
Aron <blazte.aster@gmail.com>
2024-10-03 09:02:52 -0500

    feat: crea componente banco preguntas lista base

---
+++ src/app/sistema/evaluaciones/sub-evaluaciones/banco-preguntas/components/banco-pregunta-lista/banco-pregunta-lista.component.ts
@@ -0,0 +1,36 @@
+import {
+    IActionTable,
+    IColumn,
+    TablePrimengComponent,
+} from '@/app/shared/table-primeng/table-primeng.component'
+import { CommonModule } from '@angular/common'
+import { Component, EventEmitter, Input, Output } from '@angular/core'
+import { IPregunta } from '../../models/pregunta.model'
+import { BancoPreguntaPreviewComponent } from '../banco-pregunta-preview/banco-pregunta-preview.component'
+
+@Component({
+    selector: 'app-banco-pregunta-lista',
+    standalone: true,
+    imports: [
+        CommonModule,
+        TablePrimengComponent,
+        BancoPreguntaPreviewComponent,
+    ],
+    templateUrl: './banco-pregunta-lista.component.html',
+    styleUrl: './banco-pregunta-lista.component.scss',
+})
+export class BancoPreguntaListaComponent {
+    @Output() accionBtnItemTable = new EventEmitter()
+    @Output() selectedRowDataChange = new EventEmitter()
+    @Input() columnas: IColumn[] = []
+
+    @Input() data: IPregunta[] = []
+    @Input() selectedRowData: IPregunta[] = []
+    @Input() accionesTabla: IActionTable[]
+    @Input() expandedRowKeys = {}
+
+    onSelectionChange(event) {
+        this.selectedRowData = event
+        this.selectedRowDataChange.emit(event)
+    }
+}

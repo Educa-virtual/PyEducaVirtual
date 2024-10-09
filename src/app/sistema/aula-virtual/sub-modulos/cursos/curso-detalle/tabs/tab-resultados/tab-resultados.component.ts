d2b3577e85d2197f3d36d361b40b9a271aacd321
Your Name <you@example.com>
2024-10-09 17:17:49 -0500

    seagregocomponenteresultados

---
+++ src/app/sistema/aula-virtual/sub-modulos/cursos/curso-detalle/tabs/tab-resultados/tab-resultados.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-tab-resultados',
+    standalone: true,
+    templateUrl: './tab-resultados.component.html',
+    styleUrls: ['./tab-resultados.component.css'],
+})
+export class TabResultadosComponent {}

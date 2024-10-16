7f7a276800a5e10ca3e7028d746702536f7c0f0f
Mitwar99 <mitwarhuacan@hotmail.com>
2024-10-16 17:38:05 -0500

    Obtener datos de foros

--- src/app/sistema/aula-virtual/sub-modulos/actividades/actividad-foro/foro-form-container/foro-form-container.component.ts
+++ src/app/sistema/aula-virtual/sub-modulos/actividades/actividad-foro/foro-form-container/foro-form-container.component.ts
@@ -1,6 +1,6 @@
 import { CommonInputComponent } from '@/app/shared/components/common-input/common-input.component'
 import { CommonModule } from '@angular/common'
-import { Component, inject } from '@angular/core'
+import { Component, inject, OnInit } from '@angular/core'
 import {
     FormBuilder,
     FormGroup,
@@ -12,6 +12,7 @@ import { DisponibilidadFormComponent } from '../../components/disponibilidad-for
 import { DropdownModule } from 'primeng/dropdown'
 import { ButtonModule } from 'primeng/button'
 import { EditorModule } from 'primeng/editor'
+import { ApiAulaService } from '@/app/sistema/aula-virtual/services/api-aula.service'
 
 @Component({
     selector: 'app-foro-form-container',
@@ -28,9 +29,12 @@ import { EditorModule } from 'primeng/editor'
     templateUrl: './foro-form-container.component.html',
     styleUrl: './foro-form-container.component.scss',
 })
-export class ForoFormContainerComponent {
+export class ForoFormContainerComponent implements OnInit {
+    // _aulaService obtener datos
+    private _aulaService = inject(ApiAulaService)
     private _formBuilder = inject(FormBuilder)
     private ref = inject(DynamicDialogRef)
+    perfiles: any[] = []
 
     public categorias = []
 
@@ -39,8 +43,22 @@ export class ForoFormContainerComponent {
         descripcion: ['', [Validators.required]],
         categoria: [0, [Validators.required]],
     })
+    ngOnInit(): void {
+        const userId = 1
+        this._aulaService.guardarForo(userId).subscribe((Data) => {
+            this.perfiles = Data['data']
+        })
+
+        console.log('Datos mit', this.perfiles)
+    }
 
     closeModal(data) {
         this.ref.close(data)
+    }
+
+    submitFormulario(data) {
+        console.log('Prueba Mit', data)
+        this._aulaService.guardarForo(data)
+        console.log(this._aulaService)
     }
 }

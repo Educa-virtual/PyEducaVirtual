61739126b9a2f05bb9f42835e051ebf90713ad2b
DREW <jhoand60@gmail.com>
2024-10-17 17:10:12 -0500

    fix(FormPerfiles): add logout when dont perfiles

--- src/app/layout/toolbar/roles-dropdown/form-perfiles/form-perfiles.component.ts
+++ src/app/layout/toolbar/roles-dropdown/form-perfiles/form-perfiles.component.ts
@@ -1,6 +1,7 @@
 import { PrimengModule } from '@/app/primeng.module'
 import { ConstantesService } from '@/app/servicios/constantes.service'
 import { LocalStoreService } from '@/app/servicios/local-store.service'
+import { TokenStorageService } from '@/app/servicios/token.service'
 import { Component, EventEmitter, Input, Output } from '@angular/core'
 
 @Component({
@@ -19,11 +20,12 @@ export class FormPerfilesComponent {
     name: string
     constructor(
         private store: LocalStoreService,
-        private ConstantesService: ConstantesService
+        private ConstantesService: ConstantesService,
+        private tokenStorageService: TokenStorageService
     ) {
         this.name = this.ConstantesService.nombres
         const user = this.store.getItem('dremoUser')
-        this.perfiles = user.perfiles
+        this.perfiles = user ? user.perfiles : this.logout()
     }
 
     changePerfil(perfiles): void {
@@ -35,5 +37,10 @@ export class FormPerfilesComponent {
     }
     closeModal() {
         this.accionBtnItem.emit({ accion: 'close-modal', item: [] })
+    }
+    logout(): void {
+        this.store.clear()
+        this.tokenStorageService.signOut()
+        window.location.reload()
     }
 }

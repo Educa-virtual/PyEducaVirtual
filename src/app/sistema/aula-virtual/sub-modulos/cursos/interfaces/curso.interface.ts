15ddb07d2c694c8b79fc9b539040bbdec169f44c
Aron <blazte.aster@gmail.com>
2024-10-18 11:35:47 -0500

    Revert "Aula virtual (#93)"
    This reverts commit 3c289ab8813a03a0125bdd3471fd5873ba170fa9.

--- src/app/sistema/aula-virtual/sub-modulos/cursos/interfaces/curso.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface ICurso {
-    iCursoId: string
-    cCursoNombre: string
-    descripcion?: string
-    seccion?: string
-    cGradoAbreviacion?: string
-    iEstudiantes?: number
-    iSilaboId: string
-}
--- src/app/sistema/evaluaciones/sub-evaluaciones/areas/interfaces/area.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface IArea {
-    id: number
-    nombre: string
-    descripcion: string
-    seccion?: string
-    grado?: string
-    totalEstudiantes: number
-    nivel: string
-}

4f83af4c2e74f53dac04b39ffb64728c1c2ef9b1
Aron <blazte.aster@gmail.com>
2024-10-18 11:36:00 -0500

    Revert "Aula virtual (#93)" (#94)
    This reverts commit 3c289ab8813a03a0125bdd3471fd5873ba170fa9.

--- src/app/sistema/configuracion/configuracion.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core'
-import { CommonModule } from '@angular/common'
-import { ConfiguracionRoutingModule } from './configuracion-routing.module'
-
-@NgModule({
-    imports: [CommonModule, ConfiguracionRoutingModule],
-})
-export class ConfiguracionModule {}
--- src/app/sistema/evaluaciones/sub-evaluaciones/banco-preguntas/models/pregunta-aula.model.ts
+++
@@ -1,7 +0,0 @@
-import { IPregunta } from './pregunta.model'
-
-export interface iPreguntaAula extends IPregunta {
-    iDocenteId: number
-    iCurrContId: number
-    iNivelCicloId: number
-}

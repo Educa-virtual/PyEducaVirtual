853955f500ef13055b01f0032a23cc823b01a7da
ejhomz <ejhomz7@gmail.com>
2024-10-29 08:08:38 -0500

    actualizacion de servicios

--- src/app/servicios/general.service.ts
+++ src/app/servicios/general.service.ts
@@ -60,4 +60,33 @@ export class GeneralService {
     subirArchivo(data: any) {
         return this.http.post(`${baseUrl}/general/subir-archivo`, data)
     }
+
+    getDias() {
+        return this.http.get(
+            `${baseUrl}/acad/calendarioAcademico/selCalAcademico?json={"jmod": "grl", "jtable": "dias"}&_opcion=getConsulta`
+
+            /*
+            `${baseUrl}/acad/calendarioAcademico/selCalAcademico`,
+            {
+                json: JSON.stringify({ jmod: 'grl', jtable: 'dias' }),
+                _opcion: 'getConsulta',
+            }*/
+        )
+    }
+
+    getModalidad() {
+        return this.http.get(
+            `${baseUrl}/acad/calendarioAcademico/selCalAcademico?json={"jmod": "acad", "jtable": "modalidad_servicios"}&_opcion=getConsulta`
+        )
+    }
+
+    getTurno() {
+        return this.http.post(
+            `${baseUrl}/acad/calendarioAcademico/addCalAcademico`,
+            {
+                json: JSON.stringify({ jmod: 'grl', jtable: 'turno' }),
+                _opcion: 'getConsulta',
+            }
+        )
+    }
 }

618f99d008236863ba88a0f3e8eaf3be5a81f92c
Aron <blazte.aster@gmail.com>
2024-10-31 17:36:20 -0500

    valida pregunta libre

--- src/app/shared/directives/editor-only-view.directive.ts
+++ src/app/shared/directives/editor-only-view.directive.ts
@@ -1,21 +1,40 @@
-import { Directive, ElementRef, Host, inject } from '@angular/core'
+import {
+    Directive,
+    ElementRef,
+    Host,
+    inject,
+    Input,
+    OnChanges,
+    SimpleChanges,
+} from '@angular/core'
 import { Editor } from 'primeng/editor'
 
 @Directive({
     selector: '[appEditorOnlyView]',
     standalone: true,
 })
-export class EditorOnlyViewDirective {
+export class EditorOnlyViewDirective implements OnChanges {
+    @Input() showToolbar = false
     private readonly elementRef: ElementRef<HTMLElement> = inject(ElementRef)
 
-    editorModules = {
-        toolbar: false,
+    constructor(@Host() private pEditor: Editor) {
+        this.updateEditorModules()
     }
 
-    constructor(@Host() private pEditor: Editor) {
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes['showToolbar']) {
+            this.updateEditorModules()
+        }
+    }
+
+    private updateEditorModules(): void {
+        const editorModules = {
+            toolbar: this.showToolbar,
+        }
+
         if (this.pEditor) {
             this.pEditor.readonly = true
-            this.pEditor.modules = this.editorModules
+            this.pEditor.modules = editorModules
         }
     }
 }

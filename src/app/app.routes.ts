6ff2154a9f7e521873ba392dbd11fcf1a59a06cf
DREW <jhoand60@gmail.com>
2024-11-04 10:14:01 -0500

    fix: remove Demo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,9 @@ import { Routes } from '@angular/router'
 
 import { LoginComponent } from './shared/login/login.component'
 
-import { NotfoundComponent } from './demo/components/notfound/notfound.component'
 import { VerificacionComponent } from './shared/verificacion/verificacion.component'
 import { InicioComponent } from './sistema/inicio/inicio.component'
+import { NotfoundComponent } from './shared/notfound/notfound.component'
 
 export const routes: Routes = [
     { path: '', component: InicioComponent },
@@ -12,60 +12,6 @@ export const routes: Routes = [
         path: '',
 
         children: [
-            {
-                path: '',
-
-                loadChildren: () =>
-                    import('./demo/components/dashboard/dashboard.module').then(
-                        (m) => m.DashboardModule
-                    ),
-            },
-
-            {
-                path: 'uikit',
-
-                loadChildren: () =>
-                    import('./demo/components/uikit/uikit.module').then(
-                        (m) => m.UIkitModule
-                    ),
-            },
-
-            {
-                path: 'utilities',
-
-                loadChildren: () =>
-                    import('./demo/components/utilities/utilities.module').then(
-                        (m) => m.UtilitiesModule
-                    ),
-            },
-
-            {
-                path: 'documentation',
-
-                loadChildren: () =>
-                    import(
-                        './demo/components/documentation/documentation.module'
-                    ).then((m) => m.DocumentationModule),
-            },
-
-            {
-                path: 'blocks',
-
-                loadChildren: () =>
-                    import(
-                        './demo/components/primeblocks/primeblocks.module'
-                    ).then((m) => m.PrimeBlocksModule),
-            },
-
-            {
-                path: 'pages',
-
-                loadChildren: () =>
-                    import('./demo/components/pages/pages.module').then(
-                        (m) => m.PagesModule
-                    ),
-            },
-
             {
                 path: 'docente',
 
@@ -115,24 +61,6 @@ export const routes: Routes = [
         loadChildren: () =>
             import('./sistema/configuracion/configuracion.module').then(
                 (m) => m.ConfiguracionModule
-            ),
-    },
-
-    {
-        path: 'auth',
-
-        loadChildren: () =>
-            import('./demo/components/auth/auth.module').then(
-                (m) => m.AuthModule
-            ),
-    },
-
-    {
-        path: 'landing',
-
-        loadChildren: () =>
-            import('./demo/components/landing/landing.module').then(
-                (m) => m.LandingModule
             ),
     },
 

4ec5f05e564779a811f78ebb1b57723d67d8d063
Aron <blazte.aster@gmail.com>
2024-11-05 08:13:14 -0500

    revert last changes

--- src/app/sistema/aula-virtual/sub-modulos/actividades/actividad-evaluacion/evaluacion-room/components/evaluacion-pregunta/evaluacion-pregunta.component.ts
+++ src/app/sistema/aula-virtual/sub-modulos/actividades/actividad-evaluacion/evaluacion-room/components/evaluacion-pregunta/evaluacion-pregunta.component.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common'
 import { Component, EventEmitter, Input, Output } from '@angular/core'
-import { EvaluacionPreguntaAlternativaComponent } from './evaluacion-pregunta-alternativa/evaluacion-pregunta-alternativa.component'
 import { BancoPreguntaPreviewComponent } from '@/app/sistema/evaluaciones/sub-evaluaciones/banco-preguntas/components/banco-pregunta-preview/banco-pregunta-preview.component'
 import { PrimengModule } from '@/app/primeng.module'
+import { EvaluacionPreguntaAlternativaComponent } from './evaluacion-pregunta-alternativa/evaluacion-pregunta-alternativa.component'
 
 @Component({
     selector: 'app-evaluacion-pregunta',

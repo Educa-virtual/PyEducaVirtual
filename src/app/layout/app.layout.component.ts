c48b89a2faab01c1b46ee5769998748ecea3e448
DREW <jhoand60@gmail.com>
2024-11-12 11:28:09 -0500

    fix(menu): new design

--- src/app/layout/app.layout.component.ts
+++ src/app/layout/app.layout.component.ts
@@ -148,6 +148,7 @@ export class AppLayoutComponent implements OnDestroy {
     }
 
     get containerClass() {
+        console.log(this.layoutService.config().menuMode)
         return {
             'layout-theme-light':
                 this.layoutService.config().colorScheme === 'light',
@@ -155,13 +156,26 @@ export class AppLayoutComponent implements OnDestroy {
                 this.layoutService.config().colorScheme === 'dark',
             'layout-overlay':
                 this.layoutService.config().menuMode === 'overlay',
-            'layout-static': this.layoutService.config().menuMode === 'static',
+            'layout-static':
+                this.layoutService.state.staticMenuDesktopInactive &&
+                this.layoutService.config().menuMode === 'static',
             'layout-static-inactive':
                 this.layoutService.state.staticMenuDesktopInactive &&
+                !this.layoutService.state.staticMenuToggle &&
+                this.layoutService.config().menuMode === 'static',
+            'layout-static-toggle':
+                this.layoutService.state.staticMenuDesktopInactive &&
+                this.layoutService.state.staticMenuToggle &&
                 this.layoutService.config().menuMode === 'static',
             'layout-overlay-active': this.layoutService.state.overlayMenuActive,
             'layout-mobile-active':
                 this.layoutService.state.staticMenuMobileActive,
+            // 'layout-mobile-inactive':
+            //     this.layoutService.state.staticMenuMobileActive && !this.layoutService.state.staticMenuToggle &&
+            //     this.layoutService.config().menuMode === 'static',
+            // 'layout-mobile-toggle':
+            //     this.layoutService.state.staticMenuMobileActive && this.layoutService.state.staticMenuToggle &&
+            //     this.layoutService.config().menuMode === 'static',
             'p-input-filled':
                 this.layoutService.config().inputStyle === 'filled',
             'p-ripple-disabled': !this.layoutService.config().ripple,

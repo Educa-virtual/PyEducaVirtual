e979c1f060fb206f68a446a322d6d3014bff82cb
DREW <jhoand60@gmail.com>
2024-11-12 11:40:09 -0500

    fix(ForoRoom): update error

--- src/app/servicios/general.service.ts
+++ src/app/servicios/general.service.ts
@@ -20,7 +20,7 @@ export class GeneralService {
         }
         return this.url
     }
-    getGralP    refix(data) {
+    getGralPrefix(data) {
         switch (data.petition) {
             case 'post':
                 this.url = this.http.post(

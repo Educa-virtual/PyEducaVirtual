ac87b2a724bcf6ecdbb1a09d0942a16167df50e3
DREW <jhoand60@gmail.com>
2024-11-27 16:00:08 -0500

    fix(RecoverPassword): new design

--- src/app/shared/recover-password/recover-password.component.ts
+++ src/app/shared/recover-password/recover-password.component.ts
@@ -1,11 +1,39 @@
 import { PrimengModule } from '@/app/primeng.module'
-import { Component } from '@angular/core'
+import {
+    Component,
+    EventEmitter,
+    Input,
+    Output,
+    OnChanges,
+} from '@angular/core'
+import { ModalPrimengComponent } from '../modal-primeng/modal-primeng.component'
 
 @Component({
     selector: 'app-recover-password',
     standalone: true,
-    imports: [PrimengModule],
+    imports: [PrimengModule, ModalPrimengComponent],
     templateUrl: './recover-password.component.html',
     styleUrl: './recover-password.component.scss',
 })
-export class RecoverPasswordComponent {}
+export class RecoverPasswordComponent implements OnChanges {
+    @Output() accionBtnItem = new EventEmitter()
+
+    @Input() showModal: boolean = true
+
+    formulario: number = 1
+    ngOnChanges(changes) {
+        if (changes.showModal?.currentValue) {
+            this.showModal = changes.showModal.currentValue
+        }
+    }
+    accionBtn(elemento): void {
+        const { accion } = elemento
+        const { item } = elemento
+
+        switch (accion) {
+            case 'close-modal':
+                this.accionBtnItem.emit({ accion, item })
+                break
+        }
+    }
+}

7d98d161a867be7ef284fa652d1fb7a228717837
DREW <jhoand60@gmail.com>
2024-12-02 04:59:32 -0500

    fix: Notificaciones - add portafolio

--- src/app/layout/toolbar/app.topbar.component.ts
+++ src/app/layout/toolbar/app.topbar.component.ts
@@ -1,16 +1,17 @@
-import { Component, ElementRef, ViewChild, OnInit } from '@angular/core'
+import { Component, ElementRef, ViewChild, OnInit, inject } from '@angular/core'
 import { LayoutService } from '../service/app.layout.service'
 import { LocalStoreService } from '../../servicios/local-store.service'
 import { TokenStorageService } from '../../servicios/token.service'
 import { Router, RouterLink } from '@angular/router'
 import { DropdownModule } from 'primeng/dropdown'
 import { FormsModule } from '@angular/forms'
-import { NgClass } from '@angular/common'
 import { MenuModule } from 'primeng/menu'
 import { RolesDropdownComponent } from './roles-dropdown/roles-dropdown.component'
 import { AnioEscolarComponent } from './roles-dropdown/anio-escolar/anio-escolar.component'
 import { PrimengModule } from '@/app/primeng.module'
 import { UserAccountComponent } from './roles-dropdown/user-account/user-account.component'
+import { GeneralService } from '@/app/servicios/general.service'
+import { MessageService } from 'primeng/api'
 
 @Component({
     selector: 'app-topbar',
@@ -20,7 +21,6 @@ import { UserAccountComponent } from './roles-dropdown/user-account/user-account
         RouterLink,
         DropdownModule,
         FormsModule,
-        NgClass,
         MenuModule,
         RolesDropdownComponent,
         AnioEscolarComponent,
@@ -30,6 +30,8 @@ import { UserAccountComponent } from './roles-dropdown/user-account/user-account
     styleUrl: './app.topbar.component.scss',
 })
 export class AppTopBarComponent implements OnInit {
+    private _GeneralService = inject(GeneralService)
+    private _MessageService = inject(MessageService)
     years = []
     selectedYear: string
 
@@ -43,6 +45,23 @@ export class AppTopBarComponent implements OnInit {
     // @ViewChild('topbarmenubutton') topbarMenuButton!: ElementRef
     @ViewChild('topbarmenu') menu!: ElementRef
 
+    items = [
+        {
+            label: 'Notificaciones',
+            items: [
+                {
+                    label: 'Julio Salazar creó la tarea "Descubrimiento de América ..."',
+                    icon: 'pi pi-book',
+                    sublabel: 'Hace 1 día',
+                },
+                {
+                    label: 'Roberto comentó la publicación de Julio ...',
+                    icon: 'pi pi-book',
+                    sublabel: 'Hace 1 mes',
+                },
+            ],
+        },
+    ]
     constructor(
         public layoutService: LayoutService,
         private store: LocalStoreService,
@@ -59,7 +78,6 @@ export class AppTopBarComponent implements OnInit {
 
         const perfil = this.store.getItem('dremoPerfil')
         this.perfiles = user.perfiles
-        console.log(this.perfiles)
         const perfil_data = {
             iPerfilId: 0,
             cPerfilNombre: '-',
@@ -70,6 +88,13 @@ export class AppTopBarComponent implements OnInit {
         const modulo = this.store.getItem('dremoModulo')
         this.modulos = user.modulos
         this.selectedModulo = modulo ? modulo.iModuloId : null
+
+        if (user.iDocenteId) {
+            this.notificacionDocente(user.iDocenteId)
+        }
+        if (user.iEstudianteId) {
+            this.notificacionEstudiante(user.iEstudianteId)
+        }
     }
 
     changeModulo(value) {
@@ -101,7 +126,6 @@ export class AppTopBarComponent implements OnInit {
     }
 
     actionTopBar(elemento): void {
-        console.log(elemento)
         const { accion } = elemento
         const { item } = elemento
 
@@ -116,24 +140,52 @@ export class AppTopBarComponent implements OnInit {
             case 'modulo':
                 this.changeModulo(item)
                 break
+            case 'notificacion_docente':
+                console.log(item)
+                break
+            case 'notificacion_estudiante':
+                console.log(item)
+                break
         }
     }
-
-    items = [
-        {
-            label: 'Notificaciones',
-            items: [
-                {
-                    label: 'Julio Salazar creó la tarea "Descubrimiento de América ..."',
-                    icon: 'pi pi-book',
-                    sublabel: 'Hace 1 día',
-                },
-                {
-                    label: 'Roberto comentó la publicación de Julio ...',
-                    icon: 'pi pi-book',
-                    sublabel: 'Hace 1 mes',
-                },
-            ],
-        },
-    ]
+    notificacionDocente(iDocenteId) {
+        const params = {
+            petition: 'post',
+            group: 'aula-virtual',
+            prefix: 'notificacion_docente',
+            ruta: 'mostrar_notificacion',
+            data: {
+                iDocenteId: iDocenteId,
+            },
+        }
+        this.getInformation(params, params.ruta + '-' + params.prefix)
+    }
+    notificacionEstudiante(iEstudianteId) {
+        const params = {
+            petition: 'post',
+            group: 'aula-virtual',
+            prefix: 'notificacion_estudiante',
+            ruta: 'mostrar_notificacion',
+            data: {
+                iEstudianteId: iEstudianteId,
+            },
+        }
+        this.getInformation(params, params.prefix)
+    }
+    getInformation(params, accion) {
+        this._GeneralService.getGralPrefix(params).subscribe({
+            next: (response) => {
+                this.actionTopBar({ accion, item: response?.data })
+            },
+            complete: () => {},
+            error: (error) => {
+                console.log(error)
+                this._MessageService.add({
+                    severity: 'error',
+                    summary: 'Error',
+                    detail: error,
+                })
+            },
+        })
+    }
 }
--- src/app/sistema/docente/portafolio/portafolio.component.ts
+++ src/app/sistema/docente/portafolio/portafolio.component.ts
@@ -1,5 +1,12 @@
 import { PrimengModule } from '@/app/primeng.module'
-import { Component } from '@angular/core'
+import { ConstantesService } from '@/app/servicios/constantes.service'
+import { GeneralService } from '@/app/servicios/general.service'
+import { LocalStoreService } from '@/app/servicios/local-store.service'
+import { environment } from '@/environments/environment'
+import { HttpClient } from '@angular/common/http'
+import { Component, inject, OnInit } from '@angular/core'
+import { MessageService } from 'primeng/api'
+import { catchError, map, throwError } from 'rxjs'
 
 @Component({
     selector: 'app-portafolio',
@@ -8,19 +15,206 @@ import { Component } from '@angular/core'
     templateUrl: './portafolio.component.html',
     styleUrl: './portafolio.component.scss',
 })
-export class PortafolioComponent {
-    cursos = [
-        {
-            cNombreCurso: 'Comunicación',
-        },
-        {
-            cNombreCurso: 'Matemática',
-        },
-        {
-            cNombreCurso: 'Educación Religiosa',
-        },
-        {
-            cNombreCurso: 'Ciencia y Tecnología',
-        },
-    ]
+export class PortafolioComponent implements OnInit {
+    private _ConstantesService = inject(ConstantesService)
+    private _GeneralService = inject(GeneralService)
+    private _MessageService = inject(MessageService)
+    private http = inject(HttpClient)
+    private _LocalStoreService = inject(LocalStoreService)
+
+    backend = environment.backend
+    private backendApi = environment.backendApi
+
+    cPortafolioFichaPermanencia
+    cPortafolioPerfilEgreso
+    cPortafolioPlanEstudios
+    cPortafolioItinerario = []
+    cPortafolioProgramaCurricular
+    cPortafolioFichasDidacticas
+    cPortafolioSesionesAprendizaje
+
+    cursos = []
+    ngOnInit() {
+        this.obtenerPortafolios()
+        this.obtenerCuadernosCampo()
+    }
+    getInformation(params, accion) {
+        this._GeneralService.getGralPrefix(params).subscribe({
+            next: (response) => {
+                this.accionBtnItem({ accion, item: response?.data })
+            },
+            complete: () => {},
+            error: (error) => {
+                this._MessageService.add({
+                    severity: 'error',
+                    summary: 'Error',
+                    detail: error,
+                })
+            },
+        })
+    }
+
+    accionBtnItem(elemento): void {
+        const { accion } = elemento
+        const { item } = elemento
+        switch (accion) {
+            case 'docente-obtenerPortafolios':
+                item.length
+                    ? (this.cPortafolioItinerario = item[0][
+                          'cPortafolioItinerario'
+                      ]
+                          ? JSON.parse(item[0]['cPortafolioItinerario'])
+                          : [])
+                    : []
+                break
+            case 'docente-obtenerCuadernosCampo':
+                this.cursos = item
+                this.cursos.forEach(
+                    (item) =>
+                        (item.cCuadernoUrl = item.cCuadernoUrl
+                            ? JSON.parse(item.cCuadernoUrl)
+                            : item.cCuadernoUrl)
+                )
+                break
+            case 'docente-guardarItinerario':
+                this.obtenerPortafolios()
+                break
+            case 'docente-guardarFichas':
+                this.obtenerCuadernosCampo()
+                break
+        }
+    }
+    obtenerPortafolios() {
+        const params = {
+            petition: 'post',
+            group: 'docente',
+            prefix: 'portafolios',
+            ruta: 'obtenerPortafolios',
+            data: {
+                iDocenteId: this._ConstantesService.iDocenteId,
+                iYAcadId: this._ConstantesService.iYAcadId,
+            },
+            params: { skipSuccessMessage: true },
+        }
+        this.getInformation(params, params.group + '-' + params.ruta)
+    }
+
+    obtenerCuadernosCampo() {
+        const year = this._LocalStoreService.getItem('dremoYear')
+        const params = {
+            petition: 'post',
+            group: 'docente',
+            prefix: 'cuadernos-campo',
+            ruta: 'obtenerCuadernosCampo',
+            data: {
+                iPersId: this._ConstantesService.iPersId,
+                iYearId: year,
+                iSemAcadId: null,
+                iIieeId: null,
+            },
+            params: { skipSuccessMessage: true },
+        }
+        this.getInformation(params, params.group + '-' + params.ruta)
+    }
+
+    guardarItinerario() {
+        const params = {
+            petition: 'post',
+            group: 'docente',
+            prefix: 'portafolios',
+            ruta: 'guardarItinerario',
+            data: {
+                iDocenteId: this._ConstantesService.iDocenteId,
+                iYAcadId: this._ConstantesService.iYAcadId,
+                cPortafolioItinerario: JSON.stringify(
+                    this.cPortafolioItinerario
+                ),
+            },
+            params: { skipSuccessMessage: true },
+        }
+        this.getInformation(params, params.group + '-' + params.ruta)
+    }
+
+    guardarFichas(item) {
+        const params = {
+            petition: 'post',
+            group: 'docente',
+            prefix: 'cuadernos-campo',
+            ruta: 'guardarFichas',
+            data: {
+                iSilaboId: item.iSilaboId,
+                cCuadernoUrl: item.cCuadernoUrl.length
+                    ? JSON.stringify(item.cCuadernoUrl)
+                    : [],
+            },
+            params: { skipSuccessMessage: true },
+        }
+        this.getInformation(params, params.group + '-' + params.ruta)
+    }
+
+    async onUploadChange(evt: any, tipo: any, item: any) {
+        const file = evt.target.files[0]
+
+        if (file) {
+            const dataFile = await this.objectToFormData({
+                file: file,
+                nameFile: tipo,
+            })
+            this.http
+                .post(
+                    `${this.backendApi}/general/subir-archivo?` +
+                        'skipSuccessMessage=true',
+                    dataFile
+                )
+                .pipe(
+                    map((event: any) => {
+                        if (event.validated) {
+                            switch (tipo) {
+                                case 'itinerario':
+                                    this.cPortafolioItinerario = []
+                                    this.cPortafolioItinerario.push({
+                                        name: file.name,
+                                        ruta: event.data,
+                                    })
+                                    this.guardarItinerario()
+                                    break
+                                case 'fichas-aprendizaje':
+                                    //1:fichas-aprendizaje
+                                    //2:cuadernos-campo
+                                    console.log(item.cCuadernoUrl)
+                                    item.cCuadernoUrl = []
+                                    item.cCuadernoUrl.push({
+                                        typePortafolio: 1,
+                                        name: file.name,
+                                        ruta: event.data,
+                                    })
+                                    this.guardarFichas(item)
+                                    break
+                            }
+                        }
+                    }),
+                    catchError((err: any) => {
+                        return throwError(err.message)
+                    })
+                )
+                .toPromise()
+        }
+    }
+
+    objectToFormData(obj: any) {
+        const formData = new FormData()
+        Object.keys(obj).forEach((key) => {
+            if (obj[key] !== '') {
+                formData.append(key, obj[key])
+            }
+        })
+
+        return formData
+    }
+
+    openLink(item) {
+        if (!item) return
+        const ruta = environment.backend + '/' + item
+        window.open(ruta, '_blank')
+    }
 }

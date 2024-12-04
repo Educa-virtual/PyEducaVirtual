7bfaa1e0f9a7e2d877930d8fbca7f0ea0bc4cee7
DREW <jhoand60@gmail.com>
2024-12-04 16:33:53 -0500

    fix(EvaluacionEstudiante): mostrar pdf encabezado

--- src/app/sistema/aula-virtual/sub-modulos/actividades/actividad-evaluacion/evaluacion-estudiantes/evaluacion-estudiantes.component.ts
+++ src/app/sistema/aula-virtual/sub-modulos/actividades/actividad-evaluacion/evaluacion-estudiantes/evaluacion-estudiantes.component.ts
@@ -8,6 +8,8 @@ import { ConstantesService } from '@/app/servicios/constantes.service'
 import { GeneralService } from '@/app/servicios/general.service'
 import { ConfirmationService, MessageService } from 'primeng/api'
 import { ConfirmationModalService } from '@/app/shared/confirm-modal/confirmation-modal.service'
+import { NgxDocViewerModule } from 'ngx-doc-viewer'
+import { environment } from '@/environments/environment'
 
 @Component({
     selector: 'app-evaluacion-estudiantes',
@@ -18,6 +20,7 @@ import { ConfirmationModalService } from '@/app/shared/confirm-modal/confirmatio
         CommonModule,
         TimeComponent,
         PrimengModule,
+        NgxDocViewerModule,
     ],
     templateUrl: './evaluacion-estudiantes.component.html',
     styleUrl: './evaluacion-estudiantes.component.scss',
@@ -29,6 +32,7 @@ export class EvaluacionEstudiantesComponent implements OnChanges {
     private _ConfirmationModalService = inject(ConfirmationModalService)
     private _ConfirmationService = inject(ConfirmationService)
 
+    environment = environment.backend
     @Input() evaluacion
 
     iPreguntaId: number = 0

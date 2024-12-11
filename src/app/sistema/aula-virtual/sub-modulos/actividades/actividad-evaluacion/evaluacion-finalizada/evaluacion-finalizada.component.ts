2a16e8b711e554f15ddd210794e4227765d3b707
sandoval2024-prog <willyalfredo_123_2020@hotmail.com>
2024-12-11 11:52:20 -0500

    fix (actualizando evaluacion finalizada)

--- src/app/sistema/aula-virtual/sub-modulos/actividades/actividad-evaluacion/evaluacion-finalizada/evaluacion-finalizada.component.ts
+++ src/app/sistema/aula-virtual/sub-modulos/actividades/actividad-evaluacion/evaluacion-finalizada/evaluacion-finalizada.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core'
-//import { EmptySectionComponent } from "../../../../../../shared/components/empty-section/empty-section.component";
-//import { RecursosListaComponent } from "../../../../../../shared/components/recursos-lista/recursos-lista.component";
-
+import { InputGroupModule } from 'primeng/inputgroup'
+import { InputTextModule } from 'primeng/inputtext'
+import { EmptySectionComponent } from '../../../../../../shared/components/empty-section/empty-section.component'
+import { RecursosListaComponent } from '../../../../../../shared/components/recursos-lista/recursos-lista.component'
 @Component({
     standalone: true,
     selector: 'app-evaluacion-finalizada',
     templateUrl: './evaluacion-finalizada.component.html',
     styleUrls: ['./evaluacion-finalizada.component.css'],
-    imports: [],
+    imports: [
+        InputGroupModule,
+        InputTextModule,
+        EmptySectionComponent,
+        RecursosListaComponent,
+    ],
 })
 export class EvaluacionFinalizadaComponent implements OnInit {
     evaluacion: any

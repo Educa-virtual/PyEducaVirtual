706bdd8cd3bea0091b01ab5b70347cb4d4810bbd
ejhomz <ejhomz7@gmail.com>
2024-12-12 14:14:37 -0500

    component asignacion grados

--- src/app/sistema/gestion-institucional/gestion-institucional-routing.module.ts
+++ src/app/sistema/gestion-institucional/gestion-institucional-routing.module.ts
@@ -11,6 +11,7 @@ import { ConfigHoraDocenteComponent } from './config-grado-seccion/steps/config-
 import { ConfigAsignarGradoComponent } from './config-grado-seccion/steps/config-asignar-grado/config-asignar-grado.component'
 import { ConfigResumenComponent } from './config-grado-seccion/steps/config-resumen/config-resumen.component'
 import { ConfigFechasComponent } from './config-fechas/config-fechas.component'
+import { IesPersonalComponent } from './ies-personal/ies-personal.component'
 
 const routes: Routes = [
     { path: 'calendarioAcademico', component: CalendarioAcademicoComponent },
@@ -19,11 +20,12 @@ const routes: Routes = [
     { path: 'ambiente', component: ConfigAmbienteComponent },
     { path: 'grado', component: ConfigGradoComponent },
     { path: 'seccion', component: ConfigSeccionComponent },
-    { path: 'plane-estudio', component: ConfigPlanEstudiosComponent },
+    { path: 'plan-estudio', component: ConfigPlanEstudiosComponent },
     { path: 'hora-docente', component: ConfigHoraDocenteComponent },
     { path: 'asignar-grado', component: ConfigAsignarGradoComponent },
     { path: 'resumen', component: ConfigResumenComponent },
     { path: 'fechas', component: ConfigFechasComponent },
+    { path: 'IesPersonal', component: IesPersonalComponent },
 ]
 
 @NgModule({

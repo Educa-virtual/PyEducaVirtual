51f11a0b2a87d5cf574d8df7666042b950ba2498
carlos alberto chura flores <chura.f5@gmail.com>
2025-02-05 11:53:53 -0500

    Se agrego el componente de reporte en el modulo de administrador

---
+++ src/app/sistema/administrador/reporte/reporte.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-reporte',
+    standalone: true,
+    imports: [],
+    templateUrl: './reporte.component.html',
+    styleUrl: './reporte.component.scss',
+})
+export class ReporteComponent {}

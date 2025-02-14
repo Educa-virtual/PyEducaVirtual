69b991c00ec47549a065a261833d06cacfbc5d58
Mitwar99 <mitwarhuacan@hotmail.com>
2025-02-14 17:12:56 -0500

    Avance de componente reutilizable

--- src/app/shared/container-preguntas/container-preguntas.component.ts
+++ src/app/shared/container-preguntas/container-preguntas.component.ts
@@ -1,12 +1,18 @@
+import { PrimengModule } from '@/app/primeng.module'
 import { Component, OnInit, Input } from '@angular/core'
 
 @Component({
     selector: 'app-container-preguntas',
+    standalone: true,
     templateUrl: './container-preguntas.component.html',
     styleUrls: ['./container-preguntas.component.scss'],
+    imports: [PrimengModule],
 })
 export class ContainerPreguntasComponent implements OnInit {
-    @Input() nombre: string
+    @Input() pregunta: string = ''
+    @Input() alternativas: string[] = []
+
+    seleccion: string | null = null
 
     constructor() {
         console.log('ff')
@@ -14,5 +20,14 @@ export class ContainerPreguntasComponent implements OnInit {
 
     ngOnInit() {
         console.log('ff')
+    }
+    // meto de al seleccionar una opción
+    seleccionarOpcion(opcion: string) {
+        this.seleccion = opcion
+        console.log('Seleccionaste', opcion)
+    }
+    // como convertir el (id) en letras y poder listar
+    getLetra(index: number): string {
+        return String.fromCharCode(65 + index) // Convierte 0 → A, 1 → B, 2 → C, etc.
     }
 }

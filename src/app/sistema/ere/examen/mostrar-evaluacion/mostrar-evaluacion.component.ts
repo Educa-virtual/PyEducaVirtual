132cd3cd9fe9f5162331fb9c246a3700e35ecca8
Mitwar99 <mitwarhuacan@hotmail.com>
2025-03-04 14:59:51 -0500

    redir examen

---
+++ src/app/sistema/ere/examen/mostrar-evaluacion/mostrar-evaluacion.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core'
+import { PrimengModule } from '@/app/primeng.module'
+import { ProgressBarModule } from 'primeng/progressbar'
+import { RadioButtonModule } from 'primeng/radiobutton'
+
+@Component({
+    selector: 'app-mostrar-evaluacion',
+    standalone: true,
+    templateUrl: './mostrar-evaluacion.component.html',
+    styleUrls: ['./mostrar-evaluacion.component.scss'],
+    imports: [PrimengModule, ProgressBarModule, RadioButtonModule],
+})
+export class MostrarEvaluacionComponent implements OnInit {
+    pregunta = [
+        {
+            title: 'Pregunta N°1: Si tienes 48 caramelos y regalas 23, ¿Cuántos te quedan?',
+            tipo: 'unica',
+            opciones: ['Opción A', 'Opción B', 'Opción C'],
+        },
+        {
+            title: 'Pregunta N°2: Si en cada caja hay 5 lápices y tienes 4 cajas, ¿cuántos lápices tienes en total?  ',
+            tipo: 'múltiple',
+            enunciado:
+                'Selecciona las respuestas correctas según el siguiente enunciado:',
+            opciones: ['Opción 1', 'Opción 2', 'Opción 3', 'Opción 4'],
+        },
+        { title: 'Title 3', content: 'Content 3' },
+    ]
+
+    subPreguntas = [
+        { title: 'pregunta 1' },
+        { title: 'pregunta 2' },
+        { title: 'pregunta 3' },
+    ]
+    subAlternativas: string[] = ['23', 'ya 24', '30?', 'N/A']
+    alternativas: string[] = ['no se tu dime', 'ya 24', '30?', 'N/A']
+
+    seleccion: string | null = null
+
+    constructor() {}
+
+    ngOnInit() {
+        console.log('mostrar nada')
+    }
+    // meto de al seleccionar una opción
+    seleccionarOpcion(opcion: string) {
+        this.seleccion = opcion
+        console.log('Seleccionaste', opcion)
+    }
+    // como convertir el (id) en letras y poder listar
+    getLetra(index: number): string {
+        return String.fromCharCode(65 + index) // Convierte 0 → A, 1 → B, 2 → C, etc.
+    }
+}
--- src/app/sistema/ere/examen/rendir-examen/rendir-examen.component.ts
+++ src/app/sistema/ere/examen/rendir-examen/rendir-examen.component.ts
@@ -1,18 +1,53 @@
 import { PrimengModule } from '@/app/primeng.module'
+import {
+    matAccessTime,
+    matCalendarMonth,
+    matHideSource,
+    matListAlt,
+    matMessage,
+    matRule,
+    matStar,
+} from '@ng-icons/material-icons/baseline'
 import { ContainerPageComponent } from '@/app/shared/container-page/container-page.component'
 import { Component, OnInit } from '@angular/core'
+import { provideIcons } from '@ng-icons/core'
+
+import { IconComponent } from '@/app/shared/icon/icon.component'
+import { MostrarEvaluacionComponent } from '../mostrar-evaluacion/mostrar-evaluacion.component'
 
 @Component({
     selector: 'app-rendir-examen',
     standalone: true,
     templateUrl: './rendir-examen.component.html',
     styleUrls: ['./rendir-examen.component.scss'],
-    imports: [PrimengModule, ContainerPageComponent],
+    imports: [
+        PrimengModule,
+        ContainerPageComponent,
+        IconComponent,
+        MostrarEvaluacionComponent,
+    ],
+    providers: [
+        provideIcons({
+            matHideSource,
+            matCalendarMonth,
+            matMessage,
+            matStar,
+            matRule,
+            matListAlt,
+            matAccessTime,
+        }),
+    ],
 })
 export class RendirExamenComponent implements OnInit {
     constructor() {}
 
     ngOnInit() {
         console.log('hola')
+    }
+
+    // para ocultadar la interfaz de inicio.
+    mostrarOtraInterfaz = false
+    cambiarInterfaz() {
+        this.mostrarOtraInterfaz = !this.mostrarOtraInterfaz
     }
 }

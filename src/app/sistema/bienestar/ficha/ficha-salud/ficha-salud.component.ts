e5649fc51d750bb3fb447ccaba2b0c6e31427323
Paolo Villanueva <nvillanueva@gmail.com>
2025-03-05 10:51:43 -0500

    Add seccion salud ficha bienestar

---
+++ src/app/sistema/bienestar/ficha/ficha-salud/ficha-salud.component.ts
@@ -0,0 +1,78 @@
+import { PrimengModule } from '@/app/primeng.module'
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+
+@Component({
+    selector: 'app-ficha-salud',
+    standalone: true,
+    imports: [PrimengModule],
+    templateUrl: './ficha-salud.component.html',
+    styleUrl: './ficha-salud.component.scss',
+})
+export class FichaSaludComponent implements OnInit {
+    formSalud: FormGroup
+    dolencias: Array<object>
+    visibleInput: Array<boolean>
+    visibleAlergiaInput: Array<boolean>
+
+    constructor(private fb: FormBuilder) {}
+
+    ngOnInit(): void {
+        this.visibleInput = Array(7).fill(false)
+        this.visibleAlergiaInput = Array(2).fill(false)
+
+        this.dolencias = [
+            { id: 1, nombre: 'Asma' },
+            { id: 2, nombre: 'Diabetes' },
+            { id: 3, nombre: 'Epilepsia' },
+            { id: 4, nombre: 'Artritis' },
+            { id: 5, nombre: 'Reumatismo' },
+            { id: 6, nombre: 'Hipertensión' },
+            { id: 7, nombre: 'Estrés' },
+        ]
+
+        try {
+            this.formSalud = this.fb.group({
+                iFichaDGId: [null, Validators.required],
+                bFichaDGAlergiaMedicamentos: [false],
+                cFichaDGAlergiaMedicamentos: [null],
+                bFichaDGAlergiaOtros: [false],
+                cFichaDGAlergiaOtros: [null],
+            })
+        } catch (error) {
+            console.log(error, 'error inicializando formulario')
+        }
+    }
+
+    handleSwitchChange(event: any, index: number) {
+        if (event?.checked === undefined) {
+            this.visibleInput[index] = false
+            return null
+        }
+        if (event.checked === true) {
+            this.visibleInput[index] = true
+        } else {
+            this.visibleInput[index] = false
+        }
+    }
+
+    handleSwitchAlergiaChange(event: any, index: number) {
+        if (event?.checked === undefined) {
+            this.visibleAlergiaInput[index] = false
+            return null
+        }
+        if (event.checked === true) {
+            this.visibleAlergiaInput[index] = true
+        } else {
+            this.visibleAlergiaInput[index] = false
+        }
+    }
+
+    guardarDatos() {
+        console.log(this.formSalud.value, 'guardando datos')
+    }
+
+    actualizarDatos() {
+        console.log(this.formSalud.value, 'actualizando datos')
+    }
+}

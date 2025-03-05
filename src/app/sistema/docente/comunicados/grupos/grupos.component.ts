d4f8951367767cc2c9774b4d7239a5b640744cab
carlos alberto chura flores <chura.f5@gmail.com>
2025-03-05 18:24:50 -0500

    Se agrego una tabla para agregar miembros

--- src/app/sistema/docente/comunicados/grupos/grupos.component.ts
+++ src/app/sistema/docente/comunicados/grupos/grupos.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core'
+import { Component, inject } from '@angular/core'
 import { PrimengModule } from '@/app/primeng.module'
 import { FormsModule } from '@angular/forms'
 import { TablePrimengComponent } from '@/app/shared/table-primeng/table-primeng.component'
+import { ConstantesService } from '@/app/servicios/constantes.service'
+import { GeneralService } from '@/app/servicios/general.service'
+import { MessageService } from 'primeng/api'
 @Component({
     selector: 'app-grupos',
     standalone: true,
@@ -10,13 +13,21 @@ import { TablePrimengComponent } from '@/app/shared/table-primeng/table-primeng.
     styleUrl: './grupos.component.scss',
 })
 export class GruposComponent {
-    visible = false
+    private GeneralService = inject(GeneralService)
+    constructor(
+        private ConstantesService: ConstantesService,
+        private messageService: MessageService
+    ) {
+        this.iSedeId = this.ConstantesService.iSedeId
+    }
+    iSedeId = ''
+    visible = true
     grupo: string
     columnas = [
         {
             type: 'item-checkbox',
             width: '1rem',
-            field: 'cItem',
+            field: 'iSeleccionado',
             header: 'Elegir',
             text_header: 'center',
             text: 'center',
@@ -30,9 +41,17 @@ export class GruposComponent {
             text: 'center',
         },
         {
-            type: 'item',
+            type: 'text',
             width: '1rem',
-            field: 'cItem',
+            field: 'cPersDocumento',
+            header: 'Documento',
+            text_header: 'center',
+            text: 'center',
+        },
+        {
+            type: 'text',
+            width: '1rem',
+            field: 'completos',
             header: 'Apellidos y Nombres',
             text_header: 'center',
             text: 'center',
@@ -46,31 +65,69 @@ export class GruposComponent {
             text: 'center',
         },
         {
-            type: 'item',
+            type: 'text',
             width: '1rem',
-            field: 'cItem',
+            field: 'cPersDomicilio',
             header: 'Direccion Domiciliaria',
-            text_header: 'center',
-            text: 'center',
-        },
-        {
-            type: 'item',
-            width: '1rem',
-            field: 'cItem',
-            header: 'Acciones',
             text_header: 'center',
             text: 'center',
         },
     ]
     data = []
     cities = [
-        { grupo: 'Padres', codigo: 1 },
         { grupo: 'Docentes', codigo: 2 },
-        { grupo: 'Estudiantes', codigo: 3 },
+        { grupo: 'Estudiantes', codigo: 1 },
         // { grupo: 'Personal Ugel', codigo: 4 },
     ]
 
     mostrarMdoal() {
         this.visible = true
+    }
+
+    mostrarDocentes() {
+        if (this.grupo) {
+            this.obtenerDocentes(2)
+        } else {
+            this.messageService.add({
+                severity: 'error',
+                summary: 'Advertencia',
+                detail: 'Debe Seleccionar Tipo de Persona',
+            })
+        }
+    }
+
+    obtenerDocentes(opcion: number) {
+        const params = {
+            petition: 'post',
+            group: 'com',
+            prefix: 'miembros',
+            ruta: 'obtener_miembros',
+            data: {
+                opcion: opcion,
+                iSedeId: this.iSedeId,
+            },
+        }
+        this.getInformation(params, 'obtenerDocentes')
+    }
+    getInformation(params, accion) {
+        this.GeneralService.getGralPrefix(params).subscribe({
+            next: (response: any) => {
+                this.accionBtnItem({ accion, item: response?.data })
+            },
+            complete: () => {},
+        })
+    }
+    accionBtnItem(event): void {
+        const { accion } = event
+        const { item } = event
+
+        switch (accion) {
+            case 'obtenerDocentes':
+                this.data = item
+                break
+            case 'setearDataxiSeleccionado':
+                console.log(item)
+                break
+        }
     }
 }

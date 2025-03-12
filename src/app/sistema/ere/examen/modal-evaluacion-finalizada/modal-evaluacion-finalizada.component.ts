5217d2cd2ab247e4b0b2d5b6fab71134d4b09637
DREW <jhoand60@gmail.com>
2025-03-12 07:47:53 -0500

    fix(ModalEvaluacionFinalizada): update styles y agregar formulario encuesta

--- src/app/sistema/ere/examen/modal-evaluacion-finalizada/modal-evaluacion-finalizada.component.ts
+++ src/app/sistema/ere/examen/modal-evaluacion-finalizada/modal-evaluacion-finalizada.component.ts
@@ -10,4 +10,11 @@ import { Component } from '@angular/core'
 })
 export class ModalEvaluacionFinalizadaComponent {
     visible: boolean = true
+
+    irEncuesta() {
+        window.open(
+            'https://docs.google.com/forms/d/e/1FAIpQLSewCXH33oUdW1u1ydx2hsyxI_dBWFnMLXtqaFqLanG1j5Fj9Q/viewform',
+            '_blank'
+        )
+    }
 }

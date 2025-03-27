9fe0aabd4e8b7da7b88925103a0f89c00b6054fd
DREW <jhoand60@gmail.com>
2025-03-27 13:16:23 -0500

    fix: enviar iIieeId para verificar rendir examen al estudiante

--- src/app/sistema/ere/examen/mostrar-evaluacion/mostrar-evaluacion.component.ts
+++ src/app/sistema/ere/examen/mostrar-evaluacion/mostrar-evaluacion.component.ts
@@ -24,6 +24,7 @@ import { MessageService } from 'primeng/api'
 import { Router } from '@angular/router'
 import { TimeComponent } from '../../../../shared/time/time.component'
 import { LocalStoreService } from '@/app/servicios/local-store.service'
+import { ConstantesService } from '@/app/servicios/constantes.service'
 
 @Component({
     selector: 'app-mostrar-evaluacion',
@@ -53,6 +54,7 @@ export class MostrarEvaluacionComponent implements OnInit, AfterViewChecked {
     private _MessageService = inject(MessageService)
     private router = inject(Router)
     private _ChangeDetectorRef = inject(ChangeDetectorRef)
+    private _ConstantesService = inject(ConstantesService)
 
     constructor(private store: LocalStoreService) {}
 
@@ -76,28 +78,22 @@ export class MostrarEvaluacionComponent implements OnInit, AfterViewChecked {
             petition: 'post',
             group: 'ere',
             prefix: 'evaluacion',
-            ruta: 'handleCrudOperation',
+            ruta: 'obtenerEvaluacionxiEvaluacionIdxiCursoNivelGradIdxiIieeId',
             data: {
-                opcion: 'CONSULTARxiEvaluacionId',
+                opcion: 'CONSULTARxiEvaluacionIdxiCursoNivelGradIdxiIieeId',
                 iEvaluacionId: this.iEvaluacionId,
-                valorBusqueda: this.iCursoNivelGradId,
+                iCursoNivelGradId: this.iCursoNivelGradId,
+                iIieeId: this._ConstantesService.iIieeId,
             },
         }
         this.getInformation(params, params.data.opcion)
     }
 
     rendirExamen() {
-        const params = {
-            petition: 'post',
-            group: 'ere',
-            prefix: 'evaluacion',
-            ruta: 'handleCrudOperation',
-            data: {
-                opcion: 'CONSULTAR-ESTADOxiEvaluacionId',
-                iEvaluacionId: this.iEvaluacionId,
-            },
-        }
-        this.getInformation(params, params.data.opcion)
+        this.accionBtnItem({
+            accion: 'CONSULTAR-ESTADOxiEvaluacionId',
+            item: [],
+        })
     }
 
     getInformation(params, accion) {
@@ -121,7 +117,7 @@ export class MostrarEvaluacionComponent implements OnInit, AfterViewChecked {
         const { accion } = elemento
         const { item } = elemento
         switch (accion) {
-            case 'CONSULTARxiEvaluacionId':
+            case 'CONSULTARxiEvaluacionIdxiCursoNivelGradIdxiIieeId':
                 this.evaluacion = item.length ? item[0] : null
                 this.store.setItem('evaluacion', this.evaluacion)
                 break
@@ -129,13 +125,9 @@ export class MostrarEvaluacionComponent implements OnInit, AfterViewChecked {
                 if (this.bEstadoTiempo) {
                     return
                 }
-                item.length
-                    ? item[0]['iEstado']
-                        ? this.router.navigate([
-                              `ere/rendir-examen/${this.iEvaluacionId}/areas/${this.iCursoNivelGradId}/${this.cEvaluacionNombre}/${this.cCursoNombre}/${this.cGradoNombre}`,
-                          ])
-                        : null
-                    : null
+                this.router.navigate([
+                    `ere/rendir-examen/${this.iEvaluacionId}/areas/${this.iCursoNivelGradId}/${this.cEvaluacionNombre}/${this.cCursoNombre}/${this.cGradoNombre}`,
+                ])
                 break
             case 'tiempo-finalizado':
                 this.bEstadoTiempo = false

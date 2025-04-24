abf8561fe85d359f5c148bb3ee4b21045aa4b4e9
Mitwar99 <mitwarhuacan@hotmail.com>
2025-04-24 13:42:22 -0500

    Agregar componente de selección de imagen y su integración en el formulario de apertura de curso; implementar un carrusel para mostrar productos con opciones de respuesta.

--- src/app/shared/imagen-select/imagen-select.component.ts
+++ src/app/shared/imagen-select/imagen-select.component.ts
@@ -9,9 +9,85 @@ import { Component, OnInit } from '@angular/core'
     imports: [PrimengModule],
 })
 export class ImagenSelectComponent implements OnInit {
+    products = [
+        {
+            name: 'Capaciaciones',
+            image: 'assets/demo/images/product/bamboo-watch.jpg',
+            description: 'Product Description',
+            price: 65,
+            quantity: 1,
+            status: 'INSTOCK',
+        },
+        {
+            name: 'Especilizaciones',
+            image: 'assets/demo/images/product/black-watch.jpg',
+            description: 'Product Description',
+            price: 72,
+            quantity: 1,
+            status: 'INSTOCK',
+        },
+        {
+            name: 'Diplamados',
+            image: 'http://www.thebusinessofsports.com/wp-content/uploads/2010/10/facebook-icon-200x200.png',
+            description: 'Product Description',
+            price: 79,
+            quantity: 1,
+            status: 'INSTOCK',
+        },
+        {
+            name: 'Especilizaciones',
+            image: 'assets/demo/images/product/black-watch.jpg',
+            description: 'Product Description',
+            price: 72,
+            quantity: 1,
+            status: 'INSTOCK',
+        },
+        {
+            name: 'Diplamados',
+            image: 'http://www.thebusinessofsports.com/wp-content/uploads/2010/10/facebook-icon-200x200.png',
+            description: 'Product Description',
+            price: 79,
+            quantity: 1,
+            status: 'INSTOCK',
+        },
+    ]
+
+    responsiveOptions: any[] | undefined
+
     constructor() {}
 
     ngOnInit() {
-        console.log('gg')
+        // this.productService.getProductsSmall().then((products) => {
+        //     this.products = products;
+        // });
+
+        this.responsiveOptions = [
+            {
+                breakpoint: '1199px',
+                numVisible: 1,
+                numScroll: 1,
+            },
+            {
+                breakpoint: '991px',
+                numVisible: 2,
+                numScroll: 1,
+            },
+            {
+                breakpoint: '767px',
+                numVisible: 1,
+                numScroll: 1,
+            },
+        ]
     }
+
+    // getSeverity(status: string) {
+    //     switch (status) {
+    //         case 'INSTOCK':
+    //             return 'success';
+    //         case 'LOWSTOCK':
+    //             return 'warning';
+    //         case 'OUTOFSTOCK':
+    //             return 'danger';
+    //     }
+    // }
 }

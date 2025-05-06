c8fe3e70d33c5b7db8781ffaf27a66b59a195eab
Max Cantuta <maxcantutajor.24@gmail.com>
2025-05-06 14:00:33 -0500

    new mantenimeinto  usuario component

---
+++ src/app/sistema/gestion-institucional/new-mantenimiento-usuario/new-mantenimiento-usuario.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-new-mantenimiento-usuario',
+    standalone: true,
+    imports: [],
+    templateUrl: './new-mantenimiento-usuario.component.html',
+    styleUrl: './new-mantenimiento-usuario.component.scss',
+})
+export class NewMantenimientoUsuarioComponent {}

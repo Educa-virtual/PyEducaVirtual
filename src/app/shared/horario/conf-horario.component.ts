659f51321876e21a40c13aaeb646860049f55e4d
DREW <jhoand60@gmail.com>
2025-05-07 15:58:15 -0500

    fix: actualizar horario ie con su detalle
    Hazaña: agregar métodos para administrar los horarios de los cursos y actualizar los componentes de la interfaz de usuario
    
    - Implementado los métodos obtenerCursosDiasHorarios y guardarRemoverCursosDiasHorarios en GeneralService para interacciones API.
    - Conf-horario.component.html refactorizado para mejorar la interfaz de usuario para la selección y programación de cursos.
    - Se actualizó conf-horario.component.ts para manejar la selección de cursos y la gestión de horarios con lógica mejorada.
    - Se eliminaron el código y los comentarios no utilizados para mejorar la legibilidad y el mantenimiento.
    - Estilos ajustados en conf-horario.component.scss para una mejor retroalimentación visual sobre la selección de cursos.
    - Configuracion-horario.component.html y configuracion-horario.component.ts mejorados para mejorar la experiencia del usuario en la configuración del curso.

--- src/app/shared/horario/conf-horario.component.ts
+++ src/app/shared/horario/conf-horario.component.ts
@@ -2,14 +2,17 @@ import {
     Component,
     ElementRef,
     EventEmitter,
+    inject,
     Input,
     OnChanges,
     Output,
+    SimpleChanges,
     ViewChild,
 } from '@angular/core'
 import { PrimengModule } from '@/app/primeng.module'
 import { NgIf } from '@angular/common'
 import { NoDataComponent } from '../no-data/no-data.component'
+import { ConfirmationModalService } from '../confirm-modal/confirmation-modal.service'
 @Component({
     selector: 'app-conf-horario',
     standalone: true,
@@ -22,70 +25,59 @@ export class ConfHorariosComponent implements OnChanges {
 
     @Input() cursos = []
     @Input() dias = []
-    @Input() horas = []
     @Input() horario = []
+    @Input() bloques: number = 0
+
+    cursoSeleccionado
 
     @ViewChild('cursosContainer', { static: false })
     cursosContainer!: ElementRef
 
+    private _ConfirmationModalService = inject(ConfirmationModalService)
+
     scrollCursos(direction: number): void {
         const container = this.cursosContainer.nativeElement
-        const scrollAmount = 270 // ajusta según el ancho de tus tarjetas
+        const scrollAmount = 270
         container.scrollBy({
             left: scrollAmount * direction,
             behavior: 'smooth',
         })
     }
-    cursoSeleccionado
-    ngOnChanges(changes) {
-        if (changes.cursos.currentValue) {
-            this.cursos = changes.cursos.currentValue
+
+    ngOnChanges(changes: SimpleChanges) {
+        if (changes['cursos']) {
+            this.cursos = changes['cursos'].currentValue
         }
-        if (changes.dias.currentValue) {
-            this.dias = changes.dias.currentValue
+        if (changes['dias']) {
+            this.dias = changes['dias'].currentValue
         }
-        if (changes.horas.currentValue) {
-            this.horas = changes.horas.currentValue
+        if (changes['horario']) {
+            this.horario = changes['horario'].currentValue
         }
-        if (changes.horario.currentValue) {
-            this.horario = changes.horario.currentValue
+        if (changes['bloques']) {
+            this.bloques = changes['bloques'].currentValue
         }
     }
-
-    existeCursoHoraEnDia(iDiaId: number, iHorarioIeDetId: number): boolean {
-        return this.horario.some(
-            (hour) =>
-                hour.iDiaId === iDiaId &&
-                hour.iHorarioIeDetId === iHorarioIeDetId
-        )
+    getHorarioxiBloque(iBloque: number) {
+        return this.horario.filter((horario) => horario.iBloque === iBloque)
     }
 
-    obtenerNombreCursoHoraEnDia(
-        iDiaId: number,
-        iHorarioIeDetId: number
-    ): string {
-        const horario = this.horario.find(
-            (hour) =>
-                hour.iDiaId === iDiaId &&
-                hour.iHorarioIeDetId === iHorarioIeDetId
-        )
-        return horario ? horario.cCursoNombre : ''
-    }
-
-    agregarRemoverHorario(horario, dia, curso, accion) {
-        const item = {
-            iCursoId: 1,
-            cCursoNombre: curso.cCursoNombre,
-            iDiaId: dia.iDiaId,
-            cDiaNombre: dia.cDiaNombre,
-            iHorarioIeId: horario.iHorarioIeId,
-            inicio: horario.inicio,
-            fin: horario.fin,
-            cDocente: horario.cDocente,
-            iHorarioIeDetId: horario.iHorarioIeDetId,
+    agregarRemoverHorario(horario, accion) {
+        if (!this.cursoSeleccionado) {
+            this._ConfirmationModalService.openAlert({
+                header: '¡Atención!, debe seleccionar un curso',
+            })
+            return
         }
-
-        this.accionBtn(accion, item)
+        switch (accion) {
+            case 'agregar-horario':
+                horario.idDocCursoId = this.cursoSeleccionado.idDocCursoId
+                break
+            case 'remover-horario':
+                horario.idDocCursoId = null
+                break
+        }
+        this.accionBtn(accion, horario)
     }
     accionBtn(accion, item) {
         const data = {
@@ -95,230 +87,7 @@ export class ConfHorariosComponent implements OnChanges {
         this.accionBtnItem.emit(data)
     }
 
-    // addCurso(bloqueId, diaCurso: any) {
-    //     const curso = this.cursoSeleccionado
-
-    //     if (!curso || curso.horasAsignadas >= curso.horasDisponibles) {
-    //         console.warn('Curso sin horas disponibles')
-    //         return
-    //     }
-
-    //     // Actualizar bloquesHorarios
-    //     this.bloquesHorarios = this.bloquesHorarios.map((bloque) => {
-    //         if (bloque.id === bloqueId) {
-    //             return {
-    //                 ...bloque,
-    //                 [diaCurso]: curso,
-    //             }
-    //         }
-    //         return bloque
-    //     })
-
-    //     // Actualizar cursos
-    //     this.cursos = this.cursos.map((c) => {
-    //         if (c.id === curso.id) {
-    //             const actualizado = {
-    //                 ...c,
-    //                 horasAsignadas: c.horasAsignadas + 1,
-    //             }
-
-    //             // actualizar cursoSeleccionado también para mantenerlo en sincronía
-    //             this.cursoSeleccionado = actualizado
-
-    //             return actualizado
-    //         }
-    //         return c
-    //     })
-    // }
-
-    // removeCurso(bloqueId: number, diaCurso: string) {
-    //     // Encontrar el curso que está en ese bloque y día
-    //     const cursoRemovido = this.bloquesHorarios.find(
-    //         (b) => b.id === bloqueId
-    //     )?.[diaCurso]
-
-    //     if (!cursoRemovido || !cursoRemovido.id) {
-    //         console.warn('No hay curso asignado para remover.')
-    //         return
-    //     }
-
-    //     // Remover el curso del bloque
-    //     this.bloquesHorarios = this.bloquesHorarios.map((bloque) => {
-    //         if (bloque.id === bloqueId) {
-    //             // Puedes poner undefined o eliminar la clave si prefieres
-    //             const { [diaCurso]: _, ...resto } = bloque
-    //             return {
-    //                 ..._,
-    //                 ...resto,
-    //                 [diaCurso]: undefined,
-    //             }
-    //         }
-    //         return bloque
-    //     })
-
-    //     // Disminuir la cantidad de horas asignadas al curso removido
-    //     this.cursos = this.cursos.map((curso) => {
-    //         if (curso.id === cursoRemovido.id) {
-    //             const actualizado = {
-    //                 ...curso,
-    //                 horasAsignadas: curso.horasAsignadas - 1,
-    //             }
-
-    //             // Si justo estamos mostrando ese curso como seleccionado, actualiza también
-    //             if (this.cursoSeleccionado?.id === actualizado.id) {
-    //                 this.cursoSeleccionado = actualizado
-    //             }
-
-    //             return actualizado
-    //         }
-    //         return curso
-    //     })
-    // }
-
-    // isDay(bloque: any): boolean {
-    //     return (
-    //         typeof bloque === 'object' &&
-    //         bloque !== null &&
-    //         Object.keys(bloque).length > 0
-    //     )
-    // }
-
-    // isBloque(bloque: any): boolean {
-    //     return typeof bloque === 'string'
-    // }
-
-    bloquesHorarios: Array<{ id: number; Bloque: string; [dia: string]: any }> =
-        [
-            {
-                id: 1,
-                Bloque: '8:00 - 9:30',
-                Lun: { id: 1, curso: 'Matemáticas' },
-                Mar: {},
-                Mie: {},
-                Jue: {},
-                Vie: {},
-            },
-            {
-                id: 2,
-                Bloque: '9:45 - 11:15',
-                Lun: {},
-                Mar: {},
-                Mie: {},
-                Jue: {},
-                Vie: {},
-            },
-            {
-                id: 3,
-                Bloque: '11:30 - 13:00',
-                Lun: {},
-                Mar: {},
-                Mie: {},
-                Jue: {},
-                Vie: {},
-            },
-            {
-                id: 4,
-                Bloque: '14:30 - 16:00',
-                Lun: {},
-                Mar: {},
-                Mie: {},
-                Jue: {},
-                Vie: {},
-            },
-            {
-                id: 5,
-                Bloque: '16:30 - 18:00',
-                Lun: {},
-                Mar: {},
-                Mie: {},
-                Jue: {},
-                Vie: {},
-            },
-        ]
-
-    festividades
-    eventoFestividades
-    actividades
-    eventoActividad
-    curricula
-    eventoCurricular
-    calendarOptions
-    DURACION_BLOQUE: number = 1.5
-
-    cursosSeleccionados: number[] = []
-    asignaciones: Record<number, number> = {} // {bloqueId: cursoId}
-    dialogOpen: boolean = false
-    bloqueSeleccionado: number | null = null
-
-    // toggleCurso(cursoId: number): void {
-    //     const index = this.cursosSeleccionados.indexOf(cursoId)
-    //     if (index !== -1) {
-    //         this.cursosSeleccionados.splice(index, 1)
-    //     } else {
-    //         this.cursosSeleccionados.push(cursoId)
-    //     }
-    // }
-
-    // calcularHorasAsignadas(cursoId: number): number {
-    //     const cantidad = Object.values(this.asignaciones).filter(
-    //         (id) => id === cursoId
-    //     ).length
-    //     return cantidad * this.DURACION_BLOQUE
-    // }
-
-    // tieneHorasDisponibles(cursoId: number): boolean {
-    //     const curso = this.cursos.find((c) => c.id === cursoId)
-    //     if (!curso) return false
-    //     return this.calcularHorasAsignadas(cursoId) < curso.horasDisponibles
-    // }
-
-    // getBloqueId(diaIndex: number, horaIndex: number): number {
-    //     return diaIndex * this.horas.length + horaIndex + 1
-    // }
-
-    // getCursoEnBloque(bloqueId: number): any | undefined {
-    //     const cursoId = this.asignaciones[bloqueId]
-    //     return this.cursos.find((curso) => curso.id === cursoId)
-    // }
-
-    // asignarCursoABloque(bloqueId: number, cursoId: number): void {
-    //     if (this.tieneHorasDisponibles(cursoId)) {
-    //         this.asignaciones[bloqueId] = cursoId
-    //     }
-    // }
-
-    // eliminarAsignacion(bloqueId: number): void {
-    //     delete this.asignaciones[bloqueId]
-    // }
-
-    // abrirDialogo(bloqueId: number): void {
-    //     this.bloqueSeleccionado = bloqueId
-    //     this.dialogOpen = true
-    // }
-
-    // cerrarDialogo(): void {
-    //     this.dialogOpen = false
-    //     this.bloqueSeleccionado = null
-    // }
-
-    // calcularPorcentaje(cursoId: number): number {
-    //     const curso = this.cursos.find((c) => c.id === cursoId)
-    //     if (!curso) return 0
-
-    //     const asignadas = this.calcularHorasAsignadas(cursoId)
-    //     return (asignadas / curso.horasDisponibles) * 100
-    // }
-
-    // getCursoPorId(cursoId: number) {
-    //     return this.cursos.find((c) => c.id === cursoId)
-    // }
-
-    // onClickBloque(bloqueId: number, horaIndex: number) {
-    //     console.log('Bloque:', bloqueId, 'Hora Index:', horaIndex)
-    // }
-
-    // asignarCursoDesdeDialog(cursoId) {
-    //     // lógica para asignar curso desde el diálogo
-    //     console.log('Asignando curso desde diálogo...' + cursoId)
-    // }
+    getRango(n: number): number[] {
+        return Array.from({ length: n }, (_, i) => i + 1)
+    }
 }
--- src/app/sistema/gestion-institucional/horario/configuracion-horario/configuracion-horario.component.ts
+++ src/app/sistema/gestion-institucional/horario/configuracion-horario/configuracion-horario.component.ts
@@ -1,19 +1,19 @@
 import { Component, inject, OnInit } from '@angular/core'
 import { GeneralService } from '@/app/servicios/general.service'
-import { MessageService } from 'primeng/api'
 import { PrimengModule } from '@/app/primeng.module'
 
-import { ConfirmationModalService } from '@/app/shared/confirm-modal/confirmation-modal.service'
 import { ConfHorariosComponent } from '@/app/shared/horario/conf-horario.component'
-import { ApiService } from '@/app/servicios/api.service'
 import { ToolbarPrimengComponent } from '../../../../shared/toolbar-primeng/toolbar-primeng.component'
 import { ConstantesService } from '@/app/servicios/constantes.service'
+import { MessageService } from 'primeng/api'
 interface Curso {
     iCursoId: number
     cCursoNombre: string
-    cDocente: string
-    horasDisponibles: number
-    horasAsignadas: number
+    nombre_completo: string
+    nombre_corto: string
+    nCursoTotalHoras: number
+    iDocHoraAsignada: number
+    horarios: any[]
 }
 @Component({
     selector: 'app-configuracion-horario',
@@ -29,220 +29,29 @@ export class ConfiguracionHorarioComponent implements OnInit {
     iGradoId: string = ''
     iSeccionId: string = ''
 
-    cursos: Curso[] = [
-        {
-            iCursoId: 1,
-            cCursoNombre: 'Matemáticas',
-            cDocente: 'Dr. García',
-            horasDisponibles: 6,
-            horasAsignadas: 1,
-        },
-        {
-            iCursoId: 2,
-            cCursoNombre: 'Física',
-            cDocente: 'Dra. Rodríguez',
-            horasDisponibles: 4,
-            horasAsignadas: 2,
-        },
-        {
-            iCursoId: 3,
-            cCursoNombre: 'Química',
-            cDocente: 'Lic. Pérez',
-            horasDisponibles: 3,
-            horasAsignadas: 0,
-        },
-        {
-            iCursoId: 4,
-            cCursoNombre: 'Historia',
-            cDocente: 'Mg. Ramírez',
-            horasDisponibles: 5,
-            horasAsignadas: 0,
-        },
-        {
-            iCursoId: 5,
-            cCursoNombre: 'Lenguaje',
-            cDocente: 'Lic. Torres',
-            horasDisponibles: 4,
-            horasAsignadas: 0,
-        },
-    ]
+    cursos: Curso[] = []
     dias: any = []
-    configHoras: any = [
-        {
-            iHorarioIeId: '19',
-            inicio: '07:00:00',
-            fin: '07:45:00',
-            iBloqueHorarioId: 1,
-            bloque: 'Bloque 01',
-            iTipoBloqueId: null,
-            iHorarioIeDetId: 124,
-        },
-        {
-            iHorarioIeId: '19',
-            inicio: '07:45:00',
-            fin: '08:30:00',
-            iBloqueHorarioId: 2,
-            bloque: 'Bloque 02',
-            iTipoBloqueId: null,
-            iHorarioIeDetId: 125,
-        },
-        {
-            iHorarioIeId: '19',
-            inicio: '08:30:00',
-            fin: '09:15:00',
-            iBloqueHorarioId: 3,
-            bloque: 'Bloque 03',
-            iTipoBloqueId: null,
-            iHorarioIeDetId: 126,
-        },
-        {
-            iHorarioIeId: '19',
-            inicio: '09:15:00',
-            fin: '10:00:00',
-            iBloqueHorarioId: 4,
-            bloque: 'Bloque 04',
-            iTipoBloqueId: null,
-            iHorarioIeDetId: 127,
-        },
-        {
-            iHorarioIeId: '19',
-            inicio: '10:00:00',
-            fin: '10:15:00',
-            iBloqueHorarioId: 9,
-            bloque: 'Receso 01',
-            iTipoBloqueId: null,
-            iHorarioIeDetId: 128,
-        },
-        {
-            iHorarioIeId: '19',
-            inicio: '10:15:00',
-            fin: '11:00:00',
-            iBloqueHorarioId: 5,
-            bloque: 'Bloque 05',
-            iTipoBloqueId: null,
-            iHorarioIeDetId: 129,
-        },
-        {
-            iHorarioIeId: '19',
-            inicio: '11:00:00',
-            fin: '11:45:00',
-            iBloqueHorarioId: 6,
-            bloque: 'Bloque 06',
-            iTipoBloqueId: null,
-            iHorarioIeDetId: 130,
-        },
-        {
-            iHorarioIeId: '19',
-            inicio: '11:45:00',
-            fin: '12:30:00',
-            iBloqueHorarioId: 7,
-            bloque: 'Bloque 07',
-            iTipoBloqueId: null,
-            iHorarioIeDetId: 131,
-        },
-        {
-            iHorarioIeId: '19',
-            inicio: '12:30:00',
-            fin: '13:15:00',
-            iBloqueHorarioId: 8,
-            bloque: 'Bloque 08',
-            iTipoBloqueId: null,
-            iHorarioIeDetId: 132,
-        },
-        {
-            iHorarioIeId: '19',
-            inicio: '13:15:00',
-            fin: '14:00:00',
-            iBloqueHorarioId: 10,
-            bloque: 'Receso 02',
-            iTipoBloqueId: null,
-            iHorarioIeDetId: 133,
-        },
-    ]
-
-    horario = [
-        {
-            iCursoId: 1,
-            cCursoNombre: 'Matemáticas',
-            iDiaId: '1',
-            cDiaNombre: 'Lunes',
-            iHorarioIeId: '19',
-            inicio: '07:00:00',
-            fin: '07:45:00',
-            cDocente: 'Dr. García',
-            iHorarioIeDetId: 124,
-        },
-        {
-            iCursoId: 2,
-            cCursoNombre: 'Física',
-            iDiaId: '2',
-            cDiaNombre: 'Martesf',
-            iHorarioIeId: '19',
-            inicio: '07:45:00',
-            fin: '08:30:00',
-            cDocente: 'Dra. Rodríguez',
-            iHorarioIeDetId: 125,
-        },
-        {
-            iCursoId: 2,
-            cCursoNombre: 'Física',
-            iDiaId: '1',
-            cDiaNombre: 'Lunes',
-            iHorarioIeId: '19',
-            inicio: '07:45:00',
-            fin: '08:30:00',
-            cDocente: 'Dra. Rodríguez',
-            iHorarioIeDetId: 125,
-        },
-    ]
+    bloques: number = 0
+    horario = []
 
     accionBtn(elemento): void {
         const { accion } = elemento
         const { item } = elemento
         switch (accion) {
             case 'agregar-horario':
-                this.horario.push(item)
-                for (const key in this.cursos) {
-                    this.cursos[key].horasAsignadas = this.horario.filter(
-                        (hour) => hour.iCursoId === this.cursos[key].iCursoId
-                    ).length
-                }
-                break
             case 'remover-horario':
-                const index = this.horario.findIndex(
-                    (h) =>
-                        h.iCursoId === item.iCursoId &&
-                        h.iDiaId === item.iDiaId &&
-                        h.iHorarioIeDetId === item.iHorarioIeDetId
-                )
-                if (index !== -1) {
-                    this.horario.splice(index, 1)
-                }
-                for (const key in this.cursos) {
-                    this.cursos[key].horasAsignadas = this.horario.filter(
-                        (hour) => hour.iCursoId === this.cursos[key].iCursoId
-                    ).length
-                }
+                this.guardarRemoverCursosDiasHorarios(item)
                 break
             default:
                 break
         }
     }
 
-    private _ConfirmationModalService = inject(ConfirmationModalService)
     private _GeneralService = inject(GeneralService)
     private _ConstantesService = inject(ConstantesService)
     private _MessageService = inject(MessageService)
-    private _ApiService = inject(ApiService)
 
     async ngOnInit() {
-        this.dias = await this._ApiService.getData({
-            esquema: 'grl',
-            tabla: 'dias',
-            campos: '*',
-            where: '1=1',
-        })
-
         this.obtenerGradoSeccion()
     }
 
@@ -275,47 +84,41 @@ export class ConfiguracionHorarioComponent implements OnInit {
     }
 
     obtenerSecciones() {
-        console.log(this.iGradoId)
-        console.log(
-            this.gradosSecciones.filter(
-                (item) => item.iGradoId === this.iGradoId
-            )
-        )
         this.secciones = this.gradosSecciones.filter(
             (item) => item.iGradoId === this.iGradoId
         )
     }
 
     obtenerCursosxiGradoIdxiSeccionId() {
-        this._GeneralService
-            .searchCalendario({
-                json: JSON.stringify({
-                    iSedeId: this._ConstantesService.iSedeId,
-                    iYAcadId: this._ConstantesService.iYAcadId,
-                    iGradoId: this.iGradoId,
-                    iSeccionId: this.iSeccionId,
-                }),
-                _opcion: 'getAreasXiSeccionIdXiGradoId',
-            })
-            .subscribe({
-                next: (data: any) => {
-                    console.log(
-                        this.gradosSecciones.filter(
-                            (item) => item.iGradoId === this.iGradoId
-                        )
-                    )
-                    console.log(data)
-                },
-                error: (error) => {
-                    console.error(
-                        'Error fetching Servicios de Atención:',
-                        error
-                    )
-                },
-                complete: () => {
-                    //   console.log('Request completed')
-                },
-            })
+        const data = {
+            iSedeId: this._ConstantesService.iSedeId,
+            iYAcadId: this._ConstantesService.iYAcadId,
+            iGradoId: this.iGradoId,
+            iSeccionId: this.iSeccionId,
+        }
+        this._GeneralService.obtenerCursosDiasHorarios(data).subscribe({
+            next: (data: any) => {
+                const horarioIe = data.data
+                if (horarioIe.length > 0) {
+                    this.cursos = horarioIe[0]['cursos']
+                        ? JSON.parse(horarioIe[0]['cursos'])
+                        : []
+                    this.dias = horarioIe[0]['dias']
+                        ? JSON.parse(horarioIe[0]['dias'])
+                        : []
+                    this.horario = horarioIe[0]['horarios']
+                        ? JSON.parse(horarioIe[0]['horarios'])
+                        : []
+                    this.bloques = horarioIe[0]['iTotalBloques']
+                }
+            },
+            error: (error) => {
+                console.error('Error fetching Servicios de Atención:', error)
+            },
+            complete: () => {
+                //   console.log('Request completed')
+            },
+        })
     }
 
     removeDuplicatesByiGradoId(array: any[]): any[] {
@@ -329,23 +132,28 @@ export class ConfiguracionHorarioComponent implements OnInit {
         })
     }
 
-    // confirm() {
-    //     this._ConfirmationModalService.openConfiSave({
-    //         message: '¿Estás seguro de que deseas guardar y continuar?',
-    //         header: 'Advertencia de autoguardado',
-    //         icon: 'pi pi-exclamation-triangle',
-    //         accept: () => {
-    //             // Acción para eliminar el registro
-    //             this.router.navigate(['/gestion-institucional/resumen'])
-    //         },
-    //         reject: () => {
-    //             // Mensaje de cancelación (opcional)
-    //             this.messageService.add({
-    //                 severity: 'error',
-    //                 summary: 'Cancelado',
-    //                 detail: 'Acción cancelada',
-    //             })
-    //         },
-    //     })
-    // }
+    guardarRemoverCursosDiasHorarios(item) {
+        this._GeneralService.guardarRemoverCursosDiasHorarios(item).subscribe({
+            next: (data: any) => {
+                if (data.validated) {
+                    this.obtenerCursosxiGradoIdxiSeccionId()
+                }
+                // console.log(data)
+            },
+            error: (error) => {
+                // console.error(
+                //     'Error fetching Servicios de Atención:',
+                //     error
+                // )
+                this._MessageService.add({
+                    severity: 'error',
+                    summary: 'Error',
+                    detail: error,
+                })
+            },
+            complete: () => {
+                //   console.log('Request completed')
+            },
+        })
+    }
 }

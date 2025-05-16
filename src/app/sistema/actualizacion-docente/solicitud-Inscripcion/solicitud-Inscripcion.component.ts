e3e8d416900979ac733dd1a4c703b4b518965b36
DREW <jhoand60@gmail.com>
2025-05-16 08:10:15 -0500

    feat: implement new components for course registration and listing, including card and detail views

--- src/app/sistema/actualizacion-docente/solicitud-Inscripcion/solicitud-Inscripcion.component.ts
+++ src/app/sistema/actualizacion-docente/solicitud-Inscripcion/solicitud-Inscripcion.component.ts
@@ -2,12 +2,11 @@ import { PrimengModule } from '@/app/primeng.module'
 import { ToolbarPrimengComponent } from '@/app/shared/toolbar-primeng/toolbar-primeng.component'
 import { Component } from '@angular/core'
 import {
-    TablePrimengComponent,
     IColumn,
     IActionTable,
 } from '@/app/shared/table-primeng/table-primeng.component'
-import { AperturaCursoComponent } from '../apertura-curso/apertura-curso.component'
 import { TabsPrimengComponent } from '@/app/shared/tabs-primeng/tabs-primeng.component'
+import { CardCapacitacionesComponent } from './card-capacitaciones/card-capacitaciones.component'
 
 @Component({
     selector: 'app-solicitud-inscripcion',
@@ -17,14 +16,13 @@ import { TabsPrimengComponent } from '@/app/shared/tabs-primeng/tabs-primeng.com
     imports: [
         PrimengModule,
         ToolbarPrimengComponent,
-        TablePrimengComponent,
-        AperturaCursoComponent,
         TabsPrimengComponent,
+        CardCapacitacionesComponent,
     ],
 })
 export class SolicitudInscripcionComponent {
     activeIndex: number = 0
-
+    cursoSeleccionado
     tabs = [
         {
             title: 'Apertura de Curso',
@@ -121,6 +119,85 @@ export class SolicitudInscripcionComponent {
             accion: 'agregarConclusion',
             type: 'item',
             class: 'p-button-rounded p-button-danger p-button-text',
+        },
+    ]
+
+    data = [
+        {
+            iCapacitacionId:
+                'BL5XB8NQmabwA3zDlgW710Jn0bPxMYeRVory4jKZvpGEkq2d90',
+            iTipoCapId: '2jdp2ERVe0QYG8agql5J1ybONbOMzW93KvLNZ7okAmD4xXBrwe',
+            iNivelPedId: '2jdp2ERVe0QYG8agql5J1ybONbOMzW93KvLNZ7okAmD4xXBrwe',
+            iTipoPubId: '2jdp2ERVe0QYG8agql5J1ybONbOMzW93KvLNZ7okAmD4xXBrwe',
+            cCapTitulo: 'Título 1',
+            cCapDescripcion: 'Descripción 1',
+            iTotalHrs: '5',
+            dFechaInicio: '2025-05-05',
+            dFechaFin: '2025-05-05',
+            iInstId: '2jdp2ERVe0QYG8agql5J1ybONbOMzW93KvLNZ7okAmD4xXBrwe',
+            iCosto: '1',
+            nCosto: '1.00',
+            iImageAleatorio: '1',
+            cImagenUrl:
+                '[{"id":2,"name":"Miss Lesson","url":"/images/recursos/miss-lesson-animate.svg"}]',
+            iEstado: '1',
+            cTipoCapNombre: 'Diplomando',
+            cNivelPedNombre: 'Educación Inicial',
+            cTipoPubNombre: 'General',
+            cPersNombre: 'MITWAR KENYO',
+            cPersPaterno: 'HUACAN',
+            cPersMaterno: 'QUISPE',
+        },
+        {
+            iCapacitacionId:
+                'ZMDNgoXjdk9Qz0ZyWKra3B1PQ3nRG54pY2lwq78vEAeVbmLJx1',
+            iTipoCapId: '2jdp2ERVe0QYG8agql5J1ybONbOMzW93KvLNZ7okAmD4xXBrwe',
+            iNivelPedId: '2jdp2ERVe0QYG8agql5J1ybONbOMzW93KvLNZ7okAmD4xXBrwe',
+            iTipoPubId: '2jdp2ERVe0QYG8agql5J1ybONbOMzW93KvLNZ7okAmD4xXBrwe',
+            cCapTitulo: 'Título 1',
+            cCapDescripcion: 'Descripción 1',
+            iTotalHrs: '5',
+            dFechaInicio: '2025-05-05',
+            dFechaFin: '2025-05-05',
+            iInstId: '2jdp2ERVe0QYG8agql5J1ybONbOMzW93KvLNZ7okAmD4xXBrwe',
+            iCosto: '1',
+            nCosto: '1.00',
+            iImageAleatorio: '1',
+            cImagenUrl:
+                '[{"id":2,"name":"Miss Lesson","url":"/images/recursos/miss-lesson-animate.svg"}]',
+            iEstado: '1',
+            cTipoCapNombre: 'Diplomando',
+            cNivelPedNombre: 'Educación Inicial',
+            cTipoPubNombre: 'General',
+            cPersNombre: 'MITWAR KENYO',
+            cPersPaterno: 'HUACAN',
+            cPersMaterno: 'QUISPE',
+        },
+        {
+            iCapacitacionId:
+                'G1NzYXA5kg23LdaMq480mDQPj8nlGJrBV7Kb9yeowpWxvjRZER',
+            iTipoCapId: 'zR0lYAQJLx4e3jZ2wW7GrMN6obPomB89bqz5EdavkXpDy1KgVV',
+            iNivelPedId: '37A3j9KEx1GNyJV82MqekXwn3APbY4LvRpr0QdaWzlmg5DZBoZ',
+            iTipoPubId: '2jdp2ERVe0QYG8agql5J1ybONbOMzW93KvLNZ7okAmD4xXBrwe',
+            cCapTitulo: 'WebFlow University: Ultimate Web Design Course',
+            cCapDescripcion:
+                'El Ultimate Web Design Course es un curso gratuito ofrecido por la universidad de WebFlow, una plataforma de aprendizaje en línea alojada y desarrollada por el CMS y la herramienta de diseño WebFlow.',
+            iTotalHrs: '129',
+            dFechaInicio: '2025-05-14',
+            dFechaFin: '2025-05-28',
+            iInstId: '2jdp2ERVe0QYG8agql5J1ybONbOMzW93KvLNZ7okAmD4xXBrwe',
+            iCosto: '1',
+            nCosto: '120.00',
+            iImageAleatorio: '1',
+            cImagenUrl:
+                '{"id":3,"name":"Miss Student","url":"http://127.0.0.1:8000/images/recursos/miss-student-animate.svg"}',
+            iEstado: '1',
+            cTipoCapNombre: 'Capacitación',
+            cNivelPedNombre: 'Educación Secundaria',
+            cTipoPubNombre: 'General',
+            cPersNombre: 'MITWAR KENYO',
+            cPersPaterno: 'HUACAN',
+            cPersMaterno: 'QUISPE',
         },
     ]
 }

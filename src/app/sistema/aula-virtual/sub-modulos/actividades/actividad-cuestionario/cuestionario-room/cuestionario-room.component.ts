185d61cc127be7b40c5b9f511569d2aa299c9629
Mitwar99 <mitwarhuacan@hotmail.com>
2025-06-03 10:36:45 -0500

    mx: Obtener datos de cuestionario

--- src/app/sistema/aula-virtual/sub-modulos/actividades/actividad-cuestionario/cuestionario-room/cuestionario-room.component.ts
+++ src/app/sistema/aula-virtual/sub-modulos/actividades/actividad-cuestionario/cuestionario-room/cuestionario-room.component.ts
@@ -1,13 +1,15 @@
 import { PrimengModule } from '@/app/primeng.module'
-import { Component, Input, OnInit } from '@angular/core'
+import { Component, inject, Input, OnInit } from '@angular/core'
 import { MenuItem } from 'primeng/api'
 import { Location } from '@angular/common'
 import { matListAlt, matPeople } from '@ng-icons/material-icons/baseline'
 import { DialogService } from 'primeng/dynamicdialog'
 import { provideIcons } from '@ng-icons/core'
 import { IconComponent } from '@/app/shared/icon/icon.component'
-import { TabViewModule } from 'primeng/tabview'
 import { ToolbarPrimengComponent } from '@/app/shared/toolbar-primeng/toolbar-primeng.component'
+import { ConstantesService } from '@/app/servicios/constantes.service'
+import { GeneralService } from '@/app/servicios/general.service'
+import { RemoveHTMLPipe } from '@/app/shared/pipes/remove-html.pipe'
 @Component({
     selector: 'app-cuestionario-room',
     standalone: true,
@@ -16,8 +18,8 @@ import { ToolbarPrimengComponent } from '@/app/shared/toolbar-primeng/toolbar-pr
     imports: [
         PrimengModule,
         IconComponent,
-        TabViewModule,
         ToolbarPrimengComponent,
+        RemoveHTMLPipe,
     ],
     providers: [provideIcons({ matListAlt, matPeople }), DialogService],
 })
@@ -27,8 +29,12 @@ export class CuestionarioRoomComponent implements OnInit {
     @Input() iActTopId: number
     @Input() contenidoSemana
 
+    private _constantesService = inject(ConstantesService)
+    private GeneralService = inject(GeneralService)
+
     items: MenuItem[] | undefined
     home: MenuItem | undefined
+    cuestionario: any = {}
 
     constructor(private location: Location) {}
 
@@ -51,8 +57,32 @@ export class CuestionarioRoomComponent implements OnInit {
         ]
 
         this.home = { icon: 'pi pi-home', routerLink: '/' }
+        // -----------------
+        this.obtenerInformacion()
     }
     goBack() {
         this.location.back()
+    }
+
+    // Obtener datos del cuestionario:
+    obtenerInformacion() {
+        const data = {
+            petition: 'get',
+            group: 'aula-virtual',
+            prefix: 'cuestionarios',
+            ruta: this.ixActivadadId.toString(),
+            params: {
+                iCredId: this._constantesService.iCredId, // Asignar el ID del crédito
+            },
+        }
+        this.GeneralService.getGralPrefixx(data).subscribe({
+            next: (resp) => {
+                this.cuestionario = resp.data.length ? resp.data[0] : {}
+                console.log('Respuesta del cuestionario:', this.cuestionario)
+            },
+            error: (err) => {
+                console.error('Error obteniendo cuestionario:', err)
+            },
+        })
     }
 }

144d5247921b9d7dadf91f3145024225c3fa083a
Wilmar Garibaldi <w_garibaldi@hotmail.com>
2025-06-16 10:40:23 -0500

    Ajustes en imagenes al rendir examen

--- src/app/sistema/ere/evaluaciones/examen/modal-evaluacion-finalizada/modal-evaluacion-finalizada.component.ts
+++ src/app/sistema/ere/evaluaciones/examen/modal-evaluacion-finalizada/modal-evaluacion-finalizada.component.ts
@@ -13,6 +13,7 @@ import { Router } from '@angular/router'
 })
 export class ModalEvaluacionFinalizadaComponent implements OnInit {
     @Input() grado: string
+    @Input() cerrarSesion: boolean = false
     visible: boolean = true
     mostrarBotonEncuesta: boolean = false
 
@@ -51,8 +52,12 @@ export class ModalEvaluacionFinalizadaComponent implements OnInit {
     }
 
     logout(): void {
-        this.store.clear()
-        this.tokenStorageService.signOut()
-        window.location.reload()
+        if (this.cerrarSesion) {
+            this.router.navigate(['/'])
+        } else {
+            this.store.clear()
+            this.tokenStorageService.signOut()
+            window.location.reload()
+        }
     }
 }

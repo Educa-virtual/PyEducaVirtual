5c2a6629d29fa2c560e7858419375cc562544990
Mitwar99 <mitwarhuacan@hotmail.com>
2025-06-20 13:29:29 -0500

    mx: guardar respuestas

--- src/app/sistema/aula-virtual/sub-modulos/actividades/actividad-cuestionario/cuestionario-preguntas/cuestionario-preguntas.component.ts
+++ src/app/sistema/aula-virtual/sub-modulos/actividades/actividad-cuestionario/cuestionario-preguntas/cuestionario-preguntas.component.ts
@@ -9,6 +9,7 @@ import { CuestionarioFormPreguntasComponent } from '../cuestionario-form-pregunt
 import { GeneralService } from '@/app/servicios/general.service'
 import { ConfirmationModalService } from '@/app/shared/confirm-modal/confirmation-modal.service'
 import { DOCENTE, ESTUDIANTE } from '@/app/servicios/perfilesConstantes'
+// import { group } from '@angular/animations'
 // import { aC } from '@fullcalendar/core/internal-common'
 
 @Component({
@@ -178,6 +179,62 @@ export class CuestionarioPreguntasComponent implements OnInit {
         setTimeout(() => {
             this.loading = false
         }, 2000)
+    }
+
+    respuesta: string = ''
+
+    guadarRespuesta(): void {
+        const params = {
+            petition: 'get',
+            group: 'aula-virtual',
+            prefix: 'pregunta-alternativas-respuestas',
+            ruta: 'cuestionario',
+            data: this.respuesta,
+            params: {
+                iCredId: this._constantesService.iCredId,
+            },
+        }
+        // Servicio para obtener los instructores
+        this.GeneralService.getGralPrefixx(params).subscribe({
+            next: (response) => {
+                if (response.validated) {
+                    this.messageService.add({
+                        severity: 'success',
+                        summary: 'Acción exitosa',
+                        detail: response.message,
+                    })
+                    this.showModal = false
+                    this.obtenerCuestionario()
+                    // this.instructorForm.reset()
+                }
+            },
+            error: (error) => {
+                const errores = error?.error?.errors
+                if (error.status === 422 && errores) {
+                    // Recorre y muestra cada mensaje de error
+                    Object.keys(errores).forEach((campo) => {
+                        errores[campo].forEach((mensaje: string) => {
+                            this.messageService.add({
+                                severity: 'error',
+                                summary: 'Error de validación',
+                                detail: mensaje,
+                            })
+                        })
+                    })
+                } else {
+                    // Error genérico si no hay errores específicos
+                    this.messageService.add({
+                        severity: 'error',
+                        summary: 'Error',
+                        detail:
+                            error?.error?.message ||
+                            'Ocurrió un error inesperado',
+                    })
+                }
+            },
+        })
+        console.log('Respuesta ingresada:', params)
+        // Aquí puedes enviar la variable o hacer lo que necesites
     }
     guardarPregunta(data: any) {
         const datos = {

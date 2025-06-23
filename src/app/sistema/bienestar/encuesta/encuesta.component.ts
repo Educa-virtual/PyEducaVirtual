a7c4c62efd6aa76416d0e57d563ba4b6ac8eb306
Paolo Villanueva <nvillanueva@gmail.com>
2025-06-23 12:53:02 -0500

    Formulario de registro de encuestas
    - Crear servicio para manejar peticiones de encuestas

--- src/app/sistema/bienestar/encuesta/encuesta.component.ts
+++ src/app/sistema/bienestar/encuesta/encuesta.component.ts
@@ -2,46 +2,72 @@ import { PrimengModule } from '@/app/primeng.module'
 import { Component, EventEmitter, inject, OnInit, Output } from '@angular/core'
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 import { MessageService } from 'primeng/api'
+import { DatosEncuestaService } from '../services/datos-encuesta.service'
+import {
+    IActionTable,
+    TablePrimengComponent,
+} from '@/app/shared/table-primeng/table-primeng.component'
 
 @Component({
     selector: 'app-encuesta',
     standalone: true,
-    imports: [PrimengModule],
+    imports: [PrimengModule, TablePrimengComponent],
     templateUrl: './encuesta.component.html',
     styleUrl: './encuesta.component.scss',
 })
 export class EncuestaComponent implements OnInit {
     @Output() es_visible = new EventEmitter<any>()
     active: number = 0
-    formEncuesta: FormGroup
-    categorias: Array<object>
-    estados: Array<object>
     ultima_fecha_anio: Date = new Date(new Date().getFullYear(), 11, 31)
     fecha_actual: Date = new Date()
     es_especialista: boolean = false
 
-    tipo_sectores: Array<object>
-    zonas: Array<object>
-    ugeles: Array<object>
+    formEncuesta: FormGroup
+    formPoblacion: FormGroup
+    formEditores: FormGroup
+
+    categorias: Array<object>
+    opciones: Array<object>
     distritos: Array<object>
-    ies: Array<object>
+    nivel_tipos: Array<object>
     nivel_grados: Array<object>
+    areas: Array<object>
     secciones: Array<object>
+    zonas: Array<object>
+    tipo_sectores: Array<object>
+    ugeles: Array<object>
+    cursos: Array<object>
+    ies: Array<object>
     sexos: Array<object>
+    estados: Array<object>
+    perfiles: Array<object>
+
+    poblacion: Array<object> = []
+    editores: Array<object> = []
+
+    cantidad_poblacion: number = 0
 
     private _messageService = inject(MessageService)
 
-    constructor(private fb: FormBuilder) {}
+    constructor(
+        private fb: FormBuilder,
+        private datosEncuestas: DatosEncuestaService
+    ) {}
 
     ngOnInit(): void {
         this.formEncuesta = this.fb.group({
-            cEncuestaTitulo: ['', Validators.required],
-            cEncuestaSubtitulo: ['', Validators.required],
-            cEncuestaDescripcion: ['', Validators.required],
-            iCategoriaEncuestaId: [null, Validators.required],
-            iEstado: [null, Validators.required],
-            dFechaInicio: ['', Validators.required],
-            dFechaFin: [''],
+            iEncuId: [null],
+            cEncuNombre: ['', Validators.required],
+            cEncuDescripcion: ['', Validators.required],
+            iEncuCateId: [null, Validators.required],
+            iEstado: [1, Validators.required],
+            dEncuDesde: [null, Validators.required],
+            dEncuHasta: [null, Validators.required],
+        })
+
+        this.formPoblacion = this.fb.group({
+            iNivelTipoId: [null],
+            iCursoId: [null],
             iTipoSectorId: [null],
             iZonaId: [null],
             iUgelId: [null],
@@ -50,23 +76,311 @@ export class EncuestaComponent implements OnInit {
             iNivelGradoId: [null],
             iSeccionId: [null],
             cPersSexo: [null],
+            poblacion: [''],
         })
 
-        this.categorias = [
-            { label: 'Socioeconómica', value: 1 },
-            { label: 'Psicosocial', value: 2 },
-            { label: 'Vocacional', value: 3 },
-            { label: 'Vida estudiantil', value: 4 },
-        ]
+        this.formEditores = this.fb.group({
+            iPerfilId: [null],
+            cPerfilNomnre: [''],
+            iEncuOpcId: [null],
+            cEncuOpcNombre: [''],
+            cantidad: [0],
+        })
 
-        this.estados = [
-            { label: 'En edición', value: 1 },
-            { label: 'Aprobado', value: 2 },
-            { label: 'Finalizado', value: 3 },
+        this.datosEncuestas.getEncuestaParametros().subscribe((data: any) => {
+            this.perfiles = this.datosEncuestas.getPerfiles(data?.perfiles)
+            this.categorias = this.datosEncuestas.getCategorias(
+                data?.categorias
+            )
+            this.opciones = this.datosEncuestas.getOpciones(data?.opciones)
+            this.distritos = this.datosEncuestas.getDistritos(data?.distritos)
+            this.secciones = this.datosEncuestas.getSecciones(data?.secciones)
+            this.zonas = this.datosEncuestas.getZonas(data?.zonas)
+            this.tipo_sectores = this.datosEncuestas.getTipoSectores(
+                data?.tipo_sectores
+            )
+            this.ugeles = this.datosEncuestas.getUgeles(data?.ugeles)
+            this.nivel_tipos = this.datosEncuestas.getNivelesTipos(
+                data?.nivel_tipos
+            )
+            this.ies = this.datosEncuestas.getInstitucionesEducativas(
+                data?.instituciones_educativas
+            )
+            this.sexos = this.datosEncuestas.getSexos()
+            this.estados = this.datosEncuestas.getEstados()
+            this.datosEncuestas.getNivelesGrados(data?.nivel_grados)
+            this.datosEncuestas.getAreas(data?.areas)
+        })
+
+        this.formPoblacion
+            .get('iNivelTipoId')
+            .valueChanges.subscribe((value) => {
+                this.formPoblacion.get('iNivelGradoId')?.setValue(null)
+                this.nivel_grados = null
+                this.filterNivelesGrados(value)
+
+                this.formPoblacion.get('iCursoId')?.setValue(null)
+                this.areas = null
+                this.filterAreas(value)
+
+                this.formPoblacion.get('iIieeId')?.setValue(null)
+                this.ies = null
+                this.filterInstitucionesEducativas()
+            })
+        this.formPoblacion.get('iDsttId').valueChanges.subscribe(() => {
+            this.formPoblacion.get('iIieeId')?.setValue(null)
+            this.ies = null
+            this.filterInstitucionesEducativas()
+        })
+        this.formPoblacion.get('iZonaId').valueChanges.subscribe(() => {
+            this.formPoblacion.get('iIieeId')?.setValue(null)
+            this.ies = null
+            this.filterInstitucionesEducativas()
+        })
+        this.formPoblacion.get('iTipoSectorId').valueChanges.subscribe(() => {
+            this.formPoblacion.get('iIieeId')?.setValue(null)
+            this.ies = null
+            this.filterInstitucionesEducativas()
+        })
+        this.formPoblacion.get('iUgelId').valueChanges.subscribe((value) => {
+            this.formPoblacion.get('iDsttId')?.setValue(null)
+            this.formPoblacion.get('iIieeId')?.setValue(null)
+            this.ies = null
+            this.distritos = null
+            this.filterInstitucionesEducativas()
+            this.filterDistritos(value)
+        })
+    }
+
+    filterNivelesTipos() {
+        this.nivel_tipos = this.datosEncuestas.filterNivelesTipos()
+    }
+
+    filterNivelesGrados(iNivelTipoId: number) {
+        this.nivel_grados =
+            this.datosEncuestas.filterNivelesGrados(iNivelTipoId)
+    }
+
+    filterAreas(iNivelTipoId: number) {
+        this.areas = this.datosEncuestas.filterAreas(iNivelTipoId)
+    }
+
+    filterDistritos(iUgelId: number) {
+        this.distritos = this.datosEncuestas.filterDistritos(iUgelId)
+    }
+
+    filterInstitucionesEducativas() {
+        const iNivelTipoId = this.formPoblacion.get('iNivelTipoId')?.value
+        const iDsttId = this.formPoblacion.get('iDsttId')?.value
+        const iZonaId = this.formPoblacion.get('iZonaId')?.value
+        const iTipoSectorId = this.formPoblacion.get('iTipoSectorId')?.value
+        const iUgelId = this.formPoblacion.get('iUgelId')?.value
+        this.ies = this.datosEncuestas.filterInstitucionesEducativas(
+            iNivelTipoId,
+            iDsttId,
+            iZonaId,
+            iTipoSectorId,
+            iUgelId
+        )
+    }
+
+    actualizarPoblacion() {
+        const nivel_tipo: any = this.nivel_tipos
+            ? this.nivel_tipos.find(
+                  (nivel: any) =>
+                      nivel.value ==
+                      this.formPoblacion.get('iNivelTipoId')?.value
+              )
+            : null
+        const tipo_sector: any = this.tipo_sectores
+            ? this.tipo_sectores.find(
+                  (sector: any) =>
+                      sector.value ==
+                      this.formPoblacion.get('iTipoSectorId')?.value
+              )
+            : null
+        const tipo_zona: any = this.zonas
+            ? this.zonas.find(
+                  (zona: any) =>
+                      zona.value == this.formPoblacion.get('iZonaId')?.value
+              )
+            : null
+        const ugel: any = this.ugeles
+            ? this.ugeles.find(
+                  (ugel: any) =>
+                      ugel.value == this.formPoblacion.get('iUgelId')?.value
+              )
+            : null
+        const distrito: any = this.distritos
+            ? this.distritos.find(
+                  (distrito: any) =>
+                      distrito.value == this.formPoblacion.get('iDsttId')?.value
+              )
+            : null
+        const ie: any = this.ies
+            ? this.ies.find(
+                  (ie: any) =>
+                      ie.value == this.formPoblacion.get('iIieeId')?.value
+              )
+            : null
+        const area: any = this.areas
+            ? this.areas.find(
+                  (area: any) =>
+                      area.value == this.formPoblacion.get('iCursoId')?.value
+              )
+            : null
+        const nivel_grado: any = this.nivel_grados
+            ? this.nivel_grados.find(
+                  (nivel: any) =>
+                      nivel.value ==
+                      this.formPoblacion.get('iNivelGradoId')?.value
+              )
+            : null
+        const seccion: any = this.secciones
+            ? this.secciones.find(
+                  (seccion: any) =>
+                      seccion.value ==
+                      this.formPoblacion.get('iSeccionId')?.value
+              )
+            : null
+        const sexo: any = this.sexos
+            ? this.sexos.find(
+                  (sexo: any) =>
+                      sexo.value == this.formPoblacion.get('cPersSexo')?.value
+              )
+            : null
+
+        let poblacion: any = [
+            nivel_tipo?.label,
+            tipo_sector?.label,
+            tipo_zona?.label,
+            ugel?.label,
+            distrito?.label,
+            ie?.label,
+            area?.label,
+            nivel_grado?.label,
+            seccion?.label,
+            sexo?.label,
         ]
+        poblacion = poblacion.filter((item: any) => item != null)
+        this.formPoblacion.get('poblacion')?.setValue(poblacion.join(', '))
+        this.datosEncuestas.getCantidadPoblacion(poblacion)
+    }
+
+    actualizarEditores() {
+        const perfil: any = this.perfiles.find(
+            (perfil: any) =>
+                perfil.value == this.formEditores.get('iPerfilId')?.value
+        )
+        const opcion: any = this.opciones.find(
+            (opcion: any) =>
+                opcion.value == this.formEditores.get('iOpcionId')?.value
+        )
+        this.formEditores
+            .get('cPerfilNombre')
+            ?.setValue(perfil ? perfil.label : '')
+        this.formEditores
+            .get('cEncuOpcNombre')
+            ?.setValue(opcion ? opcion.label : '')
     }
 
     guardarEncuesta() {
-        // realizar guardado
+        if (this.formEncuesta.invalid) {
+            this._messageService.add({
+                severity: 'warn',
+                summary: 'Advertencia',
+                detail: 'Debe especificar los campos obligatorios',
+            })
+        }
+
+        if (this.poblacion.length == 0) {
+            this._messageService.add({
+                severity: 'warn',
+                summary: 'Advertencia',
+                detail: 'Debe especificar al menos una población objetivo',
+            })
+        }
     }
+
+    agregarPoblacion() {
+        this.actualizarPoblacion()
+        const form: Array<object> = this.formPoblacion.value
+        this.poblacion = [...this.poblacion, form]
+        this.formPoblacion.reset()
+    }
+
+    agregarEditor() {
+        this.actualizarEditores()
+        const form = this.formEditores.value
+        this.editores = [...this.editores, form]
+        this.formEditores.reset()
+    }
+
+    actions: IActionTable[] = []
+
+    actionsLista: IActionTable[]
+
+    columns = []
+
+    columns_poblacion = [
+        {
+            type: 'item',
+            width: '10%',
+            field: '',
+            header: 'N°',
+            text_header: 'left',
+            text: 'left',
+        },
+        {
+            type: 'text',
+            width: '70%',
+            field: 'poblacion',
+            header: 'Población',
+            text_header: 'left',
+            text: 'left',
+        },
+        {
+            type: 'actions',
+            width: '20%',
+            field: '',
+            header: 'Acciones',
+            text_header: 'left',
+            text: 'left',
+        },
+    ]
+
+    columns_editores = [
+        {
+            type: 'item',
+            width: '5%',
+            field: 'item',
+            header: '#',
+            text_header: 'left',
+            text: 'left',
+        },
+        {
+            type: 'text',
+            width: '20%',
+            field: 'cPerfilNombre',
+            header: 'Editor',
+            text_header: 'left',
+            text: 'left',
+        },
+        {
+            type: 'text',
+            width: '10%',
+            field: 'cEncuOpcNombre',
+            header: 'Opción',
+            text_header: 'left',
+            text: 'left',
+        },
+        {
+            type: 'actions',
+            width: '20%',
+            field: '',
+            header: 'Acciones',
+            text_header: 'left',
+            text: 'left',
+        },
+    ]
 }
---
+++ src/app/sistema/bienestar/services/datos-encuesta.service.ts
@@ -0,0 +1,339 @@
+import { Injectable, OnDestroy } from '@angular/core'
+import { Subject } from 'rxjs'
+import { map } from 'rxjs/operators'
+import { of } from 'rxjs'
+import { HttpClient } from '@angular/common/http'
+import { environment } from '@/environments/environment'
+
+const baseUrl = environment.backendApi
+
+@Injectable({
+    providedIn: 'root',
+})
+export class DatosEncuestaService implements OnDestroy {
+    private onDestroy$ = new Subject<boolean>()
+
+    constructor(private http: HttpClient) {}
+
+    lista: any[] = []
+
+    parametros: any
+
+    categorias: Array<object>
+    opciones: Array<object>
+    distritos: Array<object>
+    nivel_tipos: Array<object>
+    nivel_grados: Array<object>
+    areas: Array<object>
+    secciones: Array<object>
+    zonas: Array<object>
+    tipo_sectores: Array<object>
+    ugeles: Array<object>
+    instituciones_educativas: Array<object>
+    sexos: Array<object>
+    estados: Array<object>
+    perfiles: Array<object>
+
+    listarEncuestas(data: any) {
+        return this.http.post(`${baseUrl}/bienestar/listarEncuestas`, data)
+    }
+
+    verEncuesta(data: any) {
+        return this.http.post(`${baseUrl}/bienestar/verEncuesta`, data)
+    }
+
+    guardarEncuesta(data: any) {
+        return this.http.post(`${baseUrl}/bienestar/guardarEncuesta`, data)
+    }
+
+    actualizarEncuesta(data: any) {
+        return this.http.post(`${baseUrl}/bienestar/actualizarEncuesta`, data)
+    }
+
+    getCantidadPoblacion(data: any) {
+        return this.http.post(`${baseUrl}/bienestar/getCantidadPoblacion`, data)
+    }
+
+    /*
+     * Funciones para popular parametros de formularios de ficha
+     */
+
+    getEncuestaParametros() {
+        if (!this.parametros) {
+            return this.http.get(`${baseUrl}/bienestar/crearEncuesta`).pipe(
+                map((data: any) => {
+                    this.parametros = data.data[0]
+                    return this.parametros
+                })
+            )
+        }
+        return of(this.parametros)
+    }
+
+    getCategorias(data: any) {
+        if (!this.categorias && data) {
+            const items = JSON.parse(data.replace(/^"(.*)"$/, '$1'))
+            return items.map((categoria: any) => ({
+                value: categoria.iEncuCateId,
+                label: categoria.cEncuCateNombre,
+            }))
+        }
+        return this.categorias
+    }
+
+    getOpciones(data: any) {
+        if (!this.opciones && data) {
+            const items = JSON.parse(data.replace(/^"(.*)"$/, '$1'))
+            return items.map((opcion: any) => ({
+                value: opcion.iEncuOpcId,
+                label: opcion.cEncuOpcNombre,
+            }))
+        }
+        return this.opciones
+    }
+
+    getDistritos(data: any) {
+        if (!this.distritos && data) {
+            const items = JSON.parse(data.replace(/^"(.*)"$/, '$1'))
+            this.distritos = items.map((distrito) => ({
+                value: distrito.iDsttId,
+                label: distrito.cDsttNombre,
+                ugeles: distrito.ugeles,
+            }))
+            return this.distritos
+        }
+        return this.distritos
+    }
+
+    filterDistritos(iUgelId: any) {
+        if (!iUgelId) return null
+        return this.distritos.filter((distrito: any) => {
+            const pertenece_ugel = distrito.ugeles.find(
+                (ugel: any) => ugel.iUgelId === iUgelId
+            )
+            if (pertenece_ugel) {
+                return distrito
+            }
+            return null
+        })
+    }
+
+    getNivelesTipos(data: any) {
+        if (!this.nivel_tipos && data) {
+            const items = JSON.parse(data.replace(/^"(.*)"$/, '$1'))
+            this.nivel_tipos = items.map((nivel) => ({
+                value: nivel.iNivelTipoId,
+                label: nivel.cNivelTipoNombre,
+            }))
+            return this.nivel_tipos
+        }
+        return this.nivel_tipos
+    }
+
+    filterNivelesTipos() {
+        return this.nivel_tipos
+    }
+
+    getNivelesGrados(data: any) {
+        if (!this.nivel_grados && data) {
+            const items = JSON.parse(data.replace(/^"(.*)"$/, '$1'))
+            this.nivel_grados = items.map((nivel) => ({
+                value: nivel.iNivelGradoId,
+                label: nivel.cGradoNombre,
+                iNivelTipoId: nivel.iNivelTipoId,
+            }))
+            return this.nivel_grados
+        }
+        return this.nivel_grados
+    }
+
+    filterNivelesGrados(iNivelTipoId: any) {
+        if (!iNivelTipoId) return null
+        return this.nivel_grados.filter((nivel_grado: any) => {
+            if (nivel_grado.iNivelTipoId === iNivelTipoId) {
+                return nivel_grado
+            }
+            return null
+        })
+    }
+
+    getAreas(data: any) {
+        if (!this.areas && data) {
+            const items = JSON.parse(data.replace(/^"(.*)"$/, '$1'))
+            this.areas = items.map((area) => ({
+                value: area.iCursoId,
+                label: area.cCursoNombre,
+                iNivelTipoId: area.iNivelTipoId,
+            }))
+            return this.areas
+        }
+        return this.areas
+    }
+
+    filterAreas(iNivelTipoId: any) {
+        if (!iNivelTipoId) return null
+        return this.areas.filter((area: any) => {
+            if (area.iNivelTipoId === iNivelTipoId) {
+                return area
+            }
+            return null
+        })
+    }
+
+    getSecciones(data: any) {
+        if (!this.secciones && data) {
+            const items = JSON.parse(data.replace(/^"(.*)"$/, '$1'))
+            this.secciones = items.map((seccion) => ({
+                value: seccion.iSeccionId,
+                label: seccion.cSeccionNombre,
+            }))
+            return this.secciones
+        }
+        return this.secciones
+    }
+
+    getZonas(data: any) {
+        if (!this.zonas && data) {
+            const items = JSON.parse(data.replace(/^"(.*)"$/, '$1'))
+            this.zonas = items.map((zona) => ({
+                value: zona.iZonaId,
+                label: zona.cZonaNombre,
+            }))
+            return this.zonas
+        }
+        return this.zonas
+    }
+
+    getTipoSectores(data: any) {
+        if (!this.tipo_sectores && data) {
+            const items = JSON.parse(data.replace(/^"(.*)"$/, '$1'))
+            this.tipo_sectores = items.map((tipo_sector) => ({
+                value: tipo_sector.iTipoSectorId,
+                label: tipo_sector.cTipoSectorNombre,
+            }))
+            return this.tipo_sectores
+        }
+        return this.tipo_sectores
+    }
+
+    getUgeles(data: any) {
+        if (!this.ugeles && data) {
+            const items = JSON.parse(data.replace(/^"(.*)"$/, '$1'))
+            this.ugeles = items.map((ugel) => ({
+                value: ugel.iUgelId,
+                label: ugel.cUgelNombre,
+            }))
+            return this.ugeles
+        }
+        return this.ugeles
+    }
+
+    getPerfiles(data: any) {
+        if (!this.perfiles && data) {
+            const items = JSON.parse(data.replace(/^"(.*)"$/, '$1'))
+            this.perfiles = items.map((perfil) => ({
+                value: perfil.iPerfilId,
+                label: perfil.cPerfilNombre,
+            }))
+            return this.perfiles
+        }
+        return this.perfiles
+    }
+
+    getInstitucionesEducativas(data: any) {
+        if (!this.instituciones_educativas && data) {
+            const items = JSON.parse(data.replace(/^"(.*)"$/, '$1'))
+            this.instituciones_educativas = items.map((ie) => ({
+                value: ie.iIieeId,
+                label: ie.cIieeNombre,
+                iDsttId: ie?.iDsttId,
+                iNivelTipoId: ie?.iNivelTipoId,
+                iZonaId: ie?.iZonaId,
+                iTipoSectorId: ie?.iTipoSectorId,
+                iUgelId: ie?.iUgelId,
+                evaluaciones: ie?.evaluaciones,
+            }))
+            return this.instituciones_educativas
+        }
+        return this.instituciones_educativas
+    }
+
+    filterInstitucionesEducativas(
+        iNivelTipoId: any,
+        iDsttId: any,
+        iZonaId: any,
+        iTipoSectorId: any,
+        iUgelId: any
+    ) {
+        let ies_tmp: Array<object> = this.instituciones_educativas
+        if (!iNivelTipoId) {
+            return null
+        }
+        if (iNivelTipoId) {
+            ies_tmp = ies_tmp.filter((ie: any) => {
+                if (ie.iNivelTipoId == iNivelTipoId) {
+                    return ie
+                }
+                return null
+            })
+        }
+        if (iDsttId) {
+            ies_tmp = ies_tmp.filter((ie: any) => {
+                if (ie.iDsttId == iDsttId) {
+                    return ie
+                }
+                return null
+            })
+        }
+        if (iZonaId) {
+            ies_tmp = ies_tmp.filter((ie: any) => {
+                if (ie.iZonaId == iZonaId) {
+                    return ie
+                }
+                return null
+            })
+        }
+        if (iTipoSectorId) {
+            ies_tmp = ies_tmp.filter((ie: any) => {
+                if (ie.iTipoSectorId == iTipoSectorId) {
+                    return ie
+                }
+                return null
+            })
+        }
+        if (iUgelId) {
+            ies_tmp = ies_tmp.filter((ie: any) => {
+                if (ie.iUgelId == iUgelId) {
+                    return ie
+                }
+                return null
+            })
+        }
+        return ies_tmp
+    }
+
+    getSexos() {
+        if (!this.sexos) {
+            this.sexos = [
+                { label: 'MASCULINO', value: 'M' },
+                { label: 'FEMENINO', value: 'F' },
+            ]
+        }
+        return this.sexos
+    }
+
+    getEstados() {
+        if (!this.estados) {
+            this.estados = [
+                { label: 'ACTIVO', value: 1 },
+                { label: 'INACTIVO', value: 2 },
+            ]
+        }
+        return this.estados
+    }
+
+    ngOnDestroy() {
+        this.onDestroy$.next(true)
+        this.onDestroy$.complete()
+    }
+}

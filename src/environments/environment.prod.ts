047636c0cfd30cf2789453e71921d4358752a9e4
unknown <Administrador@SVRAPP_TEST.DREMOQUEGUA.local>
2025-07-01 10:43:49 -0500

    Se agrego nuevo environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    backend: '',
-    backendApi: '',
+    backend: 'http://45.169.92.189:7002',
+    backendApi: 'http://45.169.92.189:7002/api',
 }

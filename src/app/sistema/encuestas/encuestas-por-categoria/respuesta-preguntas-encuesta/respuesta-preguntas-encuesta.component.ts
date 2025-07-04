56f56aeb55daf1acb85daac8a8faa5e394e7cb6d
Max Cantuta <maxcantutajor.24@gmail.com>
2025-07-04 08:03:33 -0500

    avance

---
+++ src/app/sistema/encuestas/encuestas-por-categoria/respuesta-preguntas-encuesta/respuesta-preguntas-encuesta.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-respuesta-preguntas-encuesta',
+    standalone: true,
+    imports: [],
+    templateUrl: './respuesta-preguntas-encuesta.component.html',
+    styleUrl: './respuesta-preguntas-encuesta.component.scss',
+})
+export class RespuestaPreguntasEncuestaComponent {}

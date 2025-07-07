30e1136ecbd41de511e7930eee3a2335061c6ded
Wilmar Garibaldi <w_garibaldi@hotmail.com>
2025-07-07 08:46:45 -0500

    Avance

---
+++ src/app/sistema/encuestas/nueva-encuesta/nueva-encuesta/nueva-encuesta.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-nueva-encuesta',
+    standalone: true,
+    imports: [],
+    templateUrl: './nueva-encuesta.component.html',
+    styleUrl: './nueva-encuesta.component.scss',
+})
+export class NuevaEncuestaComponent {}

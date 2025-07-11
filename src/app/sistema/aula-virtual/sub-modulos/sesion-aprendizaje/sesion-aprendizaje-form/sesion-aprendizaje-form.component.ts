05536c82c5ca9e1728aaaa1977fc255b7d2efcb5
DREW <jhoand60@gmail.com>
2025-07-11 07:35:39 -0500

    feat: add CalendarioPeriodosEvalacionesService and integrate with SesionAprendizajeForm for period management

--- src/app/sistema/aula-virtual/sub-modulos/sesion-aprendizaje/sesion-aprendizaje-form/sesion-aprendizaje-form.component.ts
+++ src/app/sistema/aula-virtual/sub-modulos/sesion-aprendizaje/sesion-aprendizaje-form/sesion-aprendizaje-form.component.ts
@@ -6,6 +6,7 @@ import { NgIf } from '@angular/common';
 import { Component, EventEmitter, inject, Input, OnChanges, Output } from '@angular/core';
 import { MessageService } from 'primeng/api';
 import { SubirArchivoComponent } from '../../../../../shared/subir-archivo/subir-archivo.component';
+import { CalendarioPeriodosEvalacionesService } from '@/app/servicios/acad/calendario-periodos-evaluaciones.service';
 
 @Component({
   selector: 'app-sesion-aprendizaje-form',
@@ -21,13 +22,16 @@ export class SesionAprendizajeFormComponent implements OnChanges {
   private _TipoExperienciaAprendizajeService = inject(TipoExperienciaAprendizajeService);
   private _ConstantesService = inject(ConstantesService);
   private _MessageService = inject(MessageService);
+  private _CalendarioPeriodosEvalacionesService = inject(CalendarioPeriodosEvalacionesService);
 
   tipoExperiencia = [];
+  periodos = [];
 
   ngOnChanges(changes) {
     if (changes['showModal']) {
       this.showModal = changes['showModal'].currentValue;
       this.obtenerTipoExperienciaAprendizaje();
+      this.obtenerPeriodosxiYAcadIdxiSedeIdxFaseRegular();
     }
   }
 
@@ -65,6 +69,46 @@ export class SesionAprendizajeFormComponent implements OnChanges {
         }
       },
     });
+  }
+
+  obtenerPeriodosxiYAcadIdxiSedeIdxFaseRegular() {
+    const iYAcadId = this._ConstantesService.iYAcadId;
+    const iSedeId = this._ConstantesService.iSedeId;
+    const params = {
+      iCredId: this._ConstantesService.iCredId,
+    };
+    this._CalendarioPeriodosEvalacionesService
+      .obtenerPeriodosxiYAcadIdxiSedeIdxFaseRegular(iYAcadId, iSedeId, params)
+      .subscribe({
+        next: resp => {
+          if (resp.validated) {
+            const data = resp.data;
+            this.periodos = data;
+          }
+        },
+        error: error => {
+          const errores = error?.error?.errors;
+          if (error.status === 422 && errores) {
+            // Recorre y muestra cada mensaje de error
+            Object.keys(errores).forEach(campo => {
+              errores[campo].forEach((mensaje: string) => {
+                this.mostrarMensajeToast({
+                  severity: 'error',
+                  summary: 'Error de validación',
+                  detail: mensaje,
+                });
+              });
+            });
+          } else {
+            // Error genérico si no hay errores específicos
+            this.mostrarMensajeToast({
+              severity: 'error',
+              summary: 'Error',
+              detail: error?.error?.message || 'Ocurrió un error inesperado',
+            });
+          }
+        },
+      });
   }
 
   mostrarMensajeToast(message) {

5a6821ffca3d8391ddd18e14879d0e5f58fd4f87
Max Cantuta <maxcantutajor.24@gmail.com>
2025-08-05 10:22:04 -0500

    se agrega el nombre hardcoded utlizando de contexto privado  para los modales de referencia

--- src/app/sistema/ere/evaluaciones/logro-alcanzado/logro-alcanzado.component.ts
+++ src/app/sistema/ere/evaluaciones/logro-alcanzado/logro-alcanzado.component.ts
@@ -1,150 +1,150 @@
-import { Component, OnInit } from '@angular/core'
-import { PrimengModule } from '@/app/primeng.module'
+import { Component, OnInit } from '@angular/core';
+import { PrimengModule } from '@/app/primeng.module';
 import {
-    IActionTable,
-    TablePrimengComponent,
-} from '@/app/shared/table-primeng/table-primeng.component'
-import { RegistrarLogroAlcanzadoComponent } from './registrar-logro-alcanzado/registrar-logro-alcanzado.component'
-import { BoletaLogroComponent } from './boleta-logro/boleta-logro.component'
+  IActionTable,
+  TablePrimengComponent,
+} from '@/app/shared/table-primeng/table-primeng.component';
+import { RegistrarLogroAlcanzadoComponent } from './registrar-logro-alcanzado/registrar-logro-alcanzado.component';
+import { BoletaLogroComponent } from './boleta-logro/boleta-logro.component';
 @Component({
-    selector: 'app-logro-alcanzado',
-    standalone: true,
-    imports: [
-        PrimengModule,
-        TablePrimengComponent,
-        RegistrarLogroAlcanzadoComponent,
-        BoletaLogroComponent,
-    ],
-    templateUrl: './logro-alcanzado.component.html',
-    styleUrl: './logro-alcanzado.component.scss',
+  selector: 'app-logro-alcanzado',
+  standalone: true,
+  imports: [
+    PrimengModule,
+    TablePrimengComponent,
+    RegistrarLogroAlcanzadoComponent,
+    BoletaLogroComponent,
+  ],
+  templateUrl: './logro-alcanzado.component.html',
+  styleUrl: './logro-alcanzado.component.scss',
 })
 export class LogroAlcanzadoComponent implements OnInit {
-    //breadCrumbHome: MenuItem
-    dialogRegistrarLogroAlcanzado: boolean = false
-    registroTitleModal: string
-    dialogBoletaLogroAlcanzado: boolean = false
-    boletaTitleModal: string
-    selectedItem: any
-    dataSugerencias = [
-        {
-            item: 1,
-            iLogroAlcanzadoId: 1,
-            cAsunto: 'Mejora en materiales de clase',
-            cNombreEstudiante: 'Gómez Torres Luis Alberto',
-            cNivelLogroAlcanzado: '4to',
-            cSeccion: 'c',
-            docuento_identidad: '48783215',
-        },
-        {
-            item: 2,
-            iLogroAlcanzadoId: 2,
-            //dtFechaCreacion: new Date('2025-04-20'),
-            cAsunto: 'Mejora en materiales de clase',
-            cNombreEstudiante: 'perez perez luis fernando',
-            cNivelLogroAlcanzado: '4to',
-            cSeccion: 'A',
-            docuento_identidad: '46983215',
-        },
-    ]
+  //breadCrumbHome: MenuItem
+  dialogRegistrarLogroAlcanzado: boolean = false;
+  registroTitleModal: string;
+  dialogBoletaLogroAlcanzado: boolean = false;
+  boletaTitleModal: string;
+  selectedItem: any;
+  dataSugerencias = [
+    {
+      item: 1,
+      iLogroAlcanzadoId: 1,
+      cAsunto: 'Mejora en materiales de clase',
+      cNombreEstudiante: 'Gómez Torres Luis Alberto',
+      cNivelLogroAlcanzado: '4to',
+      cSeccion: 'c',
+      docuento_identidad: '48783215',
+    },
+    {
+      item: 2,
+      iLogroAlcanzadoId: 2,
+      //dtFechaCreacion: new Date('2025-04-20'),
+      cAsunto: 'Mejora en materiales de clase',
+      cNombreEstudiante: 'perez perez luis fernando',
+      cNivelLogroAlcanzado: '4to',
+      cSeccion: 'A',
+      docuento_identidad: '46983215',
+    },
+  ];
 
-    columns = [
-        {
-            type: 'item',
-            width: '1rem',
-            field: 'item',
-            header: '#',
-            text_header: 'center',
-            text: 'center',
-        },
-        {
-            type: 'text',
-            width: '5rem',
-            field: 'cNombreEstudiante',
-            header: 'Apellidos y Nombre',
-            text_header: 'center',
-            text: 'center',
-        },
-        {
-            type: 'text',
-            width: '5rem',
-            field: 'cNivelLogroAlcanzado',
-            header: 'Nivel',
-            text_header: 'center',
-            text: 'center',
-        },
-        {
-            type: 'text',
-            width: '12rem',
-            field: 'cSeccion',
-            header: 'Sección',
-            text_header: 'center',
-            text: 'center',
-        },
-        {
-            type: 'text',
-            width: '8rem',
-            field: 'docuento_identidad',
-            header: 'DNI/CE',
-            text_header: 'center',
-            text: 'center',
-        },
-        {
-            type: 'actions',
-            width: '3rem',
-            field: 'actions',
-            header: 'Acciones',
-            text_header: 'center',
-            text: 'center',
-        },
-    ]
-    ngOnInit() {
-        console.log('Logro alcanzado')
+  columns = [
+    {
+      type: 'item',
+      width: '1rem',
+      field: 'item',
+      header: '#',
+      text_header: 'center',
+      text: 'center',
+    },
+    {
+      type: 'text',
+      width: '5rem',
+      field: 'cNombreEstudiante',
+      header: 'Apellidos y Nombre',
+      text_header: 'center',
+      text: 'center',
+    },
+    {
+      type: 'text',
+      width: '5rem',
+      field: 'cNivelLogroAlcanzado',
+      header: 'Nivel',
+      text_header: 'center',
+      text: 'center',
+    },
+    {
+      type: 'text',
+      width: '12rem',
+      field: 'cSeccion',
+      header: 'Sección',
+      text_header: 'center',
+      text: 'center',
+    },
+    {
+      type: 'text',
+      width: '8rem',
+      field: 'docuento_identidad',
+      header: 'DNI/CE',
+      text_header: 'center',
+      text: 'center',
+    },
+    {
+      type: 'actions',
+      width: '3rem',
+      field: 'actions',
+      header: 'Acciones',
+      text_header: 'center',
+      text: 'center',
+    },
+  ];
+  ngOnInit() {
+    console.log('Logro alcanzado');
+  }
+  registroLogroAlcanzado() {
+    const nombreEstudiante = this.selectedItem?.cNombreEstudiante || 'Estudiante';
+    this.registroTitleModal = `Registro : ${nombreEstudiante}`;
+    this.dialogRegistrarLogroAlcanzado = true;
+  }
+  listenDialogRegistrarLogro(event: boolean) {
+    if (event == false) {
+      this.dialogRegistrarLogroAlcanzado = false;
     }
-    registroLogroAlcanzado() {
-        this.registroTitleModal =
-            'Registro : Gómez Torres Luis Alberto - Nivel: 4to - Sección: C'
-        this.dialogRegistrarLogroAlcanzado = true
+  }
+  boletaLogroImprimir() {
+    this.boletaTitleModal = 'Boleta de Logros de';
+    this.dialogBoletaLogroAlcanzado = true;
+  }
+  listenDialogBoleta(event: boolean) {
+    if (event == false) {
+      this.dialogBoletaLogroAlcanzado = false;
     }
-    listenDialogRegistrarLogro(event: boolean) {
-        if (event == false) {
-            this.dialogRegistrarLogroAlcanzado = false
-        }
+  }
+  accionBtnItemTable({ accion, item }) {
+    switch (accion) {
+      case 'Resistrar':
+        this.selectedItem = item;
+        this.registroLogroAlcanzado();
+        break;
+      case 'Imprimir':
+        this.selectedItem = item;
+        this.boletaLogroImprimir();
+        break;
     }
-    boletaLogroImprimir() {
-        this.boletaTitleModal = 'Boleta de Logros de'
-        this.dialogBoletaLogroAlcanzado = true
-    }
-    listenDialogBoleta(event: boolean) {
-        if (event == false) {
-            this.dialogBoletaLogroAlcanzado = false
-        }
-    }
-    accionBtnItemTable({ accion, item }) {
-        switch (accion) {
-            case 'Resistrar':
-                this.selectedItem = item
-                this.registroLogroAlcanzado()
-                break
-            case 'Imprimir':
-                this.selectedItem = item
-                this.boletaLogroImprimir()
-                break
-        }
-    }
-    actions: IActionTable[] = [
-        {
-            labelTooltip: 'Resistrar logro',
-            icon: 'pi pi-file-edit',
-            accion: 'Resistrar',
-            type: 'item',
-            class: 'p-button-rounded p-button-primary p-button-text',
-        },
-        {
-            labelTooltip: 'Imprimir Boleta',
-            icon: 'pi pi-print',
-            accion: 'Imprimir',
-            type: 'item',
-            class: 'p-button-rounded p-button-success p-button-text',
-        },
-    ]
+  }
+  actions: IActionTable[] = [
+    {
+      labelTooltip: 'Resistrar logro',
+      icon: 'pi pi-file-edit',
+      accion: 'Resistrar',
+      type: 'item',
+      class: 'p-button-rounded p-button-primary p-button-text',
+    },
+    {
+      labelTooltip: 'Imprimir Boleta',
+      icon: 'pi pi-print',
+      accion: 'Imprimir',
+      type: 'item',
+      class: 'p-button-rounded p-button-success p-button-text',
+    },
+  ];
 }

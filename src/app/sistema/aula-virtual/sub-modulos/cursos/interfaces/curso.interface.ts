f46a44b3761e2437f994b7dfb279f4f77af8e7f3
DREW <jhoand60@gmail.com>
2025-08-12 14:31:20 -0500

    fix: actualizar curso interface

--- src/app/sistema/aula-virtual/sub-modulos/cursos/interfaces/curso.interface.ts
+++ src/app/sistema/aula-virtual/sub-modulos/cursos/interfaces/curso.interface.ts
@@ -1,34 +1,35 @@
 export interface ICurso {
-    iCursoId: string | number
-    cCursoNombre: string
-    descripcion?: string
-    seccion?: string
-    cGradoAbreviacion?: string
-    cCicloRomanos?: string
-    cNivelNombreCursos?: string
-    iEstudiantes?: number
-    iSilaboId?: string
-    nombrecompleto?: string
-    cNivelTipoNombre?: string
-    cSeccionNombre?: string
-    cantidad?: string
-    cCursoImagen?: string
-    idDocCursoId?: string
-    iNivelCicloId?: any
-    iCantidadPreguntas?: number
-    iCantidadMaximaPreguntas?: number
-    iCursosNivelGradId?: string
-    bTieneArchivo?: boolean
-    dtExamenFechaInicio?: string
-    dtExamenFechaFin?: string
-    iExamenCantidadPreguntas?: string
-    iEvaluacionIdHashed?: string
-    iCursoNivelGradId?: string
-    cSeccion?: string
-    iIeCursoId?: string
-    iNivelGradoId?: string
-    iSeccionId?: string
-    bDocente?: string
-    cDocente?: string
-    bEstudiante?: string
+  iCursoId: string | number;
+  cCursoNombre: string;
+  descripcion?: string;
+  seccion?: string;
+  cGradoAbreviacion?: string;
+  cCicloRomanos?: string;
+  cNivelNombreCursos?: string;
+  iEstudiantes?: number;
+  iSilaboId?: string;
+  nombrecompleto?: string;
+  cNivelTipoNombre?: string;
+  cSeccionNombre?: string;
+  cantidad?: string;
+  cCursoImagen?: string;
+  idDocCursoId?: string;
+  iNivelCicloId?: any;
+  iCantidadPreguntas?: number;
+  iCantidadMaximaPreguntas?: number;
+  iCursosNivelGradId?: string;
+  bTieneArchivo?: boolean;
+  dtExamenFechaInicio?: string;
+  dtExamenFechaFin?: string;
+  iExamenCantidadPreguntas?: string;
+  iEvaluacionIdHashed?: string;
+  iCursoNivelGradId?: string;
+  cSeccion?: string;
+  iIeCursoId?: string;
+  iNivelGradoId?: string;
+  iSeccionId?: string;
+  bDocente?: string;
+  cDocente?: string;
+  bEstudiante?: string;
+  iCapacitacionId?: string | number;
 }

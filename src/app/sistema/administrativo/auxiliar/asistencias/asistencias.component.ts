ad943f6e9e01c1954453bc877d2cd06c169cc54d
carlos alberto chura flores <chura.f5@gmail.com>
2025-09-01 08:29:42 -0500

    Falta culminar graficos y justificacion

--- src/app/sistema/administrativo/auxiliar/asistencias/asistencias.component.ts
+++ src/app/sistema/administrativo/auxiliar/asistencias/asistencias.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { PrimengModule } from '@/app/primeng.module';
-import { TablePrimengComponent } from '@/app/shared/table-primeng/table-primeng.component';
 import { GeneralService } from '@/app/servicios/general.service';
 import { LocalStoreService } from '@/app/servicios/local-store.service';
 import { MessageService } from 'primeng/api';
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
+import {
+  trigger,
+  // state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 @Component({
   selector: 'app-asistencias',
   standalone: true,
-  imports: [PrimengModule, TablePrimengComponent],
+  imports: [PrimengModule, ZXingScannerModule],
   templateUrl: './asistencias.component.html',
   styleUrl: './asistencias.component.scss',
+  animations: [
+    trigger('desvanecer', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'scale(0.95)' }),
+        animate('500ms ease-out', style({ opacity: 1, transform: 'scale(1)' })),
+      ]),
+      transition(':leave', [
+        animate('500ms ease-in', style({ opacity: 0, transform: 'scale(0.95)' })),
+      ]),
+    ]),
+  ],
 })
 export class AsistenciasComponent implements OnInit {
   private servicioGeneral = inject(GeneralService);
@@ -18,11 +36,18 @@ export class AsistenciasComponent implements OnInit {
   grado: any;
   seccion: any;
   aulas: any = [];
-  finalizar: boolean = false;
   datos: any = {};
   registro: any = [];
   alumnos: any = [];
-
+  estudiante: any = [];
+  codigo: any = [];
+  registrar: boolean = false;
+  visible: boolean = false;
+  finalizar: boolean = false;
+  scanner: boolean = false;
+  progreso: boolean = false;
+  registrarEstudiante: boolean = false;
+  temporal: any = [];
   tipoAsistencia = [
     {
       iTipoAsiId: '1',
@@ -53,18 +78,18 @@ export class AsistenciasComponent implements OnInit {
       bgColor: 'orange-boton',
     },
     {
-      iTipoAsiId: '7',
-      cTipoAsiLetra: '-',
-      cTipoAsiNombre: 'Sin Registro',
-      textColor: 'yellow-50-boton',
-      bgColor: 'yellow-boton',
-    },
-    {
       iTipoAsiId: '9',
       cTipoAsiLetra: 'P',
       cTipoAsiNombre: 'Tardanza Justificada',
       textColor: 'cyan-50-boton',
       bgColor: 'cyan-boton',
+    },
+    {
+      iTipoAsiId: '7',
+      cTipoAsiLetra: '-',
+      cTipoAsiNombre: 'Sin Registro',
+      textColor: 'yellow-50-boton',
+      bgColor: 'yellow-boton',
     },
   ];
 
@@ -118,45 +143,228 @@ export class AsistenciasComponent implements OnInit {
   buscarGrupo() {
     if (this.datos.iGradoId && this.datos.iSeccionId) {
       this.finalizar = true;
-      this.servicioGeneral
-        .getRecibirDatos({
-          petition: 'post',
-          group: 'asi',
-          prefix: 'grupos',
-          ruta: 'buscar-grado-seccion',
-          data: {
-            dtFecha: this.datos.fecha,
-            iGradoId: this.datos.iGradoId,
-            iSeccionId: this.datos.iSeccionId,
-            iSedeId: this.dremoPerfil.iSedeId,
-            iYAcadId: this.dremoiYAcadId,
-          },
-        })
-        .subscribe({
-          next: data => {
-            this.alumnos = data.data;
-            this.alumnos.forEach(item => {
-              const seleccionar = this.tipoAsistencia.find(
-                lista => lista.iTipoAsiId == item.iTipoAsiId
-              );
-              item.textColor = seleccionar.textColor;
-              item.bgColor = seleccionar.bgColor;
-              item.cTipoAsiNombre = seleccionar.cTipoAsiNombre;
-            });
-            console.log('verificar #1 ', this.alumnos);
-          },
-          error: err => {
-            console.log(err);
-          },
-          complete: () => {
-            this.finalizar = false;
-          },
-        });
+
+      const enlace = {
+        petition: 'post',
+        group: 'asi',
+        prefix: 'grupos',
+        ruta: 'buscar-lista-estudiantes',
+        data: {
+          opcion: 'buscar-grupo',
+          dtAsistencia: this.datos.dtAsistencia,
+          iGradoId: this.datos.iGradoId,
+          iSeccionId: this.datos.iSeccionId,
+          iSedeId: this.dremoPerfil.iSedeId,
+          iYAcadId: this.dremoiYAcadId,
+        },
+      };
+
+      this.servicioGeneral.getRecibirDatos(enlace).subscribe({
+        next: data => {
+          this.alumnos = data.data;
+          this.alumnos.forEach(item => {
+            const seleccionar = this.tipoAsistencia.find(
+              lista => lista.iTipoAsiId == item.iTipoAsiId
+            );
+            item.textColor = seleccionar.textColor;
+            item.bgColor = seleccionar.bgColor;
+            item.cTipoAsiNombre = seleccionar.cTipoAsiNombre;
+          });
+        },
+        error: () => {
+          this.mensajeError('Mensaje del sistema', 'Error al buscar alumnos');
+          this.finalizar = false;
+        },
+        complete: () => {
+          this.finalizar = false;
+          this.registrar = true;
+        },
+      });
     } else {
       this.mensajeError('Mensaje del sistema', 'Debes Seleccionar un Grado');
     }
   }
 
+  buscarCodigo() {
+    if (this.datos.cEstCodigo || this.datos.cPersDocumento) {
+      const enlace = {
+        petition: 'post',
+        group: 'asi',
+        prefix: 'grupos',
+        ruta: 'buscar-lista-estudiantes',
+        data: {
+          opcion: 'buscar-estudiante',
+          cEstCodigo: this.datos.cEstCodigo,
+          dtAsistencia: this.datos.dtAsistencia,
+          cPersDocumento: this.datos.cPersDocumento,
+          iSedeId: this.dremoPerfil.iSedeId,
+          iYAcadId: this.dremoiYAcadId,
+        },
+      };
+
+      this.servicioGeneral.getRecibirDatos(enlace).subscribe({
+        next: data => {
+          this.estudiante = data.data;
+          this.estudiante.forEach(item => {
+            const seleccionar = this.tipoAsistencia.find(
+              lista => lista.iTipoAsiId == item.iTipoAsiId
+            );
+            item.textColor = seleccionar.textColor;
+            item.bgColor = seleccionar.bgColor;
+            item.cTipoAsiNombre = seleccionar.cTipoAsiNombre;
+          });
+        },
+        error: () => {
+          this.mensajeError('Mensaje del sistema', 'Error al buscar alumnos');
+        },
+        complete: () => {
+          this.finalizar = false;
+          this.registrarEstudiante = true;
+        },
+      });
+    }
+  }
+  escaner(qr: string) {
+    this.scanner = false;
+    const extraer = qr.split('|');
+    if (extraer.length > 0) {
+      this.visible = true;
+      this.datos.cEstCodigo = extraer[1];
+      this.datos.cPersDocumento = extraer[0];
+
+      const enlace = {
+        petition: 'post',
+        group: 'asi',
+        prefix: 'grupos',
+        ruta: 'buscar-lista-estudiantes',
+        data: {
+          opcion: 'buscar-estudiante',
+          cEstCodigo: this.datos.cEstCodigo,
+          cPersDocumento: this.datos.cPersDocumento,
+          iSedeId: this.dremoPerfil.iSedeId,
+          iYAcadId: this.dremoiYAcadId,
+        },
+      };
+
+      this.servicioGeneral.getRecibirDatos(enlace).subscribe({
+        next: data => {
+          this.codigo = data.data[0];
+          const fecha = new Date();
+          const fechas =
+            fecha.getFullYear() +
+            '-' +
+            (fecha.getMonth() + 1).toString().padStart(2, '0') +
+            '-' +
+            fecha.getDate().toString().padStart(2, '0');
+          const hora =
+            fecha.getHours().toString().padStart(2, '0') +
+            ':' +
+            fecha.getMinutes().toString().padStart(2, '0') +
+            ':' +
+            fecha.getSeconds().toString().padStart(2, '0');
+          const fechaCompleta = new Date(fechas + ' ' + hora);
+          this.codigo.dtAsistencia = fechaCompleta;
+          const marcar = this.codigo.dtTurnoInicia > fechaCompleta ? 1 : 2;
+          this.codigo.iTipoAsiId = marcar;
+        },
+        error: () => {
+          this.mensajeError('Mensaje del sistema', 'Error al buscar alumnos');
+        },
+      });
+    }
+  }
+
+  guardarAsistenciaSacnner() {
+    const enlace = {
+      petition: 'post',
+      group: 'asi',
+      prefix: 'grupos',
+      ruta: 'guardar-asistencia',
+      data: {
+        iEstudianteId: this.codigo.iEstudianteId,
+        iMatrId: this.codigo.iMatrId,
+        iNivelGradoId: this.codigo.iNivelGradoId,
+        iTipoAsiId: this.codigo.iTipoAsiId,
+        iSeccionId: this.codigo.iSeccionId,
+        iSedeId: this.dremoPerfil.iSedeId,
+        iYAcadId: this.dremoiYAcadId,
+        dtAsistencia: this.codigo.dtAsistencia,
+        idAsistencia: this.codigo.idAsistencia,
+      },
+    };
+
+    this.servicioGeneral.getRecibirDatos(enlace).subscribe({
+      next: data => {
+        const resultado = parseInt(data.data[0].resultado);
+        if (resultado > 0) {
+          this.temporal.push(this.codigo);
+        }
+        this.mensajeSuccess('Mensaje del sistema', 'Existo al guardar asistencia');
+        this.visible = false;
+      },
+      error: () => {
+        this.mensajeError('Mensaje del sistema', 'Error al guardar asistencia');
+        this.visible = false;
+      },
+    });
+  }
+
+  guardarAsistenciaAula() {
+    const enlace = {
+      petition: 'post',
+      group: 'asi',
+      prefix: 'grupos',
+      ruta: 'guardar-asistencia-aula',
+      data: {
+        asistencia: JSON.stringify(this.alumnos),
+        dtAsistencia: this.datos.dtAsistencia,
+        iSedeId: this.dremoPerfil.iSedeId,
+        iYAcadId: this.dremoiYAcadId,
+      },
+    };
+
+    this.servicioGeneral.getRecibirDatos(enlace).subscribe({
+      next: () => {
+        this.buscarGrupo();
+        this.mensajeSuccess('Mensaje del sistema', 'Existo al guardar asistencia');
+      },
+      error: () => {
+        this.buscarGrupo();
+        this.mensajeError('Mensaje del sistema', 'Error al guardar asistencia');
+      },
+    });
+  }
+
+  guardarAsistencia() {
+    const enlace = {
+      petition: 'post',
+      group: 'asi',
+      prefix: 'grupos',
+      ruta: 'guardar-asistencia',
+      data: {
+        iMatrId: this.estudiante[0].iMatrId,
+        iEstudianteId: this.estudiante[0].iEstudianteId,
+        iNivelGradoId: this.estudiante[0].iNivelGradoId,
+        iTipoAsiId: this.estudiante[0].iTipoAsiId,
+        iSeccionId: this.estudiante[0].iSeccionId,
+        iSedeId: this.dremoPerfil.iSedeId,
+        iYAcadId: this.dremoiYAcadId,
+        dtAsistencia: this.datos.dtAsistencia,
+        idAsistencia: this.estudiante[0].idAsistencia,
+      },
+    };
+
+    this.servicioGeneral.getRecibirDatos(enlace).subscribe({
+      next: () => {
+        this.mensajeSuccess('Mensaje del sistema', 'Existo al guardar asistencia');
+        this.buscarCodigo();
+      },
+      error: () => {
+        this.mensajeError('Mensaje del sistema', 'Error al guardar asistencia');
+        this.buscarCodigo();
+      },
+    });
+  }
   /**
    * filtramos las secciones
    * En caso de que se generen duplicados desde la DB, estos son eliminados
@@ -214,5 +422,17 @@ export class AsistenciasComponent implements OnInit {
     this.alumnos[index].iTipoAsiId = this.tipoAsistencia[indice].iTipoAsiId;
     this.alumnos[index].cTipoAsiLetra = this.tipoAsistencia[indice].cTipoAsiLetra;
     this.alumnos[index].cTipoAsiNombre = this.tipoAsistencia[indice].cTipoAsiNombre;
+  }
+  cambiarEstadoEstudiante(index: any, item: any) {
+    const valor = this.tipoAsistencia.findIndex(valor => valor.iTipoAsiId == item);
+    const indice = (Number(valor) + 1) % this.tipoAsistencia.length;
+    this.estudiante[index].iTipoAsiId = this.tipoAsistencia[indice].iTipoAsiId;
+    this.estudiante[index].cTipoAsiLetra = this.tipoAsistencia[indice].cTipoAsiLetra;
+    this.estudiante[index].cTipoAsiNombre = this.tipoAsistencia[indice].cTipoAsiNombre;
+  }
+  camaraEncontrada() {
+    console.log('camara encontrada');
+    this.progreso = false;
+    console.log(this.progreso);
   }
 }
--- src/app/sistema/administrativo/auxiliar/reporte-asistencias/reporte-asistencias.component.ts
+++ src/app/sistema/administrativo/auxiliar/reporte-asistencias/reporte-asistencias.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { GeneralService } from '@/app/servicios/general.service';
 import { PrimengModule } from '@/app/primeng.module';
+import { MessageService } from 'primeng/api';
+import { LocalStoreService } from '@/app/servicios/local-store.service';
 @Component({
   selector: 'app-reporte-asistencias',
   standalone: true,
@@ -7,4 +10,240 @@ import { PrimengModule } from '@/app/primeng.module';
   templateUrl: './reporte-asistencias.component.html',
   styleUrl: './reporte-asistencias.component.scss',
 })
-export class ReporteAsistenciasComponent {}
+export class ReporteAsistenciasComponent implements OnInit {
+  private servicioGeneral = inject(GeneralService);
+  dremoiYAcadId: any;
+  dremoPerfil: any;
+  dias: any = [];
+
+  meses: any = [
+    { nombre: 'Enero', numero: '01', dia: '31' },
+    { nombre: 'Febrero', numero: '02', dia: '28' },
+    { nombre: 'Marzo', numero: '03', dia: '31' },
+    { nombre: 'Abril', numero: '04', dia: '30' },
+    { nombre: 'Mayo', numero: '05', dia: '31' },
+    { nombre: 'Junio', numero: '06', dia: '30' },
+    { nombre: 'Julio', numero: '07', dia: '31' },
+    { nombre: 'Agosto', numero: '08', dia: '31' },
+    { nombre: 'Septiembre', numero: '09', dia: '30' },
+    { nombre: 'Octubre', numero: '10', dia: '31' },
+    { nombre: 'Noviembre', numero: '11', dia: '30' },
+    { nombre: 'Diciembre', numero: '12', dia: '31' },
+  ];
+
+  opcion: any = [
+    { id: 1, nombre: 'reporte por grado y Seccion' },
+    { id: 2, nombre: 'reporte por alumno' },
+  ];
+
+  tipoAsistencia = [
+    {
+      iTipoAsiId: '1',
+      cTipoAsiLetra: 'X',
+      cTipoAsiNombre: 'Asistio',
+      textColor: 'green-50-boton',
+      bgColor: 'bg-green-500',
+    },
+    {
+      iTipoAsiId: '2',
+      cTipoAsiLetra: 'T',
+      cTipoAsiNombre: 'Tardanza',
+      textColor: 'red-50-boton',
+      bgColor: 'bg-orange-500',
+    },
+    {
+      iTipoAsiId: '3',
+      cTipoAsiLetra: 'I',
+      cTipoAsiNombre: 'Inasistencia',
+      textColor: 'primary-50-boton',
+      bgColor: 'bg-red-500',
+    },
+    {
+      iTipoAsiId: '4',
+      cTipoAsiLetra: 'J',
+      cTipoAsiNombre: 'Inasistencia Justificada',
+      textColor: 'orange-50-boton',
+      bgColor: 'bg-primary-500',
+    },
+    {
+      iTipoAsiId: '9',
+      cTipoAsiLetra: 'P',
+      cTipoAsiNombre: 'Tardanza Justificada',
+      textColor: 'cyan-50-boton',
+      bgColor: 'bg-yellow-500',
+    },
+    {
+      iTipoAsiId: '7',
+      cTipoAsiLetra: '-',
+      cTipoAsiNombre: 'Sin Registro',
+      textColor: 'yellow-50-boton',
+      bgColor: 'bg-cyan-500',
+    },
+  ];
+
+  aulas: any = [];
+  alumnos: any = [];
+  tipo: any = {};
+  datos: any = {};
+  grado: any = [];
+  seccion: any = [];
+  finalizar: boolean = false;
+
+  constructor(
+    private messageService: MessageService,
+    private store: LocalStoreService
+  ) {
+    this.dremoiYAcadId = this.store.getItem('dremoiYAcadId');
+    this.dremoPerfil = this.store.getItem('dremoPerfil');
+  }
+
+  ngOnInit() {
+    this.buscarAulas();
+  }
+
+  buscarReporteEstudiante() {
+    const year = this.dremoiYAcadId;
+    const filtrar = year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+    this.meses[1].dia = filtrar ? '29' : '28';
+
+    const revisar = this.meses.find(item => item.numero === this.tipo.numero).dia;
+    this.dias = Array.from({ length: revisar }, (_, i) => i + 1);
+    const enlace = {
+      petition: 'post',
+      group: 'asi',
+      prefix: 'grupos',
+      ruta: 'buscar-reporte',
+      data: {
+        opcion: 'buscar-estudiante',
+        mes: this.tipo.numero,
+        cPersDocumento: this.datos.cPersDocumento,
+        cEstCodigo: this.datos.cEstCodigo,
+        iSedeId: this.dremoPerfil.iSedeId,
+        iYAcadId: this.dremoiYAcadId,
+      },
+    };
+
+    this.conexionReporte(enlace);
+  }
+
+  buscarReporteAula() {
+    const year = this.dremoiYAcadId;
+    const filtrar = year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+    this.meses[1].dia = filtrar ? '29' : '28';
+
+    const revisar = this.meses.find(item => item.numero === this.tipo.numero).dia;
+    this.dias = Array.from({ length: revisar }, (_, i) => i + 1);
+    const enlace = {
+      petition: 'post',
+      group: 'asi',
+      prefix: 'grupos',
+      ruta: 'buscar-reporte',
+      data: {
+        opcion: 'buscar-aula',
+        mes: this.tipo.numero,
+        iGradoId: this.datos.iGradoId,
+        iSeccionId: this.datos.iSeccionId,
+        iSedeId: this.dremoPerfil.iSedeId,
+        iYAcadId: this.dremoiYAcadId,
+      },
+    };
+
+    this.conexionReporte(enlace);
+  }
+
+  conexionReporte(enlace: any) {
+    this.servicioGeneral.getRecibirDatos(enlace).subscribe({
+      next: data => {
+        this.alumnos = data.data;
+
+        this.alumnos.forEach(list => {
+          list.asistencia = JSON.parse(list.asistencia);
+
+          list.asistencia.forEach(item => {
+            const index = parseInt(item.dtAsistencia.split('-')[2]);
+            item.diaEspecifico = index;
+            const lista = this.tipoAsistencia.find(grupo => grupo.iTipoAsiId == item.iTipoAsiId);
+            item.bgColor = lista.bgColor;
+            item.textColor = 'text-white';
+          });
+
+          this.dias.forEach(item => {
+            const lista = list.asistencia.find(list => list.diaEspecifico === item);
+            if (!lista) {
+              const nuevo = {
+                diaEspecifico: item,
+                iTipoAsiId: 7,
+                cTipoAsiLetra: '-',
+                dtAsistencia: '',
+                textColor: 'text-white',
+                bgColor: 'bg-cyan-500',
+              };
+              list.asistencia.push(nuevo);
+            }
+          });
+        });
+      },
+      error: err => {
+        console.log(err);
+      },
+      complete: () => {
+        this.finalizar = false;
+      },
+    });
+  }
+
+  buscarAulas() {
+    this.finalizar = true;
+
+    const buscar = {
+      iSedeId: this.dremoPerfil.iSedeId,
+      iYAcadId: this.dremoiYAcadId,
+    };
+
+    this.servicioGeneral
+      .getRecibirDatos({
+        petition: 'post',
+        group: 'acad',
+        prefix: 'calendarioAcademico',
+        ruta: 'searchAcademico',
+        data: {
+          json: JSON.stringify(buscar),
+          _opcion: 'getGradoSeccionXiSedeIdXiYAcadId',
+        },
+      })
+      .subscribe({
+        next: data => {
+          this.aulas = data.data;
+          this.grado = this.removeDuplicatesByiGradoId(this.aulas);
+        },
+        error: err => {
+          console.log(err);
+        },
+        complete: () => {
+          this.finalizar = false;
+        },
+      });
+  }
+
+  removeDuplicatesByiGradoId(array: any[]): any[] {
+    const seen = new Set<number>();
+    return array.filter(item => {
+      if (seen.has(item.iGradoId)) {
+        return false;
+      }
+      seen.add(item.iGradoId);
+      return true;
+    });
+  }
+
+  filtrarSeccion() {
+    const gruposUnicos = new Set();
+    this.seccion = this.aulas
+      .filter(item => item.iGradoId === this.datos.iGradoId)
+      .filter(item => {
+        if (gruposUnicos.has(item.iSeccionId)) return false;
+        gruposUnicos.add(item.iSeccionId);
+        return true;
+      });
+  }
+}

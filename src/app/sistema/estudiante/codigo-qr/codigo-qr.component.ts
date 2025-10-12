a19d55696983c0714db0d052dd9d7863c4aa1ccb
carlos alberto chura flores <chura.f5@gmail.com>
2025-10-11 23:09:10 -0500

    Se agrego funcion descargar para los codigos qr por estudiante

--- src/app/sistema/estudiante/codigo-qr/codigo-qr.component.ts
+++ src/app/sistema/estudiante/codigo-qr/codigo-qr.component.ts
@@ -1,8 +1,9 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { PrimengModule } from '@/app/primeng.module';
-import { QRCodeModule } from 'angularx-qrcode';
 import { ConstantesService } from '@/app/servicios/constantes.service';
 import { GeneralService } from '@/app/servicios/general.service';
+import { QRCodeModule } from 'angularx-qrcode';
+
 @Component({
   selector: 'app-codigo-qr',
   standalone: true,
@@ -42,5 +43,42 @@ export class CodigoQrComponent implements OnInit {
         console.log(error);
       },
     });
+  }
+  descargarImagenQR() {
+    const canvasElement = document.querySelector('canvas') as HTMLCanvasElement;
+    const dataUrl = canvasElement.toDataURL();
+
+    // converts base 64 encoded image to blobData
+    const blobData = this.convertBase64ToBlob(dataUrl);
+    const nav = window.navigator as any;
+    if (window.navigator && nav.msSaveOrOpenBlob) {
+      //IE
+      nav.msSaveOrOpenBlob(blobData, 'Qrcode');
+    } else {
+      // chrome
+      const blob = new Blob([blobData], { type: 'image/png' });
+      const url = window.URL.createObjectURL(blob);
+      // window.open(url);
+      const link = document.createElement('a');
+      link.href = url;
+      link.download = 'qr-code';
+      link.click();
+    }
+  }
+  private convertBase64ToBlob(Base64Image: any) {
+    // SPLIT INTO TWO PARTS
+    const parts = Base64Image.split(';base64,');
+    // HOLD THE CONTENT TYPE
+    const imageType = parts[0].split(':')[1];
+    // DECODE BASE64 STRING
+    const decodedData = window.atob(parts[1]);
+    // CREATE UNIT8ARRAY OF SIZE SAME AS ROW DATA LENGTH
+    const uInt8Array = new Uint8Array(decodedData.length);
+    // INSERT ALL CHARACTER CODE INTO UINT8ARRAY
+    for (let i = 0; i < decodedData.length; ++i) {
+      uInt8Array[i] = decodedData.charCodeAt(i);
+    }
+    // RETURN BLOB IMAGE AFTER CONVERSION
+    return new Blob([uInt8Array], { type: imageType });
   }
 }

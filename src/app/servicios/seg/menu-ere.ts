53c929c7b021d5d2a77d6bc5926dbac226d2cd5b
ejhomz <ejhomz7@gmail.com>
2025-12-16 15:37:41 -0500

    Se comento los recordatorios en Administrados y especialists

--- src/app/servicios/seg/menu-ere.ts
+++ src/app/servicios/seg/menu-ere.ts
@@ -170,11 +170,11 @@ export const administradorDremo = [
         label: 'Bienestar Social',
         icon: 'pi pi-fw pi-check-square',
         items: [
-          {
-            label: 'Recordatorios de cumpleaños',
-            icon: 'pi pi-fw pi-bell',
-            routerLink: ['/bienestar/recordario-fechas'],
-          },
+          // {
+          //   label: 'Recordatorios de cumpleaños',
+          //   icon: 'pi pi-fw pi-bell',
+          //   routerLink: ['/bienestar/recordario-fechas'],
+          // },
           {
             label: 'Seguimiento de bienestar',
             icon: 'pi pi-fw pi-eye',
@@ -241,11 +241,11 @@ export const especialistaDremo = [
             icon: 'pi pi-fw pi-list-check',
             routerLink: ['/bienestar/gestionar-encuestas'],
           },
-          {
-            label: 'Recordatorios de cumpleaños',
-            icon: 'pi pi-fw pi-bell',
-            routerLink: ['/bienestar/recordario-fechas'],
-          },
+          // {
+          //   label: 'Recordatorios de cumpleaños',
+          //   icon: 'pi pi-fw pi-bell',
+          //   routerLink: ['/bienestar/recordario-fechas'],
+          // },
           {
             label: 'Seguimiento de bienestar',
             icon: 'pi pi-fw pi-eye',
@@ -311,11 +311,11 @@ export const especialistaUgel = [
             icon: 'pi pi-fw pi-list-check',
             routerLink: ['/bienestar/gestionar-encuestas'],
           },
-          {
-            label: 'Recordatorios de cumpleaños',
-            icon: 'pi pi-fw pi-bell',
-            routerLink: ['/bienestar/recordario-fechas'],
-          },
+          // {
+          //   label: 'Recordatorios de cumpleaños',
+          //   icon: 'pi pi-fw pi-bell',
+          //   routerLink: ['/bienestar/recordario-fechas'],
+          // },
           {
             label: 'Seguimiento de bienestar',
             icon: 'pi pi-fw pi-eye',

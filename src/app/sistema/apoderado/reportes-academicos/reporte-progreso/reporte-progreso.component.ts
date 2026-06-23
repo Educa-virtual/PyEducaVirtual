24061ba9b06c4475bbb8bd770d083c2a7247c7a2
Paolo Villanueva <nv.paolo@gmail.com>
2026-06-23 09:24:19 -0500

    Fix reporte progreso apoderado

--- src/app/sistema/apoderado/reportes-academicos/reporte-progreso/reporte-progreso.component.ts
+++ src/app/sistema/apoderado/reportes-academicos/reporte-progreso/reporte-progreso.component.ts
@@ -64,7 +64,7 @@ export class ReporteProgresoComponent implements OnInit {
       },
     ];
 
-    this.obtenerEstudiantesApoderado();
+    // this.obtenerEstudiantesApoderado();
   }
 
   obtenerEstudiantesApoderado() {
